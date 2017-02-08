86408ca2bd651ad175a7e74ae4cd2fc2e0b004b6
Brian Lovin <briandlovin@gmail.com>
2017-02-07 19:33:11 -0800

    lift store up to root

--- src/App/index.js
+++ src/App/index.js
@@ -1,63 +1,28 @@
-import React, { Component } from 'react';
-import NavBar from './components/NavBar';
-import { Body } from './style';
-import StoryMaster from './components/StoryMaster';
-import DetailView from './components/DetailView';
-import { Provider } from 'react-redux'
-import { initStore } from '../store'
-import { loadState, saveState } from '../helpers/localStorage'
-// import ListDetail from './components/ListDetail';
-import * as firebase from 'firebase';
-import { setUser } from '../actions/user';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import NavBar from './components/NavBar'
+import { Body } from './style'
+import StoryMaster from './components/StoryMaster'
+import DetailView from './components/DetailView'
+import { setUser } from '../actions/user'
 import { setFrequencies } from '../actions/frequencies'
-import FIREBASE_CONFIG from '../config/FirebaseConfig';
-const fbconfig = {
-  apiKey: FIREBASE_CONFIG.API_KEY,
-  authDomain: FIREBASE_CONFIG.AUTH_DOMAIN,
-  databaseURL: FIREBASE_CONFIG.DB_URL,
-  storageBucket: FIREBASE_CONFIG.STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_CONFIG.MESSAGING_SENDER_ID
-};
 
-export default class App extends Component {
-	constructor() {
-    super()
-    firebase.initializeApp(fbconfig);
-
-    const localStorageState = loadState()
-    this.store = initStore(localStorageState)
-
-    this.store.subscribe(() => {
-      saveState(this.store.getState())
-    })
-
-    this.store.dispatch(setUser()) // on first load, set the user
-    this.store.dispatch(setFrequencies()) // on first load, get frequences from the server
-  }
-
-  selectTag = (tag) => {
-    this.setState({ currentTag: tag });
-  }
-
-  setCurrentUser = (user) => {
-    this.setState({
-      currentUser: user
-    });
-  }
-
-  selectPost(){
+class App extends Component {
+	constructor(props) {
+    super(props)
+    props.dispatch(setUser()) // on first load, set the user
+    props.dispatch(setFrequencies()) // on first load, get frequences from the server
   }
 
   render() {
     return(
-      <Provider store={this.store}>
-        <Body>
-          <NavBar />
-  				<StoryMaster />
-  				<DetailView />
-        
-        </Body>
-      </Provider>
+      <Body>
+        <NavBar />
+				<StoryMaster />
+				<DetailView />
+      </Body>
     )
   }
 }
+
+export default connect()(App)
--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import { BrowserRouter, Match } from 'react-router';
+import { Provider } from 'react-redux'
+import { initStore } from './store'
+import { loadState, saveState } from './helpers/localStorage'
+import * as firebase from 'firebase';
+import FIREBASE_CONFIG from './config/FirebaseConfig';
+const fbconfig = {
+  apiKey: FIREBASE_CONFIG.API_KEY,
+  authDomain: FIREBASE_CONFIG.AUTH_DOMAIN,
+  databaseURL: FIREBASE_CONFIG.DB_URL,
+  storageBucket: FIREBASE_CONFIG.STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_CONFIG.MESSAGING_SENDER_ID
+};
 
+const database = firebase.initializeApp(fbconfig)
+const localStorageState = loadState()
+const store = initStore(localStorageState)
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 const Root = () => {
 	return(
-		<BrowserRouter>
-			<Match exactly pattern="/" component={App}/>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Match exactly pattern="/" component={App}/>
+			</BrowserRouter>
+		</Provider>
 	)
 }
 
