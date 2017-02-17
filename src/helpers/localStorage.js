20dba01201d6da8565d243b667390a0a1feda889
Max <contact@mstoiber.com>
2017-02-17 13:30:52 +0100

    Reset broken states in localStorage
    If something is wrong with the saved state in the localStorage, there is
    no need to keep that broken state around

--- src/helpers/localStorage.js
+++ src/helpers/localStorage.js
@@ -1,3 +1,12 @@
+export const saveState = (state) => {
+	try {
+		const serializedState = JSON.stringify(state)
+		localStorage.setItem('state', serializedState)
+	} catch (err) {
+		// errors here
+	}
+}
+
 export const loadState = () => {
 	try {
 		const serializedState = localStorage.getItem('state')
@@ -6,16 +15,9 @@ export const loadState = () => {
 		}
 		return JSON.parse(serializedState)
 	} catch (err) {
+		// Empty the saved state since something went wrong with the data
+		saveState({})
 		return undefined
-	}
-}
-
-export const saveState = (state) => {
-	try {
-		const serializedState = JSON.stringify(state)
-		localStorage.setItem('state', serializedState)
-	} catch (err) {
-		// errors here
 	}
 }
 
