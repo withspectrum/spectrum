99ea0ad9fdd4700a2591f8dbd9eb33e0d8d020ec
Brian Lovin <briandlovin@gmail.com>
2017-03-12 15:02:51 -0700

    fix max-height

--- src/App/components/StoryDetail/style.js
+++ src/App/components/StoryDetail/style.js
@@ -5,7 +5,7 @@ export const ScrollBody = styled.div`
 	display: flex;
 	flex: 1 1 100%;
 	background-color: ${({ theme }) => theme.bg.default};
-	max-height: 100%;
+	max-height: calc(100% - 60px);
 	flex-direction: column;
 	overflow-y: scroll;
 
