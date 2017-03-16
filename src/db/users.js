40b2de1daaffb54514a1a095a2d36c29ca58a03d
Max <contact@mstoiber.com>
2017-03-15 17:18:28 -0700

    Store email in private

--- src/db/users.js
+++ src/db/users.js
@@ -15,7 +15,6 @@ export const createUser = user => {
       [`users/${uid}/public/displayName`]: user.displayName,
       [`users/${uid}/public/uid`]: uid,
       [`users/${uid}/public/photoURL`]: user.photoURL,
-      [`users/${uid}/public/email`]: user.email,
       [`users/${uid}/public/frequencies/-Kenm0MXIRCq8GkwiJKb`]: {
         //=> add `hugs n bugs` to user's default frequencies
         id: '-Kenm0MXIRCq8GkwiJKb',
@@ -35,6 +34,7 @@ export const createUser = user => {
         joined: firebase.database.ServerValue.TIMESTAMP,
       },
       [`users/${uid}/private`]: {
+        email: user.email,
         created: firebase.database.ServerValue.TIMESTAMP,
       },
     })
