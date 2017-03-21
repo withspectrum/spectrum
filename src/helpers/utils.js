ef0529a4394e0576e99d16b353f035569b22442b
Brian Lovin <briandlovin@gmail.com>
2017-03-20 23:24:49 -0700

    Properly handle timestamps between midnight and 1am

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -45,7 +45,12 @@ export const convertTimestampToDate = timestamp => {
   let month = monthNames[monthIndex];
   let year = date.getFullYear();
   let hours = date.getHours();
-  let cleanHours = hours > 12 ? hours - 12 : hours; // todo: support 24hr time
+  let cleanHours;
+  if (hours === 0) {
+    cleanHours = 12; // if timestamp is between midnight and 1am, show 12:XX am
+  } else {
+    cleanHours = hours > 12 ? hours - 12 : hours; // else show proper am/pm -- todo: support 24hr time
+  }
   let minutes = date.getMinutes();
   minutes = minutes >= 10 ? minutes : '0' + minutes.toString(); // turns 4 minutes into 04 minutes
   let ampm = hours >= 12 ? 'pm' : 'am'; // todo: support 24hr time
