99cc3238194fde11274ae03f02834641e1d9a425
Brian Lovin <briandlovin@gmail.com>
2017-03-22 11:58:50 -0700

    Update UI when store updates with reducers

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -24,8 +24,38 @@ export default function root(state = initialState, action) {
       });
     }
     case 'ADD_REACTION': {
+      return {
+        ...state,
+        messages: state.messages.map(message => {
+          if (message.id !== action.messageId) return message;
+          return {
+            ...message,
+            reactions: {
+              ...message.reactions,
+              [action.uid]: {
+                timestamp: Date.now(),
+                type: 'like',
+              },
+            },
+          };
+        }),
+      };
     }
     case 'REMOVE_REACTION': {
+      return {
+        ...state,
+        messages: state.messages.map(message => {
+          if (message.id !== action.messageId) return message;
+
+          const reactions = Object.assign({}, message.reactions);
+          delete reactions[action.uid];
+
+          return {
+            ...message,
+            reactions,
+          };
+        }),
+      };
     }
     case 'SEND_MESSAGE': {
       return {
