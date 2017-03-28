c81603cfd50c04a9058725dca676f0aadfb51946
Brian Lovin <briandlovin@gmail.com>
2017-03-27 21:18:08 -0700

    Remove extra dep import

--- src/App/MiddleColumn/StoryCard/index.js
+++ src/App/MiddleColumn/StoryCard/index.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import deepEqual from 'deep-eql';
 // eslint-disable-next-line
 import {
   StoryBody,
