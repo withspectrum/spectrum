16832f3a380e84c0dc68e704aa96afa262308d60
Brian Lovin <briandlovin@gmail.com>
2017-04-05 13:13:23 -0700

    Hotfix rollback

--- src/db/auth.js
+++ src/db/auth.js
@@ -1,13 +1,9 @@
 import auth from 'firebase/auth';
 import Raven from 'raven-js';
 
-const signIn = provider => new Promise((resolve, reject) => {
-  auth().signInWithRedirect(provider);
-
-  auth().getRedirectResult().then(result => {
-    return resolve(result.user);
-  });
-});
+const signIn = provider => {
+  return auth().signInWithPopup(provider);
+};
 
 export const signOut = () => {
   Raven.setUserContext(); // stop tracking uid in sentry
@@ -17,13 +13,10 @@ export const signOut = () => {
 /**
  * Signs a user in with Twitter and returns the user
  */
-export const signInWithTwitter = () => new Promise((resolve, reject) => {
+export const signInWithTwitter = () => {
   const twitter = new auth.TwitterAuthProvider();
-
-  return signIn(twitter).then(user => {
-    resolve(user);
-  });
-});
+  return signIn(twitter).then(result => result.user);
+};
 
 /**
  * Listen to authentication changes
