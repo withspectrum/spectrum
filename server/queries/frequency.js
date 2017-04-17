2130515b3fbb88bedb952f9eb255e995a402b8f5
Max <contact@mstoiber.com>
2017-04-17 14:26:14 +0200

    Implement story/subscriberConnections for frequency

--- server/queries/frequency.js
+++ server/queries/frequency.js
@@ -1,20 +1,53 @@
+// @flow
 /**
  * Frequency query resolvers
  */
 const { getFrequency } = require('../models/frequency');
-const { getStoryByFrequency } = require('../models/story');
+const { getStoriesByFrequency } = require('../models/story');
 const { getCommunity } = require('../models/community');
 const { getUsers } = require('../models/user');
+import paginate from '../utils/paginate-arrays';
+import type { PaginationOptions } from '../utils/paginate-arrays';
 
 module.exports = {
   Query: {
-    frequency: (_, { id }) => getFrequency(id),
+    frequency: (_: any, { id }: { id: String }) => getFrequency(id),
   },
   Frequency: {
-    stories(frequency) {
-      return getStoryByFrequency(frequency.id);
+    storyConnections: (
+      { id }: { id: String },
+      { first = 10, after }: PaginationOptions
+    ) =>
+      getStoriesByFrequency(id, { first, after }).then(([
+        stories,
+        lastStory,
+      ]) => ({
+        pageInfo: {
+          hasNextPage: stories.length > 0
+            ? lastStory.id === stories[stories.length - 1].id
+            : lastStory.id === after,
+        },
+        edges: stories.map(story => ({
+          cursor: story.id,
+          node: story,
+        })),
+      })),
+    community: ({ community }: { community: String }) =>
+      getCommunity(community),
+    subscriberConnections: (
+      { subscribers }: { subscribers: Array<string> },
+      { first = 10, after }: PaginationOptions
+    ) => {
+      const { list, hasMoreItems } = paginate(subscribers, { first, after });
+      return getUsers(list).then(users => ({
+        pageInfo: {
+          hasNextPage: hasMoreItems,
+        },
+        edges: users.map(user => ({
+          cursor: user.uid,
+          node: user,
+        })),
+      }));
     },
-    community: ({ community }) => getCommunity(community),
-    subscribers: ({ subscribers }) => getUsers(subscribers),
   },
 };
--- server/types/Frequency.js
+++ server/types/Frequency.js
@@ -1,4 +1,24 @@
 const Frequency = /* GraphQL */ `
+	type FrequencySubscribersConnection {
+		pageInfo: PageInfo!
+		edges: [FrequencySubscriberEdge!]
+	}
+
+	type FrequencySubscriberEdge {
+		cursor: String!
+		node: User!
+	}
+
+	type FrequencyStoriesConnection {
+		pageInfo: PageInfo!
+		edges: [FrequencyStoryEdge!]
+	}
+
+	type FrequencyStoryEdge {
+		cursor: String!
+		node: Story!
+	}
+
 	type Frequency {
 		id: ID!
 		createdAt: Date!
@@ -7,8 +27,8 @@ const Frequency = /* GraphQL */ `
 		description: String!
 		slug: String!
 		community: Community!
-		stories: [Story!]
-		subscribers: [User!]
+		storyConnections(first: Int = 10, after: String): FrequencyStoriesConnection!
+		subscriberConnections(first: Int = 10, after: String): FrequencySubscribersConnection!
 	}
 
 	extend type Query {
