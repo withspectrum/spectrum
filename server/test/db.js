666a876d09c66488baab5f68ffb3e432d7752dcc
Max <contact@mstoiber.com>
2017-04-23 13:00:50 +0200

    Automate database creation

--- server/test/db.js
+++ server/test/db.js
@@ -20,34 +20,29 @@ export const data = {
 };
 
 export const setup = db => {
-  return Promise.all([
-    db.tableCreate('stories').run(),
-    db.tableCreate('frequencies').run(),
-    db.tableCreate('communities').run(),
-    db.tableCreate('messages').run(),
-    db.tableCreate('direct_messages').run(),
-    db.tableCreate('sessions').run(),
-    db.tableCreate('reactions').run(),
-    db.tableCreate('direct_message_groups').run(),
-    db.tableCreate('users', { primaryKey: 'uid' }).run(),
-  ])
-    .then(() => db.table('users').insert(data.users[0]).run())
+  return db
+    .dbCreate('testing')
+    .run()
+    .then(() =>
+      Promise.all([
+        db.tableCreate('stories').run(),
+        db.tableCreate('frequencies').run(),
+        db.tableCreate('communities').run(),
+        db.tableCreate('messages').run(),
+        db.tableCreate('direct_messages').run(),
+        db.tableCreate('sessions').run(),
+        db.tableCreate('reactions').run(),
+        db.tableCreate('direct_message_groups').run(),
+        db.tableCreate('users', { primaryKey: 'uid' }).run(),
+      ])
+    )
+    .then(result => db.table('users').insert(data.users[0]).run())
     .catch(err => {
       console.log(err);
     });
 };
 
 export const teardown = db =>
-  Promise.all([
-    db.tableDrop('stories').run(),
-    db.tableDrop('frequencies').run(),
-    db.tableDrop('communities').run(),
-    db.tableDrop('messages').run(),
-    db.tableDrop('direct_messages').run(),
-    db.tableDrop('sessions').run(),
-    db.tableDrop('reactions').run(),
-    db.tableDrop('direct_message_groups').run(),
-    db.tableDrop('users').run(),
-  ]).catch(err => {
+  db.dbDrop('testing').run().catch(err => {
     console.log(err);
   });
