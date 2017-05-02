5254ac5eba9279c1df4fa3f8771a1e3226242c7e
Brian Lovin <briandlovin@gmail.com>
2017-05-02 13:03:30 -0700

    Parse timestamps to properly sort and group bubbles

--- src/views/story/components/messages.js
+++ src/views/story/components/messages.js
@@ -1,12 +1,18 @@
 // @flow
 import React from 'react';
+import { sortAndGroupMessages } from '../../../helpers/messages';
 
-const Messages = ({ messages }) => (
-  <div>
-    {messages.map(({ node }) => (
-      <div key={node.id}>“{node.message.content}”</div>
-    ))}
-  </div>
-);
+const Messages = ({ messages }) => {
+  const sortedMessages = sortAndGroupMessages(messages);
+  console.log('back in the component', sortedMessages);
+  return (
+    <div>
+      {sortedMessages.map(message => {
+        console.log(message);
+        return <div key={message.id}>“{message.message.content}”</div>;
+      })}
+    </div>
+  );
+};
 
 export default Messages;
