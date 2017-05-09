210e6667bb4b8ae0adf3c620fa286f8360a720f0
Brian Lovin <briandlovin@gmail.com>
2017-05-09 12:36:59 -0700

    Don't export app any more

--- server/index.js
+++ server/index.js
@@ -129,8 +129,6 @@ const subscriptionsServer = new SubscriptionServer(
   }
 );
 
-module.exports = app;
-
 // Start database listeners
 listeners.start();
 console.log(`GraphQL server running at http://${HOST}:${PORT}`);
