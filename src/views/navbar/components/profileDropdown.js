c95e278dd4d23e384811ba43010d9f32e45b92cf
uberbryn <hi@bryn.io>
2017-05-30 00:49:04 -0700

    reverts to brian's dropdown

--- src/views/navbar/components/profileDropdown.js
+++ src/views/navbar/components/profileDropdown.js
@@ -2,27 +2,20 @@
 import React from 'react';
 import { logout } from '../../../actions/authentication';
 import Dropdown from '../../../components/dropdown';
-import { TextButton } from '../../../components/buttons';
-import ProfileMenu from '../../user/components/profileMenu';
-import {
-  DropdownFooter,
-  UserProfileDropdownList,
-  UserProfileDropdownListItem,
-} from '../style';
+import { UserProfileDropdownList, UserProfileDropdownListItem } from '../style';
 
 export const ProfileDropdown = props => {
   return (
     <Dropdown width={'160px'}>
       <UserProfileDropdownList>
-        <ProfileMenu {...props} />
-        {/* <UserProfileDropdownListItem
+        <UserProfileDropdownListItem
           onClick={() => props.history.push(`/users/${props.user.username}`)}
         >
           Settings
         </UserProfileDropdownListItem>
         <UserProfileDropdownListItem onClick={() => logout()}>
           Log Out
-        </UserProfileDropdownListItem> */}
+        </UserProfileDropdownListItem>
       </UserProfileDropdownList>
     </Dropdown>
   );
