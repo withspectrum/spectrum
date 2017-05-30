5fa506c5f3bf587b41c9efedde2be946cc11569f
uberbryn <hi@bryn.io>
2017-05-30 00:36:46 -0700

    updates readme with new paid team details. kills explore page temporarily.

--- src/views/navbar/components/profileDropdown.js
+++ src/views/navbar/components/profileDropdown.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { logout } from '../../../actions/authentication';
 import Dropdown from '../../../components/dropdown';
 import { TextButton } from '../../../components/buttons';
+import ProfileMenu from '../../user/components/profileMenu';
 import {
   DropdownFooter,
   UserProfileDropdownList,
@@ -11,16 +12,17 @@ import {
 
 export const ProfileDropdown = props => {
   return (
-    <Dropdown width={'240px'}>
+    <Dropdown width={'160px'}>
       <UserProfileDropdownList>
-        <UserProfileDropdownListItem
+        <ProfileMenu {...props} />
+        {/* <UserProfileDropdownListItem
           onClick={() => props.history.push(`/users/${props.user.username}`)}
         >
           Settings
         </UserProfileDropdownListItem>
         <UserProfileDropdownListItem onClick={() => logout()}>
           Log Out
-        </UserProfileDropdownListItem>
+        </UserProfileDropdownListItem> */}
       </UserProfileDropdownList>
     </Dropdown>
   );
