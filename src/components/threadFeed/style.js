eba4ce84fc8c6380ebf5f8daa16f22f9cd9b6156
Brian Lovin <briandlovin@gmail.com>
2017-06-01 16:01:40 -0700

    Cleaned up fetchmore interaction on thread view

---
+++ src/components/threadFeed/style.js
@@ -0,0 +1,27 @@
+// @flow
+// $FlowFixMe
+import styled from 'styled-components';
+import { OutlineButton } from '../buttons';
+
+export const FetchMoreButton = styled(OutlineButton)`
+  width: 100%;
+  padding: 16px 0;
+
+  @media (max-width: 768px) {
+    padding: 32px 0;
+    border-radius: 0;
+    background: #fff;
+    font-size: 16px;
+    font-weight: 600;
+    color: ${props => props.theme.brand.default};
+    border: none;
+    box-shadow: none;
+    border-top: 2px solid ${props => props.theme.border.default};
+
+    &:hover {
+      background: ${props => props.theme.bg.wash};
+      border-radius: 0;
+      box-shadow: none;
+    }
+  }
+`;
