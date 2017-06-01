0fba99a7093b6e0f0cf31fd23d372b512bcd042a
Brian Lovin <briandlovin@gmail.com>
2017-05-31 23:28:40 -0700

    Fixes reactions

--- src/views/thread/mutations.js
+++ src/views/thread/mutations.js
@@ -4,6 +4,7 @@ import { graphql, gql } from 'react-apollo';
 import {
   reactionInfoFragment,
 } from '../../api/fragments/reaction/reactionInfo';
+import { messageInfoFragment } from '../../api/fragments/message/messageInfo';
 
 /*
   Send an id and boolean value to set a thread to be locked or unlocked.
@@ -47,11 +48,11 @@ export const setThreadLockMutation = graphql(
 const TOGGLE_REACTION_MUTATION = gql`
   mutation toggleReaction($reaction: ReactionInput!) {
     toggleReaction(reaction: $reaction) {
-      ...reactionInfo
+      ...messageInfo
       __typename
     }
   }
-  ${reactionInfoFragment}
+  ${messageInfoFragment}
 `;
 const TOGGLE_REACTION_OPTIONS = {
   props: ({ ownProps, mutate }) => ({
@@ -63,82 +64,7 @@ const TOGGLE_REACTION_OPTIONS = {
         variables: {
           reaction,
         },
-        optimisticResponse: {
-          __typename: 'Mutation',
-          toggleReaction: {
-            id: Math.random() * -10000,
-            type: 'like',
-            user: {
-              id: ownProps.currentUser.id,
-              __typename: 'User',
-            },
-            __typename: 'Reaction',
-          },
-        },
-        // update: (store, { data: { toggleReaction }}) => {
-        //   const data = store.readQuery({ query: GET_THREAD_MESSAGES_QUERY })
-        //   console.log('updating', data)
-        // }
       }),
-    //   updateQueries: {
-    //     getThreadMessages: (prev, { mutationResult }) => {
-    //       const newReaction = mutationResult.data.toggleReaction;
-    //       console.log('in the reaction update', prev, newReaction)
-    //       return Object.assign({}, prev, {
-    //         ...prev,
-    //         thread: {
-    //           ...prev.thread,
-    //           messageConnection: {
-    //             ...prev.thread.messageConnection,
-    //             edges: prev.thread.messageConnection.edges.map(edge => {
-    //               // make sure we're modifying the correct message
-    //               if (edge.node.id !== reaction.message) return edge;
-    //
-    //               // if no reactions exist yet, add one immediately for the
-    //               // current user
-    //               if (edge.node.reactions.length === 0) {
-    //                 return {
-    //                   ...edge,
-    //                   node: {
-    //                     ...edge.node,
-    //                     reactions: [...edge.node.reactions, newReaction],
-    //                   },
-    //                 };
-    //               }
-    //
-    //               // if the current user has already reacted, remove their
-    //               // reaction from the array
-    //               if (
-    //                 edge.node.reactions.find(r => {
-    //                   return r.user.id === newReaction.user.id;
-    //                 })
-    //               ) {
-    //                 return {
-    //                   ...edge,
-    //                   node: {
-    //                     ...edge.node,
-    //                     reactions: edge.node.reactions.filter(r => {
-    //                       return r.user.id !== newReaction.user.id;
-    //                     }),
-    //                   },
-    //                 };
-    //               }
-    //
-    //               // otherwise add the reaction
-    //               return {
-    //                 ...edge,
-    //                 node: {
-    //                   ...edge.node,
-    //                   reactions: [...edge.node.reactions, newReaction],
-    //                 },
-    //               };
-    //             }),
-    //           },
-    //         },
-    //       });
-    //     },
-    //   },
-    // }),
   }),
 };
 export const toggleReactionMutation = graphql(
