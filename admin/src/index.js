6006050451123a593fbb814fc8c4e590a962772c
Brian Lovin <briandlovin@gmail.com>
2017-06-04 23:34:43 -0700

    Styling, add message counts, range without functions

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './components/theme';
 import { client } from './api';
 import App from './App';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </ApolloProvider>,
   document.getElementById('root')
 );
