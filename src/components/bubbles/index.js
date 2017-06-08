45164d94c40c4c771a9135057f498e15862a854c
uberbryn <hi@bryn.io>
2017-06-07 20:27:05 -0700

    fixes a problem where messages didn't load

--- src/components/bubbles/index.js
+++ src/components/bubbles/index.js
@@ -27,7 +27,7 @@ export const Bubble = (props: BubbleProps) => {
 
   return (
     <TextBubble me={me} pending={pending}>
-      {type === 'thread' && message.body}
+      {message.body}
     </TextBubble>
   );
 };
