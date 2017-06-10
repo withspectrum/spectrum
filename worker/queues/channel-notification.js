245f0fcc2170d795b99ab27754adf7cba7f18b6a
Brian Lovin <briandlovin@gmail.com>
2017-06-09 18:02:49 -0700

    Clean up new channel notification

--- worker/queues/channel-notification.js
+++ worker/queues/channel-notification.js
@@ -20,194 +20,102 @@ export default () =>
     const incomingChannel = job.data.channel;
     const currentUserId = job.data.userId;
 
-    console.log('\n1', job);
-    console.log('\n2', job.data.channel);
-    console.log('\n3', job.data.userId);
-
-    // 10 minutes buffer to determine whether or not to create a new
-    // notification or update an old one
-    const TIME_BUFFER = 600000;
+    // if the channel is the default channel being created at community creation time, don't create a notification
+    if (incomingChannel.slug === 'general') {
+      return;
+    }
 
     /*
-      1. Determine if a notification for this event type and context already
-         exists. E.g. a bunch of people send messages in a thread in a short
-         period of time - it will know the 'event_type' is 'message_created'
-         and the context's id will be the same 'threadId'.
-         a. If a record is found, make sure it is within a certain reasonable
-            period of time since the notification's modifiedAt date. If it is,
-            proceed to step 2. If it is not, proceed to step 3.
-      2. If an existing notification is found that matches the event type and
-         context id and is within a certain time range of the previous upddatedAt
-         date, we can simply modify this original notification, recalcuate
-         the recipients (in case their notif preferences have changed), and
-         then update those recipients records in the usersNotifications table
-         with a new isSeen and isRead state
-      3. If an existing notification record is found but isn't recent enough, or
-         if no record was found, we will create a new notification record in the
-         db, calculate the recipients based on the notification context, and then
-         create a usersNotifications record for each recipient
+      These promises are used to create or modify a notification. The order is:
+      - actor
+      - context
+      - entity
     */
+    const promises = [
+      //get the user who created the channel (probably won't be used in the UI)
+      fetchPayload('USER', currentUserId),
+      // get the community where the channel was created
+      fetchPayload('COMMUNITY', incomingChannel.communityId),
+      // create an entity payload of the channel that was created
+      createPayload('CHANNEL', incomingChannel),
+    ];
 
-    // 1. Determine if a notification exists with the same event type, context
-    //    id, and falls within the declared time buffer
     return checkForExistingNotification(
       'CHANNEL_CREATED',
-      incomingChannel.communityId,
-      TIME_BUFFER
+      incomingChannel.communityId
     )
       .then(notification => {
-        console.log('\n7', notification);
-        /*
-          Regardless of if we find an existing notification, we will always
-          need to either update or create a record with:
-          - the actor of this event
-          - the context of this event
-          - the event entity itself (in this case a new message)
+        if (notification) {
+          return Promise.all([notification, ...promises])
+            .then(([notification, actor, context, entity]) => {
+              // actors should always be distinct to make client side rendering easier
+              const distinctActors = getDistinctActors([
+                ...notification.actors,
+                actor,
+              ]);
 
-          At this point we also need to make sure we set the context as either a story
-          thread or as a direct message thread
-        */
+              // create a new notification
+              const newNotification = Object.assign({}, notification, {
+                actors: [...distinctActors],
+                context,
+                entities: [...notification.entities, entity],
+              });
 
-        console.log('\nactor', currentUserId);
-        console.log('\nentity', incomingChannel);
-        const promises = [
-          fetchPayload('USER', currentUserId),
-          fetchPayload('COMMUNITY', incomingChannel.communityId),
-          createPayload('CHANNEL', incomingChannel),
-        ];
+              return updateNotification(newNotification);
+            })
+            .then(notification => {
+              // get the recipients of the notification by finding all members in the community that have notifications turned on
+              const recipients = getMembersInCommunity(notification.context.id);
 
-        // 2. If a notification was found that met all of our criteria, we will
-        //    construct a new notification object to update the record in the db
-        if (notification && notification !== undefined) {
-          console.log('\n8');
-          return Promise.all([notification, ...promises]).then(([
-            notification,
-            actor,
-            context,
-            entity,
-          ]) => {
-            console.log('\n8-1', actor);
-            console.log('\n8-2', context);
-            console.log('\n8-3', entity);
+              return Promise.all([notification, recipients]);
+            })
+            .then(([notification, recipients]) => {
+              // filter out the user who created the channel
+              let filteredRecipients = recipients.filter(
+                recipient => recipient !== currentUserId
+              );
 
-            // don't duplicate actors if one user sends a bunch of messages
-            // in a row
-            const distinctActors = getDistinctActors([
-              ...notification.actors,
-              actor,
-            ]);
-
-            // create the new notification shape, pushing the current
-            // events actor, context, and entity into each respective field
-            const newNotification = Object.assign({}, notification, {
-              actors: [...distinctActors],
-              context,
-              entities: [...notification.entities, entity],
+              // for each person who should receie an updated notification, mark their notification as unseen and unread
+              return Promise.all(
+                filteredRecipients.map(recipient =>
+                  markUsersNotificationsAsNew(notification.id, recipient)
+                )
+              );
             });
-
-            console.log('\n8-5', newNotification);
-
-            // update the notification in the db
-            return updateNotification(newNotification)
-              .then(notification => {
-                console.log('\n8-6', notification);
-
-                // with the updated notification, calculate the recipients of
-                // this notification. In this case it is thread participants
-                // or if the message was in a dm thread it's all the members
-                // of that dm thread
-                const recipients = getMembersInCommunity(
-                  notification.context.id
-                );
-
-                return Promise.all([notification, recipients]);
-              })
-              .then(([notification, recipients]) => {
-                // ensure we have valid recipients
-                console.log('\n8-7', recipients);
-                if (recipients && recipients.length > 0) {
-                  if (recipients && recipients.length > 0) {
-                    // don't update the notification for the current user who
-                    // triggered the event
-                    let filteredRecipients = recipients.filter(
-                      recipient => recipient !== currentUserId
-                    );
-                    // for each recipient, update the record in the
-                    // usersNotifications table to have isRead and isSeen be false
-                    return Promise.all(
-                      filteredRecipients.map(recipient =>
-                        markUsersNotificationsAsNew(
-                          notification.id,
-                          recipient
-                        ).then(data => {
-                          console.log('\n8-8', data);
-                          return data;
-                        })
-                      )
-                    );
-                  }
-                }
-              });
-          });
         } else {
-          console.log('\n9');
-          // 2. If no notification was found that met all of our criteria, we will
-          //    create a new notification record in the db
-          return Promise.all(promises).then(([actor, context, entity]) => {
-            console.log('\n9-1', actor);
-            console.log('\n9-2', context);
-            console.log('\n9-3', entity);
+          // if no notification was found that matches our bundling criteria, create a new notification
+          return Promise.all([...promises])
+            .then(([actor, context, entity]) => {
+              // create the notification record
+              const notification = {
+                actors: [actor],
+                event: 'CHANNEL_CREATED',
+                context,
+                entities: [entity],
+              };
 
-            // create the notification record
-            const notification = {
-              actors: [actor],
-              event: 'CHANNEL_CREATED',
-              context,
-              entities: [entity],
-            };
+              return storeNotification(notification);
+            })
+            .then(notification => {
+              // get the recipients of the notification by finding all members in the community that have notifications turned on
+              const recipients = getMembersInCommunity(notification.context.id);
 
-            if (incomingChannel.slug === 'general') {
-              // if the channel is the default channel being created at community
-              // creation time, don't create a notification
-              return;
-            }
+              return Promise.all([notification, recipients]);
+            })
+            .then(([notification, recipients]) => {
+              // filter out the user who created the channel
+              let filteredRecipients = recipients.filter(
+                recipient => recipient !== currentUserId
+              );
 
-            return storeNotification(notification)
-              .then(notification => {
-                console.log('\n9-4', notification);
-                // with the new notification, calculate the recipients of
-                // this notification. In this case it is thread participants
-                const recipients = getMembersInCommunity(
-                  notification.context.id
-                );
-
-                return Promise.all([notification, recipients]);
-              })
-              .then(([notification, recipients]) => {
-                console.log('\n9-5', recipients);
-                if (recipients && recipients.length > 0) {
-                  // don't update the notification for the current user who
-                  // triggered the event
-                  let filteredRecipients = recipients.filter(
-                    recipient => recipient !== currentUserId
-                  );
-                  // for each recipient, update the record in the
-                  // usersNotifications table to have isRead and isSeen be false
-                  return Promise.all(
-                    filteredRecipients.map(recipient =>
-                      storeUsersNotifications(
-                        notification.id,
-                        recipient
-                      ).then(data => {
-                        console.log('\n9-6', data);
-                        return data;
-                      })
-                    )
-                  );
-                }
-              });
-          });
+              // for each recipient, create a notification
+              return Promise.all(
+                filteredRecipients.map(recipient =>
+                  storeUsersNotifications(notification.id, recipient)
+                )
+              );
+            });
         }
       })
-      .catch(err => console.log('\nerror: ', err));
+      .catch(err => new Error(err));
   });
--- worker/queues/message-notification.js
+++ worker/queues/message-notification.js
@@ -34,7 +34,7 @@ export default () =>
       - entity
     */
     const promises = [
-      //get the user who left the meessage
+      //get the user who left the message
       fetchPayload('USER', incomingMessage.senderId),
       // get the thread the message was left in - could be a dm or story depending on the contextType
       fetchPayload(contextType, incomingMessage.threadId),
@@ -50,90 +50,84 @@ export default () =>
       .then(notification => {
         if (notification) {
           // if an existing notification exists, update it with the newest actor + entities
-          return Promise.all([notification, ...promises]).then(([
-            notification,
-            actor,
-            context,
-            entity,
-          ]) => {
-            // actors should always be distinct to make client side rendering easier
-            const distinctActors = getDistinctActors([
-              ...notification.actors,
-              actor,
-            ]);
+          return Promise.all([notification, ...promises])
+            .then(([notification, actor, context, entity]) => {
+              // actors should always be distinct to make client side rendering easier
+              const distinctActors = getDistinctActors([
+                ...notification.actors,
+                actor,
+              ]);
 
-            // create a new notification
-            const newNotification = Object.assign({}, notification, {
-              actors: [...distinctActors],
-              context,
-              entities: [...notification.entities, entity],
-            });
-
-            return updateNotification(newNotification)
-              .then(notification => {
-                /* 
-                if the message was posted in a story, get all users who have notifications turned on for that story. Otherwise the message was posted in a direct message thread, in which case we will notify all participants in the thread
-                */
-                const recipients = incomingMessage.threadType ===
-                  'directMessageThread'
-                  ? getDirectMessageThreadMembers(notification.context.id)
-                  : getThreadNotificationUsers(notification.context.id);
-
-                return Promise.all([notification, recipients]);
-              })
-              .then(([notification, recipients]) => {
-                // filter out the user who sent the message, as they should not recieve a notification for their own messages
-                const filteredRecipients = recipients.filter(
-                  recipient => recipient.userId !== currentUserId
-                );
-
-                // for each person who should receie an updated notification, mark their notification as unseen and unread
-                return Promise.all(
-                  filteredRecipients.map(recipient =>
-                    markUsersNotificationsAsNew(
-                      notification.id,
-                      recipient.userId
-                    )
-                  )
-                );
+              // create a new notification
+              const newNotification = Object.assign({}, notification, {
+                actors: [...distinctActors],
+                context,
+                entities: [...notification.entities, entity],
               });
-          });
+
+              return updateNotification(newNotification);
+            })
+            .then(notification => {
+              /* 
+            if the message was posted in a story, get all users who have notifications turned on for that story. Otherwise the message was posted in a direct message thread, in which case we will notify all participants in the thread
+            */
+              const recipients = incomingMessage.threadType ===
+                'directMessageThread'
+                ? getDirectMessageThreadMembers(notification.context.id)
+                : getThreadNotificationUsers(notification.context.id);
+
+              return Promise.all([notification, recipients]);
+            })
+            .then(([notification, recipients]) => {
+              // filter out the user who sent the message, as they should not recieve a notification for their own messages
+              const filteredRecipients = recipients.filter(
+                recipient => recipient.userId !== currentUserId
+              );
+
+              // for each person who should receie an updated notification, mark their notification as unseen and unread
+              return Promise.all(
+                filteredRecipients.map(recipient =>
+                  markUsersNotificationsAsNew(notification.id, recipient.userId)
+                )
+              );
+            });
         } else {
           // if no notification was found that matches our bundling criteria, create a new notification
-          return Promise.all([...promises]).then(([actor, context, entity]) => {
-            // create the notification record
-            const notification = {
-              actors: [actor],
-              event: 'MESSAGE_CREATED',
-              context,
-              entities: [entity],
-            };
+          return Promise.all([...promises])
+            .then(([actor, context, entity]) => {
+              // create the notification record
+              const notification = {
+                actors: [actor],
+                event: 'MESSAGE_CREATED',
+                context,
+                entities: [entity],
+              };
 
-            return storeNotification(notification)
-              .then(notification => {
-                /* 
-                if the message was posted in a story, get all users who have notifications turned on for that story. Otherwise the message was posted in a direct message thread, in which case we will notify all participants in the thread
-                */
-                const recipients = incomingMessage.threadType ===
-                  'directMessageThread'
-                  ? getDirectMessageThreadMembers(notification.context.id)
-                  : getThreadNotificationUsers(notification.context.id);
+              return storeNotification(notification);
+            })
+            .then(notification => {
+              /* 
+            if the message was posted in a story, get all users who have notifications turned on for that story. Otherwise the message was posted in a direct message thread, in which case we will notify all participants in the thread
+            */
+              const recipients = incomingMessage.threadType ===
+                'directMessageThread'
+                ? getDirectMessageThreadMembers(notification.context.id)
+                : getThreadNotificationUsers(notification.context.id);
 
-                return Promise.all([notification, recipients]);
-              })
-              .then(([notification, recipients]) => {
-                // filter out the user who sent the message, as they should not recieve a notification for their own messages
-                const filteredRecipients = recipients.filter(
-                  recipient => recipient.userId !== currentUserId
-                );
+              return Promise.all([notification, recipients]);
+            })
+            .then(([notification, recipients]) => {
+              // filter out the user who sent the message, as they should not recieve a notification for their own messages
+              const filteredRecipients = recipients.filter(
+                recipient => recipient.userId !== currentUserId
+              );
 
-                return Promise.all(
-                  filteredRecipients.map(recipient =>
-                    storeUsersNotifications(notification.id, recipient.userId)
-                  )
-                );
-              });
-          });
+              return Promise.all(
+                filteredRecipients.map(recipient =>
+                  storeUsersNotifications(notification.id, recipient.userId)
+                )
+              );
+            });
         }
       })
       .catch(err => new Error(err));
