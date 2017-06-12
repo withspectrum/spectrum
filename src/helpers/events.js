8ba2506fcf3b6af0e08c42e4f71b0fae5afed8cc
Max <contact@mstoiber.com>
2017-06-12 09:10:36 +0200

    Remove release tracking

--- src/helpers/events.js
+++ src/helpers/events.js
@@ -2,7 +2,6 @@ import Raven from 'raven-js';
 Raven.config('https://3bd8523edd5d43d7998f9b85562d6924@sentry.io/154812', {
   whitelistUrls: [/spectrum\.chat/, /www\.spectrum\.chat/],
   environment: process.env.NODE_ENV,
-  release: process.env.REACT_APP_CURRENT_GIT_COMMIT_HASH,
 }).install();
 
 const ga = window.ga;
