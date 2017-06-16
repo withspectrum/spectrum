6f5b31bddab8820bf7f54d87f43a645b58d5bfa5
Max <contact@mstoiber.com>
2017-06-16 10:44:25 +0200

    Show favicon dot if there are unread notifications
    Closes #1093

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -3,17 +3,26 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 type Props = {
-  title: string,
-  description: string,
+  title?: string,
+  description?: string,
+  showUnreadFavicon?: boolean,
 };
 
-export default ({ title, description }: Props) => (
-  <Helmet>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-    <meta name="og:title" content={title} />
-    <meta name="og:description" content={description} />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-  </Helmet>
-);
+export default ({ title, description, showUnreadFavicon }: Props) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {showUnreadFavicon &&
+        <link
+          rel="shortcut icon"
+          id="dynamic-favicon"
+          href={`${process.env.PUBLIC_URL}/img/favicon_unread.ico`}
+        />}
+    </Helmet>
+  );
+};
