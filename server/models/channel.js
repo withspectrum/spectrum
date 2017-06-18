7aa0321af09cc2d60898212722f3292985b4ea9d
Brian Lovin <briandlovin@gmail.com>
2017-06-18 16:04:54 -0700

    Fix channel fetching logic (#1116)
    * Fix channel fetching logic
    
    * add parens
    
    * Fix ordering

--- server/models/channel.js
+++ server/models/channel.js
@@ -32,9 +32,7 @@ const getPublicChannelsByCommunity = (
 };
 
 /*
-  If a user is viewing a community, they should only see threads from the
-  channels they are members of in order to avoid accidentally seeing threads
-  from a private channel.
+  If a user is viewing a community, they should see threads from all public channels as well as from private channels they are a member of.
 
   This function returns an array of objects with the field 'id' that corresponds
   to a channelId. This array of IDs will be passed into a threads method which
@@ -51,10 +49,13 @@ const getChannelsByUserAndCommunity = (
       .eqJoin('id', db.table('usersChannels'), { index: 'channelId' })
       // get channels where the user is a member OR the channel is public
       .filter(row =>
-        row('right')('isMember')
-          .eq(true)
-          .and(row('right')('userId').eq(userId))
-          .or(row('left')('isPrivate').eq(false))
+        row('left')('isPrivate')
+          .eq(false)
+          .or(
+            row('right')('isMember')
+              .eq(true)
+              .and(row('right')('userId').eq(userId))
+          )
       )
       .without({ right: 'id' })
       .zip()
