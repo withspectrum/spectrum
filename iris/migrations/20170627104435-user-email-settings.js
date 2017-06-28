ce3fa4ca3d970cb0799a5734890c51be56f0c3b2
Max Stoiber <contact@mxstbr.com>
2017-06-28 09:21:03 +0200

    Fix migration script

--- iris/migrations/20170627104435-user-email-settings.js
+++ iris/migrations/20170627104435-user-email-settings.js
@@ -1,35 +1,23 @@
 exports.up = function(r, conn) {
-  return (
-    Promise.all([
-      r.tableCreate('usersSettings').run(conn).catch(err => {
-        console.log(err);
-        throw err;
-      }),
-    ])
+  return Promise.all([
+    r
+      .tableCreate('usersSettings')
+      .run(conn)
       // Create secondary indexes
       .then(() =>
-        Promise.all([
-          // index user by username
-          r
-            .table('usersSettings')
-            .indexCreate('userId', r.row('userId'))
-            .run(conn)
-            .catch(err => {
-              console.log(err);
-              throw err;
-            }),
-        ])
+        // index user by username
+        r
+          .table('usersSettings')
+          .indexCreate('userId', r.row('userId'))
+          .run(conn)
       )
       .catch(err => {
         console.log(err);
         throw err;
-      })
-  );
+      }),
+  ]);
 };
 
 exports.down = function(r, conn) {
-  return Promise.all([r.tableDrop('usersSettings').run(conn)]).catch(err => {
-    console.log(err);
-    throw err;
-  });
+  return r.tableDrop('usersSettings').run(conn);
 };
