9005f0e9ab77b6e641af3b35610e6746bb759a7e
Max Stoiber <contact@mxstbr.com>
2017-06-30 17:39:09 +0200

    Finish migration script

--- iris/migrations/20170627104435-user-email-settings.js
+++ iris/migrations/20170627104435-user-email-settings.js
@@ -11,6 +11,33 @@ exports.up = function(r, conn) {
           .indexCreate('userId', r.row('userId'))
           .run(conn)
       )
+      .then(() =>
+        Promise.all([
+          // Get all user ids while we wait for the index to finish creating
+          r.table('users').withFields('id').map(user => user('id')).run(conn),
+          r.table('usersSettings').indexWait('userId').run(conn),
+        ])
+      )
+      .then(([userIds]) => userIds.toArray())
+      .then(userIds =>
+        Promise.all(
+          userIds.map(id =>
+            r
+              .table('usersSettings')
+              .insert({
+                userId: id,
+                notifications: {
+                  types: {
+                    newMessageInThreads: {
+                      email: true,
+                    },
+                  },
+                },
+              })
+              .run(conn)
+          )
+        )
+      )
       .catch(err => {
         console.log(err);
         throw err;
