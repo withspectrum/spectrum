46130ab88e8a56e46fead28aac199bf10f6ceb5b
Max Stoiber <contact@mxstbr.com>
2017-07-01 21:38:36 +0200

    Stop hacking around marksy, use string-replace-to-array instead

--- src/components/bubbles/index.js
+++ src/components/bubbles/index.js
@@ -1,39 +1,15 @@
 //@flow
 import React from 'react';
-import marksy from 'marksy';
+import replace from 'string-replace-to-array';
 import { TextBubble, Emoji, ImageBubble } from './style';
 
-const markdownToReact = marksy({
-  createElement: React.createElement,
-  // Override all elements to render paragraphs except for links
-  elements: {
-    a: ({ href, title, target, children }) => (
-      <a href={href} title={title} target="_blank" children={children} />
-    ),
-    h1: ({ id, children }) => <span># {children}</span>,
-    h2: ({ id, children }) => <span>## {children}</span>,
-    h3: ({ id, children }) => <span>### {children}</span>,
-    h4: ({ id, children }) => <span>#### {children}</span>,
-    blockquote: ({ children }) => <span>&gt; {children}</span>,
-    hr: () => null,
-    ol: ({ children }) => <span>{children}</span>,
-    ul: ({ children }) => <span>{children}</span>,
-    p: ({ children }) => <span>{children}</span>,
-    table: ({ children }) => <span>{children}</span>,
-    thead: ({ children }) => <span>{children}</span>,
-    tbody: ({ children }) => <span>{children}</span>,
-    tr: ({ children }) => <span>{children}</span>,
-    th: ({ children }) => <span>{children}</span>,
-    td: ({ children }) => <span>{children}</span>,
-    strong: ({ children }) => <span>**{children}**</span>,
-    em: ({ children }) => <span>_{children}_</span>,
-    br: () => null,
-    del: ({ children }) => <span>{children}</span>,
-    img: ({ src, alt }) => <span><a href={src} alt={alt}>{src}</a></span>,
-    code: ({ language, code }) => <span>{code}</span>,
-    codespan: ({ children }) => <span>{children}</span>,
-  },
-});
+const MARKDOWN_LINK = /(?:\[(.*?)\]\((.*?)\))/g;
+
+const renderMarkdownLinks = text => {
+  return replace(text, MARKDOWN_LINK, (fullLink, text, url) => (
+    <a href={url}>{text}</a>
+  ));
+};
 
 type BubbleProps = {
   me: boolean,
@@ -46,11 +22,9 @@ type BubbleProps = {
 export const Bubble = (props: BubbleProps) => {
   const { me, message } = props;
 
-  const { tree } = markdownToReact(message.body);
-
   return (
     <TextBubble me={me}>
-      {tree}
+      {renderMarkdownLinks(message.body)}
     </TextBubble>
   );
 };
