5d6a7fde3b510187058f287a5a0c1d3854d686bc
Max Stoiber <contact@mxstbr.com>
2017-07-01 10:22:39 +0200

    Linkinfy markdown messages on the frontend

--- src/components/bubbles/index.js
+++ src/components/bubbles/index.js
@@ -1,8 +1,39 @@
 //@flow
 import React from 'react';
-//$FlowFixMe
-// import * as Autolinker from 'autolinker';
+import marksy from 'marksy';
 import { TextBubble, Emoji, ImageBubble } from './style';
+
+const markdownToReact = marksy({
+  createElement: React.createElement,
+  // Override all elements to render paragraphs except for links
+  elements: {
+    a: ({ href, title, target, children }) => (
+      <a href={href} title={title} target="_blank" children={children} />
+    ),
+    h1: ({ id, children }) => <span># {children}</span>,
+    h2: ({ id, children }) => <span>## {children}</span>,
+    h3: ({ id, children }) => <span>### {children}</span>,
+    h4: ({ id, children }) => <span>#### {children}</span>,
+    blockquote: ({ children }) => <span>&gt; {children}</span>,
+    hr: () => null,
+    ol: ({ children }) => <span>{children}</span>,
+    ul: ({ children }) => <span>{children}</span>,
+    p: ({ children }) => <span>{children}</span>,
+    table: ({ children }) => <span>{children}</span>,
+    thead: ({ children }) => <span>{children}</span>,
+    tbody: ({ children }) => <span>{children}</span>,
+    tr: ({ children }) => <span>{children}</span>,
+    th: ({ children }) => <span>{children}</span>,
+    td: ({ children }) => <span>{children}</span>,
+    strong: ({ children }) => <span>**{children}**</span>,
+    em: ({ children }) => <span>_{children}_</span>,
+    br: () => null,
+    del: ({ children }) => <span>{children}</span>,
+    img: ({ src, alt }) => <span><a href={src} alt={alt}>{src}</a></span>,
+    code: ({ language, code }) => <span>{code}</span>,
+    codespan: ({ children }) => <span>{children}</span>,
+  },
+});
 
 type BubbleProps = {
   me: boolean,
@@ -15,18 +46,11 @@ type BubbleProps = {
 export const Bubble = (props: BubbleProps) => {
   const { me, message } = props;
 
-  // const formatMessageForLinks = (body: string): string => {
-  //   if (!body) {
-  //     return '';
-  //   } else {
-  //     const linkedMessage = Autolinker.link(body);
-  //     return linkedMessage;
-  //   }
-  // };
+  const { tree } = markdownToReact(message.body);
 
   return (
     <TextBubble me={me}>
-      {message.body}
+      {tree}
     </TextBubble>
   );
 };
