ca4fbf5754ae1405d66396b1588a231d8fe42147
Max Stoiber <contact@mxstbr.com>
2017-07-02 22:38:07 +0200

    Hotfix images (#1186)
    * Add production config to migrations
    
    * HOTFIX: Broken images after linkify migration

---
+++ iris/migrations/20170702194221-fix-images.js
@@ -0,0 +1,37 @@
+'use strict';
+const MARKDOWN_LINK = /(?:\[(.*?)\]\((.*?)\))/g;
+
+exports.up = function(r, conn) {
+  return (
+    // The last migration fucked images up in production
+    // so this one fixes them again 😅
+    r
+      .table('messages')
+      .filter({ messageType: 'media' })
+      .withFields('id', 'content')
+      .run(conn)
+      .then(cursor => cursor.toArray())
+      .then(messages =>
+        Promise.all(
+          messages.map(message =>
+            r
+              .table('messages')
+              .get(message.id)
+              .update({
+                content: {
+                  body: MARKDOWN_LINK.test(message.content.body)
+                    ? message.content.body.replace(MARKDOWN_LINK, '$2')
+                    : message.content.body,
+                },
+              })
+              .run(conn)
+          )
+        )
+      )
+  );
+};
+
+exports.down = function(r, conn) {
+  // Can't really undo just this change so we don't bother
+  return Promise.resolve();
+};
