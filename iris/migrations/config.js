474d40e87810cfa13b74afa65c05b97f7c8f36ff
Max Stoiber <contact@mxstbr.com>
2017-07-02 21:39:02 +0200

    Add production config to migrations

--- iris/migrations/config.js
+++ iris/migrations/config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const fs = require('fs');
 
-module.exports = {
+const DEFAULT_CONFIG = {
   driver: 'rethinkdbdash',
   db: 'spectrum',
   host: 'localhost',
   port: 28015,
   migrationsDirectory: 'iris/migrations',
 };
+
+const RUN_IN_PROD = !!process.env.COMPOSE_RETHINKDB_PASSWORD;
+
+if (RUN_IN_PROD && process.argv[4] === 'down') {
+  throw new Error('Do not drop the production database!!!!!');
+  process.exit(1);
+}
+
+module.exports = !RUN_IN_PROD
+  ? DEFAULT_CONFIG
+  : Object.assign({}, DEFAULT_CONFIG, {
+      password: process.env.COMPOSE_RETHINKDB_PASSWORD,
+      host: process.env.COMPOSE_RETHINKDB_URL,
+      port: process.env.COMPOSE_RETHINKDB_PORT,
+      ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, '../../cacert')),
+      },
+    });
