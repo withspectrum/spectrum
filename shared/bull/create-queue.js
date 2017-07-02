3ca0a723f66c4cc522c22df94616d9e89116d3c6
Max Stoiber <contact@mxstbr.com>
2017-07-02 20:10:57 +0200

    Log stalled jobs to Sentry in production (#1183)

--- shared/bull/create-queue.js
+++ shared/bull/create-queue.js
@@ -1,7 +1,15 @@
 // @flow
-var Queue = require('bull');
+const Queue = require('bull');
+const Raven = require('raven');
 
-var redis = process.env.NODE_ENV === 'production'
+Raven.config(
+  'https://3bd8523edd5d43d7998f9b85562d6924:d391ea04b0dc45b28610e7fad735b0d0@sentry.io/154812',
+  {
+    environment: process.env.NODE_ENV,
+  }
+).install();
+
+const redis = process.env.NODE_ENV === 'production'
   ? {
       port: process.env.COMPOSE_REDIS_PORT,
       host: process.env.COMPOSE_REDIS_URL,
@@ -13,6 +21,16 @@ var redis = process.env.NODE_ENV === 'production'
 const options = redis && { redis: redis };
 
 function createQueue(name /*: string */) {
+  const queue = new Queue(name, options);
+  queue.on('stalled', job => {
+    const message = `Job#${job.id} stalled, processing again.`;
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(message);
+      return;
+    }
+    // In production log stalled job to Sentry
+    Raven.captureException(new Error(message));
+  });
   return new Queue(name, options);
 }
 
