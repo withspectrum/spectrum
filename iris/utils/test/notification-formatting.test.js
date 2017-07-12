d8c6fb949f2ee610a1136cdcf4ae9a0a7bafd661
Max Stoiber <contact@mxstbr.com>
2017-07-12 15:02:48 -0700

    Add tests for notification formatting

---
+++ iris/utils/test/notification-formatting.test.js
@@ -0,0 +1,52 @@
+// @flow
+import formatNotification from '../notification-formatting';
+import CHANNEL_CREATED_FIXTURE from './fixtures/CHANNEL_CREATED.json';
+import COMMUNITY_INVITE_FIXTURE from './fixtures/COMMUNITY_INVITE.json';
+import MESSAGE_CREATED_FIXTURE from './fixtures/MESSAGE_CREATED.json';
+import REACTION_CREATED_FIXTURE from './fixtures/REACTION_CREATED.json';
+import THREAD_CREATED_FIXTURE from './fixtures/THREAD_CREATED.json';
+import USER_JOINED_COMMUNITY_FIXTURE from './fixtures/USER_JOINED_COMMUNITY.json';
+
+const USER_ID = 'gVk5mYwccUOEKiN5vtOouqroGKo1';
+
+it('should format a channel creation notification', () => {
+  let result = formatNotification(CHANNEL_CREATED_FIXTURE, USER_ID);
+  expect(result.raw).toBeDefined();
+  delete result.raw; // Don't need raw data
+  expect(result).toMatchSnapshot();
+});
+
+it('should format a community invite notification', () => {
+  let result = formatNotification(COMMUNITY_INVITE_FIXTURE, USER_ID);
+  expect(result.raw).toBeDefined();
+  delete result.raw; // Don't need raw data
+  expect(result).toMatchSnapshot();
+});
+
+it('should format a message creation notification', () => {
+  let result = formatNotification(MESSAGE_CREATED_FIXTURE, USER_ID);
+  expect(result.raw).toBeDefined();
+  delete result.raw; // Don't need raw data
+  expect(result).toMatchSnapshot();
+});
+
+it('should format a reaction creation notification', () => {
+  let result = formatNotification(REACTION_CREATED_FIXTURE, USER_ID);
+  expect(result.raw).toBeDefined();
+  delete result.raw; // Don't need raw data
+  expect(result).toMatchSnapshot();
+});
+
+it('should format a thread creation notification', () => {
+  let result = formatNotification(THREAD_CREATED_FIXTURE, USER_ID);
+  expect(result.raw).toBeDefined();
+  delete result.raw; // Don't need raw data
+  expect(result).toMatchSnapshot();
+});
+
+it('should format a user joined community notification', () => {
+  let result = formatNotification(USER_JOINED_COMMUNITY_FIXTURE, USER_ID);
+  expect(result.raw).toBeDefined();
+  delete result.raw; // Don't need raw data
+  expect(result).toMatchSnapshot();
+});
--- iris/utils/web-push.js
+++ iris/utils/web-push.js
@@ -1,6 +1,7 @@
 import webPush from 'web-push';
 const debug = require('debug')('iris:utils:web-push');
 import { getSubscriptions } from '../models/web-push-subscription';
+import formatNotification from './notification-formatting';
 
 try {
   webPush.setVapidDetails(
@@ -31,7 +32,7 @@ export const sendWebPushNotification = (subscription, payload, options) => {
 export const sendNotificationAsWebPush = notification => {
   debug('send notification as web push notification');
   // Don't sent push notifications for channel creation
-  if (notification.event === 'CHANNEL_CREATED') return;
+  // if (notification.event === 'CHANNEL_CREATED') return;
   return getSubscriptions(notification.userId)
     .then(subscriptions => {
       if (!subscriptions || subscriptions.length === 0) {
@@ -55,148 +56,4 @@ export const sendNotificationAsWebPush = notification => {
         console.log(err);
       }
     });
-};
-
-import {
-  parseActors,
-  parseEvent,
-  parseNotificationDate,
-  parseContext,
-  getMessages,
-  sortThreads,
-  sentencify,
-} from './notification-formatting';
-
-const formatNotification = (notification, currentUserId) => {
-  switch (notification.event) {
-    case 'MESSAGE_CREATED': {
-      const actors = parseActors(notification.actors, { id: currentUserId });
-      const event = parseEvent(notification.event);
-      const context = parseContext(notification.context, { id: currentUserId });
-      const date = parseNotificationDate(notification.modifiedAt);
-      const messages = getMessages(notification.entities, {
-        id: currentUserId,
-      });
-
-      return {
-        data: {
-          href: `/thread/${notification.context.id}`,
-        },
-        title: `${actors.asString} ${event} ${context.asString}`,
-        body: sentencify(
-          messages.map(({ payload }) => `"${payload.content.body}"`)
-        ),
-        raw: {
-          actors,
-          event,
-          context,
-          entities: messages,
-          date,
-        },
-      };
-    }
-    case 'REACTION_CREATED': {
-      const actors = parseActors(notification.actors, { id: currentUserId });
-      const event = parseEvent(notification.event);
-      const date = parseNotificationDate(notification.modifiedAt);
-      const context = parseContext(notification.context);
-      const message = JSON.parse(notification.context.payload);
-
-      return {
-        data: {
-          href: `/thread/${context.asObject.payload.threadId}`,
-        },
-        title: `${actors.asString} ${event} ${context.asString}`,
-        body: message.content.body,
-        raw: {
-          actors,
-          event,
-          context,
-          entity: message,
-          date,
-        },
-      };
-    }
-    case 'CHANNEL_CREATED': {
-      const date = parseNotificationDate(notification.modifiedAt);
-      const context = parseContext(notification.context);
-      const entities = notification.entities.map(entity => ({
-        ...entity,
-        payload: JSON.parse(entity.payload),
-      }));
-      const newChannelCount =
-        entities.length > 1
-          ? `${entities.length} new channels were`
-          : 'A new channel was';
-
-      return {
-        raw: {
-          date,
-          context,
-          entities,
-        },
-      };
-    }
-    case 'USER_JOINED_COMMUNITY': {
-      const actors = parseActors(notification.actors, { id: currentUserId });
-      const event = parseEvent(notification.event);
-      const date = parseNotificationDate(notification.modifiedAt);
-      const context = parseContext(notification.context);
-
-      return {
-        data: {
-          href: `/${context.asObject.payload.slug}`,
-        },
-        title: `${actors.asString} ${event} ${context.asString}`,
-        raw: {
-          actors,
-          event,
-          date,
-          context,
-        },
-      };
-    }
-    case 'THREAD_CREATED': {
-      const date = parseNotificationDate(notification.modifiedAt);
-      const context = parseContext(notification.context);
-      // sort and order the threads
-      const threads = sortThreads(notification.entities, { id: currentUserId });
-
-      const newThreadCount =
-        threads.length > 1 ? `New threads were` : 'A new thread was';
-
-      return {
-        data: {
-          href: `/thread/${threads[0].id}`,
-        },
-        title: `${newThreadCount} published in ${context.asString}`,
-        body: sentencify(threads.map(thread => `"${thread.content.title}"`)),
-        raw: {
-          date,
-          context,
-          threads,
-        },
-      };
-    }
-    case 'COMMUNITY_INVITE': {
-      const date = parseNotificationDate(notification.modifiedAt);
-      const context = parseContext(notification.context);
-      const actors = parseActors(notification.actors, { id: currentUserId });
-
-      return {
-        data: {
-          href: `/${context.asObject.slug}`,
-        },
-        title: `${actors.asString} invited you to join their community, ${context.asString}`,
-        raw: {
-          date,
-          context,
-          actors,
-        },
-      };
-    }
-    default: {
-      return {};
-    }
-  }
 };
