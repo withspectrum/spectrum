70667dba88b4cead5b8050b172bfa1888c8cd337
Brian Lovin <briandlovin@gmail.com>
2017-07-17 17:04:20 -0600

    Add comments, remove promise all

--- iris/migrations/20170717143142-dedupe-users-settings.js
+++ iris/migrations/20170717143142-dedupe-users-settings.js
@@ -1,24 +1,32 @@
 exports.up = function(r, conn) {
-  return Promise.all([r.tableCreate('usersSettingsUnique').run(conn)])
+  // create a second table that will store unique-ified usersSettings
+  return r
+    .tableCreate('usersSettingsUnique')
+    .run(conn)
     .then(() => {
+      // in the new table, insert distinct records from the original usersSettings table
       return r
         .table('usersSettingsUnique')
         .insert(r.table('usersSettings').without('id').distinct())
         .run(conn);
     })
     .then(() => {
+      // delete the old table with duplicate records
       return r.tableDrop('usersSettings').run(conn);
     })
     .then(() => {
+      // create a new clean table
       return r.tableCreate('usersSettings').run(conn);
     })
     .then(() => {
+      // insert the unique-ified usersSettings records into the now-empty usersSettings table
       return r
         .table('usersSettings')
-        .insert(r.table('usersSettingsUnique').without('id').distinct())
+        .insert(r.table('usersSettingsUnique').without('id').distinct()) // keeping this distinct function just because
         .run(conn);
     })
     .then(() => {
+      // remove the now-redundant unique-ified records
       return r.tableDrop('usersSettingsUnique').run(conn);
     });
 };
