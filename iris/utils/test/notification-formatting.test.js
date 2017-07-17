a22e628373d55d9ba5dba91669494156fb9d0ecc
Max Stoiber <contact@mxstbr.com>
2017-07-17 10:44:12 -0700

    Add test for direct message notifications

--- iris/utils/test/notification-formatting.test.js
+++ iris/utils/test/notification-formatting.test.js
@@ -3,6 +3,7 @@ import formatNotification from '../notification-formatting';
 import CHANNEL_CREATED_FIXTURE from './fixtures/CHANNEL_CREATED.json';
 import COMMUNITY_INVITE_FIXTURE from './fixtures/COMMUNITY_INVITE.json';
 import MESSAGE_CREATED_FIXTURE from './fixtures/MESSAGE_CREATED.json';
+import DIRECT_MESSAGE_CREATED_FIXTURE from './fixtures/DIRECT_MESSAGE_CREATED.json';
 import REACTION_CREATED_FIXTURE from './fixtures/REACTION_CREATED.json';
 import THREAD_CREATED_FIXTURE from './fixtures/THREAD_CREATED.json';
 import USER_JOINED_COMMUNITY_FIXTURE from './fixtures/USER_JOINED_COMMUNITY.json';
@@ -25,6 +26,13 @@ it('should format a community invite notification', () => {
 
 it('should format a message creation notification', () => {
   let result = formatNotification(MESSAGE_CREATED_FIXTURE, USER_ID);
+  expect(result.raw).toBeDefined();
+  delete result.raw; // Don't need raw data
+  expect(result).toMatchSnapshot();
+});
+
+it('should format a direct message creation notification', () => {
+  let result = formatNotification(DIRECT_MESSAGE_CREATED_FIXTURE, USER_ID);
   expect(result.raw).toBeDefined();
   delete result.raw; // Don't need raw data
   expect(result).toMatchSnapshot();
