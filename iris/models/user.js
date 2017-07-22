83c8b84570865e7c668b9b7cfe4d42af759fb76a
Max Stoiber <contact@mxstbr.com>
2017-07-22 15:54:39 -0700

    First try

--- iris/models/user.js
+++ iris/models/user.js
@@ -187,22 +187,34 @@ const setUsername = (id: string, username: string) => {
     .then(result => result.changes[0].new_val);
 };
 
+const getThreadsByChannel = channelId => {
+  return db
+    .table('threads')
+    .between([channelId, db.minval], [channelId, db.maxval], {
+      index: 'channelIdAndLastActive',
+    });
+};
+
 const getEverything = (userId: string): Promise<Array<any>> => {
   return db
     .table('usersChannels')
     .getAll(userId, { index: 'userId' })
-    .eqJoin('channelId', db.table('threads'), {
-      index: 'channelId',
-    })
-    .without({
-      left: ['id', 'channelId', 'createdAt', 'isModerator', 'isOwner'],
-    })
-    .zip()
-    .filter({ isBlocked: false, isPending: false, isMember: true })
-    .without('isBlocked', 'isPending', 'isMember')
-    .filter(thread => db.not(thread.hasFields('deletedAt')))
-    .orderBy(db.desc('lastActive'), db.desc('createdAt'))
-    .run();
+    .run()
+    .then(
+      userChannels =>
+        userChannels &&
+        userChannels.length > 0 &&
+        getThreadsByChannel(userChannels[0].channelId)
+          .union(
+            ...userChannels
+              // Omit first item as we already got that above ^
+              .slice(1)
+              // Get threads for each channel and interleave them
+              .map(({ channelId }) => getThreadsByChannel(channelId)),
+            { interleave: 'channelIdAndLastActive' }
+          )
+          .run()
+    );
 };
 
 const getUsersThreadCount = (
