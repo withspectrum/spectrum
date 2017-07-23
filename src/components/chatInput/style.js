971ea3de2e59976b62719b297f2d4fa12d72c618
Brian Lovin <briandlovin@gmail.com>
2017-07-23 16:21:44 -0700

    Mobile layout and styling fixes

--- src/components/chatInput/style.js
+++ src/components/chatInput/style.js
@@ -14,7 +14,7 @@ export const ChatInputWrapper = styled(FlexRow)`
   border-top: 2px solid ${({ theme }) => theme.border.default};
 
   @media (max-width: 768px) {
-    bottom: ${props => (props.focus ? '48px' : 'auto')};
+    bottom: ${props => (props.focus ? '0' : 'auto')};
     position: ${props => (props.focus ? 'fixed' : 'relative')};
     background-color: ${props =>
       props.focus ? props.theme.bg.default : 'transparent'};
