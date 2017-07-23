6f44bc5d62738d7bcefc25593144ea883ec7681d
Max Stoiber <contact@mxstbr.com>
2017-07-23 11:42:06 -0700

    Fix toasts, make sure outro animation runs

--- src/components/toasts/style.js
+++ src/components/toasts/style.js
@@ -21,16 +21,16 @@ const toastFade = keyframes`
     top: 8px;
 	}
 	5% {
-		opacity: 1
+		opacity: 1;
     top: 0;
 	}
-  90% {
-		opacity: 1
+  95% {
+		opacity: 1;
     top: 0;
 	}
   100% {
     opacity: 0;
-    top: 0;
+    top: -4px;
   }
 `;
 
@@ -45,13 +45,13 @@ const Toast = styled.div`
   line-height: 1.4;
   display: block;
   margin-bottom: 8px;
-  box-shadow: 0 4px 4px rgba(0,0,0,0.1);
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
   opacity: 0;
   position: relative;
-  animation-duration: 3s;
-	animation-fill-mode: forwards;
-	animation-name: ${toastFade};
-	animation-timing-function: linear;
+  animation-duration: ${props => `${props.timeout}ms` || '3s'};
+  animation-fill-mode: forwards;
+  animation-name: ${toastFade};
+  animation-timing-function: linear;
 `;
 
 export const ErrorToast = styled(Toast)`
