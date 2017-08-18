9728ae4485a87ca187fa8491fe7ea4ddf4aaf999
Bryn Jackson <hi@bryn.io>
2017-08-18 13:00:30 -0700

    Revert "Sentry Release Tracking (#1373)"
    This reverts commit f101d7b7596a1818c09891e08878c1d757ab7eac.

--- iris/routes/middlewares/raven.js
+++ iris/routes/middlewares/raven.js
@@ -1,16 +1,10 @@
 // @flow
 import Raven from 'raven';
 
-// Get hash of latest commit
-const commitHash = require('child_process')
-  .execSync('git rev-parse --short HEAD')
-  .toString();
-
 Raven.config(
   'https://3bd8523edd5d43d7998f9b85562d6924:d391ea04b0dc45b28610e7fad735b0d0@sentry.io/154812',
   {
     environment: process.env.NODE_ENV,
-    release: commitHash,
   }
 ).install();
 
--- shared/bull/create-worker.js
+++ shared/bull/create-worker.js
@@ -2,23 +2,7 @@
 // Create a worker with bull and start a small webserver which responds with
 // health information
 const http = require('http');
-const Raven = require('raven');
 const createQueue = require('./create-queue');
-
-if (process.env.NODE_ENV !== 'development') {
-  // Get hash of latest commit
-  const commitHash = require('child_process')
-    .execSync('git rev-parse --short HEAD')
-    .toString();
-
-  Raven.config(
-    'https://3bd8523edd5d43d7998f9b85562d6924:d391ea04b0dc45b28610e7fad735b0d0@sentry.io/154812',
-    {
-      environment: process.env.NODE_ENV,
-      release: commitHash,
-    }
-  ).install();
-}
 
 /*::
 type QueueMap = {
