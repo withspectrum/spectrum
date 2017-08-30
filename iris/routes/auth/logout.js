a4d71ce2cdda6a8c870b58d9f47090be54ae73f1
Maximilian Stoiber <contact@mxstbr.com>
2017-08-30 21:19:56 +0200

    Add some more logs

--- iris/routes/auth/logout.js
+++ iris/routes/auth/logout.js
@@ -8,30 +8,19 @@ const HOME = IS_PROD ? '/' : 'http://localhost:3000/';
 const logoutRouter = Router();
 
 logoutRouter.get('/', (req, res) => {
-  debug('visiting');
+  debug('started');
   const sessionCookie = req.cookies['connect.sid'];
   if (req.isUnauthenticated() || !sessionCookie) {
     debug('is unauthenticated, aborting logout');
     return res.redirect(HOME);
   }
-  const sessionId = sessionCookie.split('.')[0].replace('s:', '');
-  return destroySession(sessionId)
-    .then(() => {
-      debug(`destroyed session ${sessionId}`);
-      // I should not have to do this manually
-      // but it doesn't work otherwise ¯\_(ツ)_/¯
-      res.clearCookie('connect.sid');
-      req.logout();
-      res.redirect(HOME);
-      debug('done');
-    })
-    .catch(err => {
-      debug(`failed destroying session ${sessionId}`);
-      res.clearCookie('connect.sid');
-      console.log(err);
-      res.redirect(HOME);
-      debug('done');
-    });
+  debug('logging out');
+  req.logout();
+  req.session.destroy(err => {
+    if (err) console.log(err);
+    debug(`destroyed session, redirecting`);
+    res.redirect(HOME);
+  });
 });
 
 export default logoutRouter;
