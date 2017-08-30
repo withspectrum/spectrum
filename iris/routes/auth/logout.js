333e6b17d00f082fbf44011919bbab557eb458bc
Maximilian Stoiber <contact@mxstbr.com>
2017-08-30 18:47:47 +0200

    Add more logging to iris

--- iris/routes/auth/logout.js
+++ iris/routes/auth/logout.js
@@ -1,5 +1,6 @@
 // @flow
 import { Router } from 'express';
+const debug = require('debug')('iris:routes:auth:logout');
 import { destroySession } from '../../models/session';
 
 const IS_PROD = process.env.NODE_ENV === 'production';
@@ -7,23 +8,29 @@ const HOME = IS_PROD ? '/' : 'http://localhost:3000/';
 const logoutRouter = Router();
 
 logoutRouter.get('/', (req, res) => {
+  debug('visiting');
   const sessionCookie = req.cookies['connect.sid'];
   if (req.isUnauthenticated() || !sessionCookie) {
+    debug('is unauthenticated, aborting logout');
     return res.redirect(HOME);
   }
   const sessionId = sessionCookie.split('.')[0].replace('s:', '');
   return destroySession(sessionId)
     .then(() => {
+      debug(`destroyed session ${sessionId}`);
       // I should not have to do this manually
       // but it doesn't work otherwise ¯\_(ツ)_/¯
       res.clearCookie('connect.sid');
       req.logout();
       res.redirect(HOME);
+      debug('done');
     })
     .catch(err => {
+      debug(`failed destroying session ${sessionId}`);
       res.clearCookie('connect.sid');
       console.log(err);
       res.redirect(HOME);
+      debug('done');
     });
 });
 
