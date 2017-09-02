17ca3861d2c09c90d09359a82fb4c0f1ee926433
Maximilian Stoiber <contact@mxstbr.com>
2017-09-02 09:30:39 +0200

    Convert shared options to ES5

--- shared/graphql/apollo-client-options.js
+++ shared/graphql/apollo-client-options.js
@@ -1,49 +1,67 @@
 // @flow
-import { toIdValue } from 'react-apollo';
+var apollo = require('react-apollo'),
+  toIdValue = apollo.toIdValue;
 
-const getSharedApolloClientOptions = client => ({
-  queryDeduplication: true,
-  dataIdFromObject: result => {
-    if (result.__typename) {
-      // Custom Community cache key based on slug
-      if (result.__typename === 'Community' && !!result.slug) {
-        return `${result.__typename}:${result.slug}`;
+var getSharedApolloClientOptions = function getSharedApolloClientOptions(
+  client
+) {
+  return {
+    queryDeduplication: true,
+    dataIdFromObject: function dataIdFromObject(result) {
+      if (result.__typename) {
+        // Custom Community cache key based on slug
+        if (result.__typename === 'Community' && !!result.slug) {
+          return result.__typename + ':' + result.slug;
+        }
+        // Custom Channel cache key based on slug and community slug
+        if (
+          result.__typename === 'Channel' &&
+          !!result.slug &&
+          !!result.community &&
+          !!result.community.slug
+        ) {
+          return (
+            result.__typename + ':' + result.community.slug + ':' + result.slug
+          );
+        }
+        // This was copied from the default dataIdFromObject
+        if (result.id !== undefined) {
+          return result.__typename + ':' + result.id;
+        }
+        if (result._id !== undefined) {
+          return result.__typename + ':' + result._id;
+        }
       }
-      // Custom Channel cache key based on slug and community slug
-      if (
-        result.__typename === 'Channel' &&
-        !!result.slug &&
-        !!result.community &&
-        !!result.community.slug
-      ) {
-        return `${result.__typename}:${result.community.slug}:${result.slug}`;
-      }
-      // This was copied from the default dataIdFromObject
-      if (result.id !== undefined) {
-        return `${result.__typename}:${result.id}`;
-      }
-      if (result._id !== undefined) {
-        return `${result.__typename}:${result._id}`;
-      }
-    }
-    return null;
-  },
-  customResolvers: {
-    Query: {
-      thread: (_, { id }) =>
-        toIdValue(client.dataIdFromObject({ __typename: 'Thread', id })),
-      community: (_, { slug }) =>
-        toIdValue(client.dataIdFromObject({ __typename: 'Community', slug })),
-      channel: (_, { channelSlug, communitySlug }) =>
-        toIdValue(
-          client.dataIdFromObject({
-            __typename: 'Channel',
-            slug: channelSlug,
-            community: { slug: communitySlug },
-          })
-        ),
+      return null;
     },
-  },
-});
+    customResolvers: {
+      Query: {
+        thread: function thread(_, _ref) {
+          var id = _ref.id;
+          return toIdValue(
+            client.dataIdFromObject({ __typename: 'Thread', id: id })
+          );
+        },
+        community: function community(_, _ref2) {
+          var slug = _ref2.slug;
+          return toIdValue(
+            client.dataIdFromObject({ __typename: 'Community', slug: slug })
+          );
+        },
+        channel: function channel(_, _ref3) {
+          var channelSlug = _ref3.channelSlug,
+            communitySlug = _ref3.communitySlug;
+          return toIdValue(
+            client.dataIdFromObject({
+              __typename: 'Channel',
+              slug: channelSlug,
+              community: { slug: communitySlug },
+            })
+          );
+        },
+      },
+    },
+  };
+};
 
-export default getSharedApolloClientOptions;
+module.exports = getSharedApolloClientOptions;
