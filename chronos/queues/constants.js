2ffc7cf169e06bd6b2b4ae32fb1e73d11ea2064d
Brian Lovin <briandlovin@gmail.com>
2017-09-02 11:17:03 -0700

    Tweak constant

--- chronos/queues/constants.js
+++ chronos/queues/constants.js
@@ -5,6 +5,6 @@ export const MIN_MESSAGE_COUNT = 1;
 export const MAX_THREAD_COUNT_PER_CHANNEL = 10;
 export const MIN_THREADS_REQUIRED_FOR_DIGEST = 1;
 export const MAX_THREAD_COUNT_PER_DIGEST = 10;
-export const COMMUNITY_UPSELL_THRESHOLD = 100;
+export const COMMUNITY_UPSELL_THRESHOLD = 5;
 // queues
 export const SEND_WEEKLY_DIGEST_EMAIL = 'send weekly digest email';
