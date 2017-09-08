1bdefb0a44b844ca2e298f40c570e22ec93ee40c
Brian Lovin <briandlovin@gmail.com>
2017-09-07 17:40:19 -0700

    Refactor routes one more time

--- iris/routes/webhooks/index.js
+++ iris/routes/webhooks/index.js
@@ -1,25 +1,53 @@
 // @flow
 // $FlowFixMe
-import { Router } from 'express';
-const webhookRouter = Router();
+const env = require('node-env-file');
+const IS_PROD = process.env.NODE_ENV === 'production';
+// $FlowFixMe
+const path = require('path');
+if (!IS_PROD) {
+  env(path.resolve(__dirname, '../.env'), { raise: false });
+}
+const STRIPE_TOKEN = process.env.STRIPE_TOKEN;
+const STRIPE_WEBHOOK_SECRET = process.env['STRIPE_WEBHOOK_SECRET'];
+// $FlowFixMe
+const stripe = require('stripe')(STRIPE_TOKEN),
+  currency = 'USD';
 
-import { processInvoicePaid } from './functions';
+import { createInvoice, getInvoiceByChargeId } from '../../models/invoice';
+import {
+  updateRecurringPaymentPeriod,
+  getRecurringPaymentFromSubscriptionId,
+} from '../../models/recurringPayment';
 
-webhookRouter.post('/', (req, res) => {
-  const event = req.body;
+export const processInvoicePaid = async (event: Object) => {
+  // the object field contains all the data related to the event
+  const { data: { object } } = event;
 
-  switch (event.type) {
-    case 'invoice.payment_succeeded': {
-      return processInvoicePaid(event)
-        .then(() => res.status(200).send('Webhook received: ' + event.id))
-        .catch(err => {
-          console.log(`Error in webhook for ${event.type}: ` + err);
-        });
-    }
-    default: {
-      res.status(200).send('Webhook received: ' + event.id);
-    }
-  }
-});
+  // ensure we aren't duplicating an invoice in the db
+  // chargeIds should be unique on stripe, so if we have a record of an invoice with the same charge id we can assume it is a duplicate
+  const existingInvoice = await getInvoiceByChargeId(object.charge);
+  if (existingInvoice) return Promise.all([]);
 
-export default webhookRouter;
+  // fetch the subscription record from stripe as it contains a few new fields we need below
+  const getSubscription = await stripe.subscriptions.retrieve(
+    object.subscription
+  );
+  // we need to retrieve a community id for the payment, if one exists
+  const rPayment = await getRecurringPaymentFromSubscriptionId(
+    object.subscription
+  );
+
+  // get the customer object from stripe
+  const customer = await stripe.customers.retrieve(object.customer);
+  // create an invoice record in the database (receipt)
+  const invoice = await createInvoice(
+    object,
+    getSubscription,
+    customer,
+    rPayment
+  );
+  // update the recurringPayment record in the database that triggered this invoice payment and update the period_end and period_start dates to show in the ui
+  const updateRecurringPayment = await updateRecurringPaymentPeriod(object);
+
+  return Promise.all([getSubscription, invoice, updateRecurringPayment]);
+};
