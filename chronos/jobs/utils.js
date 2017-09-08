95d886ae5fe8a5525c38d03a0ec7d6c20242c6ea
Brian Lovin <briandlovin@gmail.com>
2017-09-08 11:05:39 -0700

    Remove unneeded code

--- chronos/jobs/utils.js
+++ chronos/jobs/utils.js
@@ -1,22 +1,9 @@
 // @flow
 const createQueue = require('../../shared/bull/create-queue');
-import Raven from 'raven';
-
-// logging to Sentry for weekly digests
-if (process.env.NODE_ENV !== 'development') {
-  Raven.config(
-    'https://3bd8523edd5d43d7998f9b85562d6924:d391ea04b0dc45b28610e7fad735b0d0@sentry.io/154812',
-    {
-      environment: process.env.NODE_ENV,
-    }
-  ).install();
-}
 
 export const addQueue = (name: string, data: any, opts: any) => {
   const worker = createQueue(name);
-  return worker
-    .add({ ...data }, { ...opts })
-    .catch(err => Raven.captureException(err));
+  return worker.add({ ...data }, { ...opts });
 };
 
 export const createJob = (
