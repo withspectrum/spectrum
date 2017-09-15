626e70824a66902b06a8eae5fe9a5bc9b2446527
Brian Lovin <briandlovin@gmail.com>
2017-09-15 13:21:57 -0700

    Story type migrate

--- iris/migrations/20170915201609-clean-up-bad-dm-data.js
+++ iris/migrations/20170915201609-clean-up-bad-dm-data.js
@@ -1,12 +1,21 @@
 // @flow
 exports.up = function(r, conn) {
-  return r
-    .table('messages')
-    .filter({ threadType: 'DIRECT_MESSAGE_GROUP' })
-    .update({
-      threadType: 'directMessageThread',
-    })
-    .run(conn);
+  return Promise.all([
+    r
+      .table('messages')
+      .filter({ threadType: 'DIRECT_MESSAGE_GROUP' })
+      .update({
+        threadType: 'directMessageThread',
+      })
+      .run(conn),
+    r
+      .table('messages')
+      .filter({ threadType: 'STORY' })
+      .update({
+        threadType: 'story',
+      })
+      .run(conn),
+  ]);
 };
 
 exports.down = function(r, conn) {
