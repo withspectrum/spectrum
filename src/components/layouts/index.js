c8ada5e73ec10cdef722437e7932012c0543e9ea
uberbryn <hi@bryn.io>
2017-09-20 15:06:47 -0700

    adds initial css-grid component. replaces rep icon.

---
+++ src/components/layouts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const HeaderGrid = styled.div`
+  display: grid;
+  grid-template-rows: [header] 48px [body] auto;
+  grid-template-columns: 100%;
+  grid-template-areas: ;
+`;
