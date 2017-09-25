fe331cb7afaa7f1b849270744264e9fe843956ba
Brian Lovin <briandlovin@gmail.com>
2017-09-24 23:21:31 -0700

    Documentation for the viewNetworkHandler HOC

--- src/components/viewNetworkHandler/index.js
+++ src/components/viewNetworkHandler/index.js
@@ -5,6 +5,21 @@ import hoistStatics from 'hoist-non-react-statics';
 
 /*
 
+This HOC can be used to wrap any component that is performaing data queries with Apollo. It will return human-readable props that indicate the status of the query. For example, in your component rather than writing:
+
+if (networkStatus === 3) {
+  ...
+}
+
+You can no write:
+
+if (isFetchingMore) {
+  ...
+}
+
+If you need to override this at any point, the original data prop is returned as well, so in your component you can still access this.props.data.networkStatus.
+
+
 Apollo NetworkStatus indicators:
 
 1. loading: The query has never been run before and the request is now pending. A query will still have this network status even if a result was returned from the cache, but a query was dispatched anyway.
@@ -33,8 +48,12 @@ const viewNetworkHandler = Component => {
     if (!data)
       return <Component {...remainingProps} ref={wrappedComponentRef} />;
 
-    // if  the view is running a query for the first time, or the view has already mounted but the variables have changed, the component should be loading
+    // 1: if  the view is running a query for the first time
+    // 2: the view has already mounted but the variables have changed, the component should be loading. For example, if you are viewing a user profile but click directly onto another user's profile from a thread facepile, the userProfile component has already loaded, but now the variable (userId) has been changed. This should force a loading state.
     const isLoading = data.networkStatus === 1 || data.networkStatus === 2;
+    const isFetchingMore = data.networkStatus === 3;
+    const isRefetching = data.networkStatus === 4;
+    const isPolling = data.networkStatus === 6;
     const hasError = data.networkStatus === 8;
 
     return (
@@ -43,6 +62,9 @@ const viewNetworkHandler = Component => {
         data={data}
         hasError={hasError}
         isLoading={isLoading}
+        isFetchingMore={isFetchingMore}
+        isRefetching={isRefetching}
+        isPolling={isPolling}
         ref={wrappedComponentRef}
       />
     );
