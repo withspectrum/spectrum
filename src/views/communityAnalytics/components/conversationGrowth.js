b03c89adc2c208e8bb4998368da3f6bd46fe4233
Brian Lovin <briandlovin@gmail.com>
2017-09-27 18:36:04 -0600

    Fix copy

--- src/views/communityAnalytics/components/conversationGrowth.js
+++ src/views/communityAnalytics/components/conversationGrowth.js
@@ -45,9 +45,9 @@ class ConversationGrowth extends React.Component<Props> {
         <SectionCard>
           <SectionSubtitle>Conversations</SectionSubtitle>
           <SectionTitle>{count}</SectionTitle>
-          {parseGrowth(weeklyGrowth, 'weekly')}
-          {parseGrowth(monthlyGrowth, 'monthly')}
-          {parseGrowth(quarterlyGrowth, 'quarterly')}
+          {parseGrowth(weeklyGrowth, '7 days')}
+          {parseGrowth(monthlyGrowth, '30 days')}
+          {parseGrowth(quarterlyGrowth, '90 days')}
         </SectionCard>
       );
     }
