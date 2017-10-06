e877e1e62306045c2b2bf0f645ee58403887fdd7
Maximilian Stoiber <contact@mxstbr.com>
2017-10-06 18:42:07 +0200

    Fix placeholder

--- src/components/draftjs-editor/index.js
+++ src/components/draftjs-editor/index.js
@@ -144,6 +144,8 @@ class Editor extends React.Component {
       focus,
       singleLine,
       version,
+      placeholder,
+      readOnly,
       ...rest
     } = this.props;
 
@@ -166,6 +168,8 @@ class Editor extends React.Component {
               this.editor = editor;
               if (this.props.editorRef) this.props.editorRef(editor);
             }}
+            readOnly={readOnly}
+            placeholder={!readOnly && placeholder}
             {...rest}
           />
           {showLinkPreview && linkPreview && linkPreview.loading ? (
@@ -211,6 +215,8 @@ class Editor extends React.Component {
                 this.editor = editor;
                 if (this.props.editorRef) this.props.editorRef(editor);
               }}
+              readOnly={readOnly}
+              placeholder={!readOnly && placeholder}
               {...rest}
             />
           </Wrapper>
