1adbccdf26103d3712febca29f809b76f26d9016
Maximilian Stoiber <contact@mxstbr.com>
2017-10-06 09:37:39 +0200

    Clean up testing setup

--- shared/testing/setup-db.js
+++ shared/testing/setup-db.js
@@ -4,30 +4,23 @@ const tables = Object.keys(data);
 /**
  * This is run before all tests in src/setupTests.js
  */
-export const setup = () => {
+export const setup = db => {
   // Create the tables
-  return (
-    Promise.all(tables.map(table => db.tableCreate(table).run()))
-      // Insert data into the tables
-      .then(() =>
-        Promise.all(
-          tables.map(table => db.table(table).insert(data[table]).run())
-        )
-      )
-      .catch(err => {
-        console.log(err);
-        throw err;
-      })
-  );
+  return Promise.all(
+    tables.map(table =>
+      db
+        .table(table)
+        .insert(data[table])
+        .run()
+    )
+  ).catch(err => {
+    console.log(err);
+    throw err;
+  });
 };
 
 const db = require('rethinkdbdash')({
   db: 'testing',
 });
 
-db
-  .dbCreate('testing')
-  .run()
-  .then(() => console.log('db created'))
-  .then(setup)
-  .then(() => process.exit());
+setup(db).then(() => process.exit());
