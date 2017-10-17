f8f4650fdff6f100715dd32216fc1c2e76e9dffe
Brian Lovin <briandlovin@gmail.com>
2017-10-16 20:32:06 -0700

    Fix migration script

--- iris/migrations/20171013195530-core-metrics-table.js
+++ iris/migrations/20171013195530-core-metrics-table.js
@@ -2,23 +2,23 @@
 
 exports.up = function(r, conn) {
   return Promise.all([
-    // r
-    //   .tableCreate('coreMetrics')
-    //   .run(conn)
-    //   .catch(err => {
-    //     console.log(err);
-    //     throw err;
-    //   })
+    r
+      .tableCreate('coreMetrics')
+      .run(conn)
+      .catch(err => {
+        console.log(err);
+        throw err;
+      }),
   ])
     .then(() => {
-      // return r
-      //   .table('coreMetrics')
-      //   .indexCreate('date', r.row('date'))
-      //   .run(conn)
-      //   .catch(err => {
-      //     console.log(err);
-      //     throw err;
-      //   })
+      return r
+        .table('coreMetrics')
+        .indexCreate('date', r.row('date'))
+        .run(conn)
+        .catch(err => {
+          console.log(err);
+          throw err;
+        });
     })
     .then(() => {
       const saveCoreMetrics = (data, index) => {
@@ -33,175 +33,17 @@ exports.up = function(r, conn) {
           .run(conn);
       };
 
-      // dau, wau, mau
-      const getAu = (range, index) => {
-        return r
-          .table('users')
-          .filter(
-            r
-              .row('lastSeen')
-              .during(
-                r.now().sub(60 * 60 * 24 * (index + 30)),
-                r.now().sub(60 * 60 * 24 * index)
-              )
-          )
-          .count()
-          .default(0)
-          .run(conn);
-      };
-
-      function getCommunitiesWithMinimumMembers(min, index) {
-        return r
-          .table('communities')
-          .filter(
-            r
-              .row('createdAt')
-              .during(
-                r.now().sub(60 * 60 * 24 * (index + 30)),
-                r.now().sub(60 * 60 * 24 * index)
-              )
-          )
-          .pluck('id')
-          .run(conn)
-          .then(cursor => cursor.toArray())
-          .then(communities => communities.map(community => community.id))
-          .then(communityIds => {
-            return Promise.all(
-              communityIds.map(community => {
-                return r
-                  .table('usersCommunities')
-                  .getAll(community, { index: 'communityId' })
-                  .filter({ isMember: true })
-                  .eqJoin('userId', r.table('users'))
-                  .zip()
-                  .filter(
-                    r
-                      .row('lastActive')
-                      .during(
-                        r.now().sub(60 * 60 * 24 * (index + 30)),
-                        r.now().sub(60 * 60 * 24 * index)
-                      )
-                  )
-                  .count()
-                  .run(conn)
-                  .then(cursor => cursor.toArray())
-                  .then(count => {
-                    return {
-                      id: community,
-                      count,
-                    };
-                  });
-              })
-            );
-          })
-          .then(data => {
-            let sortedCommunities = data
-              .filter(c => c.count > min)
-              .map(community => community.id);
-
-            return r
-              .table('communities')
-              .getAll(...sortedCommunities)
-              .filter(community => r.not(community.hasFields('deletedAt')))
-              .run(conn)
-              .then(cursor => cursor.toArray());
-          });
-      }
-
-      // dac, wac, mac
-      const getAc = (range, index) => {
-        // constants
-        const MIN_THREAD_COUNT = 5;
-        const MIN_MEMBER_COUNT = 50;
-
-        // get threads posted in the range
-        return Promise.all([
-          r
-            .table('threads')
-            .filter(
-              r
-                .row('lastActive')
-                .during(
-                  r.now().sub(60 * 60 * 24 * (index + 30)),
-                  r.now().sub(60 * 60 * 24 * index)
-                )
-            )
-            .filter(thread => r.not(thread.hasFields('deletedAt')))
-            .run(conn)
-            .then(cursor => cursor.toArray()),
-        ]).then(([threadsPostedInRange]) => {
-          // we will iterate through each thread and accumulate the counts of threads posted in each community
-          const threadCountPerCommunity = {};
-          // accumulate threads posted in the last month keyed by communityid
-          threadsPostedInRange.map(c => {
-            threadCountPerCommunity[c.communityId] = threadCountPerCommunity[
-              c.communityId
-            ]
-              ? threadCountPerCommunity[c.communityId] + 1
-              : 1;
-          });
-          const activeCommunitiesByThreads = Object.keys(
-            threadCountPerCommunity
-          ).filter(o => threadCountPerCommunity[o] >= MIN_THREAD_COUNT);
-
-          // communities that meet a minimum membership threshold
-          const activeCommunitiesByMembership = getCommunitiesWithMinimumMembers(
-            MIN_MEMBER_COUNT,
-            index
-          );
-
-          return Promise.all([
-            activeCommunitiesByThreads,
-            activeCommunitiesByMembership,
-          ]).then(([activeCommunitiesByThreads, activeByMembership]) => {
-            const activeCommunitiesByMembership = activeByMembership.map(
-              c => c.id
-            );
-
-            const intersection = (a, b) => {
-              let ai = 0,
-                bi = 0;
-              let result = [];
-
-              while (ai < a.length && bi < b.length) {
-                if (a[ai] < b[bi]) {
-                  ai++;
-                } else if (a[ai] > b[bi]) {
-                  bi++;
-                } else {
-                  /* they're equal */
-                  result.push(a[ai]);
-                  ai++;
-                  bi++;
-                }
-              }
-
-              return result;
-            };
-
-            // find communities that met both thread and membership criteria
-            const activeCommunities = intersection(
-              activeCommunitiesByThreads,
-              activeCommunitiesByMembership
-            );
-
-            return activeCommunities.length;
-          });
-        });
-      };
-
       const getCount = (table, filter, index, field) => {
         if (filter) {
           return r
             .table(table)
             .filter(filter)
             .filter(
-              r
-                .row(field)
-                .during(
-                  r.now().sub(60 * 60 * 24 * 365),
-                  r.now().sub(60 * 60 * 24 * index)
-                )
+              r.row(field).during(
+                // go back to all time, for some reason minval isn't working
+                r.now().sub(60 * 60 * 24 * 365),
+                r.now().sub(60 * 60 * 24 * index)
+              )
             )
             .filter(row => r.not(row.hasFields('deletedAt')))
             .count()
@@ -210,17 +52,33 @@ exports.up = function(r, conn) {
 
         return r
           .table(table)
+          .filter(
+            r.row(field).during(
+              // go back to all time, for some reason minval isn't working
+              r.now().sub(60 * 60 * 24 * 365),
+              r.now().sub(60 * 60 * 24 * index)
+            )
+          )
           .filter(row => r.not(row.hasFields('deletedAt')))
           .count()
           .run(conn);
       };
 
       // cpu, tpu, mpu
-      const getPu = (table, index) => {
+      const getPu = (table, field, index) => {
+        // get the count of the users within a given range
         return getCount('users', null, index, 'createdAt')
           .then(userCount => {
+            // get the count of whatever entity within a given range
             const tableCount = r
               .table(table)
+              .filter(
+                r.row(field).during(
+                  // go back to all time, for some reason minval isn't working
+                  r.now().sub(60 * 60 * 24 * 365),
+                  r.now().sub(60 * 60 * 24 * index)
+                )
+              )
               .filter(row => r.not(row.hasFields('deletedAt')))
               .count()
               .run(conn);
@@ -235,15 +93,9 @@ exports.up = function(r, conn) {
       // array of 30 things
       const arr = Array.apply(null, { length: 30 }).map(() => 0);
       const backfill = arr.map((o, i) => {
-        const dau = getAu('daily', i);
-        const wau = getAu('weekly', i);
-        const mau = getAu('monthly', i);
-        const dac = getAc('daily', i);
-        const wac = getAc('weekly', i);
-        const mac = getAc('monthly', i);
-        const cpu = getPu('usersCommunities', i);
-        const mpu = getPu('messages', i);
-        const tpu = getPu('threads', i);
+        const cpu = getPu('usersCommunities', 'createdAt', i);
+        const mpu = getPu('messages', 'timestamp', i);
+        const tpu = getPu('threads', 'createdAt', i);
         const users = getCount('users', null, i, 'createdAt');
         const communities = getCount('communities', null, i, 'createdAt');
         const threads = getCount('threads', null, i, 'createdAt');
@@ -269,12 +121,6 @@ exports.up = function(r, conn) {
         );
 
         return Promise.all([
-          dau,
-          wau,
-          mau,
-          dac,
-          wac,
-          mac,
           cpu,
           mpu,
           tpu,
@@ -287,12 +133,6 @@ exports.up = function(r, conn) {
         ]).then(
           (
             [
-              dau,
-              wau,
-              mau,
-              dac,
-              wac,
-              mac,
               cpu,
               mpu,
               tpu,
@@ -305,12 +145,6 @@ exports.up = function(r, conn) {
             ]
           ) => {
             const coreMetrics = {
-              dau,
-              wau,
-              mau,
-              dac,
-              wac,
-              mac,
               cpu,
               mpu,
               tpu,
