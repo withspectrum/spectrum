2d274fd603e179f12a550783a27f9827bee99970
Brian Lovin <briandlovin@gmail.com>
2017-10-17 17:41:27 -0700

    Set up split paths of logic for DM notifications

--- hermes/queues/send-new-direct-message-email.js
+++ hermes/queues/send-new-direct-message-email.js
@@ -1,5 +1,80 @@
+// @flow
 const debug = require('debug')('hermes:queue:send-new-direct-message-email');
+import sendEmail from '../send-email';
+import { generateUnsubscribeToken } from '../utils/generate-jwt';
+import {
+  NEW_DIRECT_MESSAGE_TEMPLATE,
+  TYPE_NEW_DIRECT_MESSAGE,
+  DEBUG_TEMPLATE,
+  TYPE_MUTE_DIRECT_MESSAGE_THREAD,
+} from './constants';
+import capitalize from '../utils/capitalize';
 
-export default job => {
-  return Promise.resolve();
+type SendNewMessageEmailJobData = {
+  recipient: {
+    email: string,
+    name: string,
+    username: string,
+    userId: string,
+  },
+  user: {
+    displayName: string,
+    username: string,
+    userId: string,
+    name: string,
+  },
+  thread: {
+    content: {
+      title: string,
+    },
+    path: string,
+    id: string,
+  },
+  message: {
+    content: {
+      body: string,
+    },
+  },
+};
+
+type SendNewMessageEmailJob = {
+  data: SendNewMessageEmailJobData,
+  id: string,
+};
+
+export default async (job: SendNewMessageEmailJob) => {
+  debug(`\nnew job: ${job.id}`);
+  const { recipient, user, thread, message } = job.data;
+  const subject = `New direct message from ${user.name} on Spectrum`;
+
+  const unsubscribeToken = await generateUnsubscribeToken(
+    user.userId,
+    TYPE_NEW_DIRECT_MESSAGE
+  );
+
+  const muteThreadToken = await generateUnsubscribeToken(
+    user.userId,
+    TYPE_MUTE_DIRECT_MESSAGE_THREAD,
+    thread.id
+  );
+
+  if (!recipient.email || !unsubscribeToken || !muteThreadToken) return;
+
+  try {
+    return sendEmail({
+      TemplateId: NEW_DIRECT_MESSAGE_TEMPLATE,
+      To: recipient.email,
+      TemplateModel: {
+        subject,
+        user,
+        thread,
+        username: user.username,
+        message,
+        muteThreadToken,
+        unsubscribeToken,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
