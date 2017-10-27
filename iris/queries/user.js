8cde12fe14e13feb85b5ba0d5ae1519c5cde8588
Brian Lovin <briandlovin@gmail.com>
2017-10-26 18:50:02 -0700

    More fixes!

--- iris/queries/user.js
+++ iris/queries/user.js
@@ -71,13 +71,10 @@ module.exports = {
     isPro: ({ id }: DBUser, _: any, { loaders }: GraphQLContext) => {
       return loaders.userRecurringPayments.load(id).then(result => {
         if (!result || result.length === 0) return false;
-        const subs = result && result.reduction;
+        const subs = result.reduction;
 
-        return Boolean(
-          Array.isArray(subs) &&
-            subs.some(
-              sub => sub.status === 'active' && subs.planId === 'beta-pro'
-            )
+        return subs.some(
+          sub => sub.status === 'active' && sub.planId === 'beta-pro'
         );
       });
     },
@@ -194,7 +191,8 @@ module.exports = {
         throw new UserError('You can only see your own recurring payments.');
       }
 
-      return loaders.userRecurringPayments.load(user.id).then(subs => {
+      return loaders.userRecurringPayments.load(user.id).then(result => {
+        const subs = result && result.reduction;
         const userProSubs =
           subs &&
           subs.length > 0 &&
