8240d1c9760d848a2de4cf8d5c24e65cc9a774b4
Maximilian Stoiber <contact@mxstbr.com>
2017-10-30 02:11:50 +0100

    channelPendingUsers loader
    For some reason this is fetched from the home community view for every
    single channel and then never displayed, which is... suboptimal.
    (even loads if you're not an admin, what)
    
    Anyways, this implements a loader so it will be faster, but ideally we
    shouldn't request this at all unless you go to the channel member view
    and are a channel mod or something like that.

--- iris/models/usersChannels.js
+++ iris/models/usersChannels.js
@@ -472,6 +472,15 @@ const getPendingUsersInChannel = (
   );
 };
 
+const getPendingUsersInChannels = (channelIds: Array<string>) => {
+  return db
+    .table('usersChannels')
+    .getAll(...channelIds, { index: 'channelId' })
+    .group('channelId')
+    .filter({ isPending: true })
+    .run();
+};
+
 const getBlockedUsersInChannel = (
   channelId: string
 ): Promise<Array<string>> => {
@@ -592,4 +601,5 @@ module.exports = {
   getOwnersInChannel,
   getUserPermissionsInChannel,
   getUsersPermissionsInChannels,
+  getPendingUsersInChannels,
 };
