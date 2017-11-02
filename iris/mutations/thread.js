ba560f5220684b77f5f1fb7daae51d14fc6fb93d
Maximilian Stoiber <contact@mxstbr.com>
2017-11-02 11:52:25 +0100

    Refactor editThread mutation to use async/await
    Much nicer :ok_hand:

--- iris/mutations/thread.js
+++ iris/mutations/thread.js
@@ -146,7 +146,7 @@ module.exports = {
         },
       });
     },
-    editThread: (_, { input }, { user }) => {
+    editThread: async (_, { input }, { user }) => {
       const currentUser = user;
 
       // user must be authed to edit a thread
@@ -162,80 +162,68 @@ module.exports = {
         );
       }
 
-      return getThreads([input.threadId])
-        .then(threads => {
-          // select the thread
-          const threadToEvaluate = threads[0];
+      const threads = await getThreads([input.threadId]);
 
-          // if the thread doesn't exist
-          if (!threads || !threadToEvaluate) {
-            return new UserError("This thread doesn't exist");
-          }
+      // select the thread
+      const threadToEvaluate = threads[0];
 
-          // only the thread creator can edit the thread
-          if (threadToEvaluate.creatorId !== currentUser.id) {
-            return new UserError(
-              "You don't have permission to make changes to this thread."
-            );
-          }
+      // if the thread doesn't exist
+      if (!threads || !threadToEvaluate) {
+        return new UserError("This thread doesn't exist");
+      }
 
-          let attachments = [];
-          // if the thread came in with attachments
-          if (input.attachments) {
-            // iterate through them and construct a new attachment object
-            input.attachments.map(attachment => {
-              attachments.push({
-                attachmentType: attachment.attachmentType,
-                data: JSON.parse(attachment.data),
-              });
-            });
+      // only the thread creator can edit the thread
+      if (threadToEvaluate.creatorId !== currentUser.id) {
+        return new UserError(
+          "You don't have permission to make changes to this thread."
+        );
+      }
 
-            const newInput = Object.assign({}, input, {
-              attachments,
-            });
-
-            return editThread(newInput);
-          } else {
-            // if no attachments were passed into the thread, we can just publish
-            // as-is
-            return editThread(input);
-          }
-        })
-        .then(editedThread => {
-          if (!input.filesToUpload) return Promise.all([editedThread]);
-
-          // if the edited thread has new files to upload
-          return Promise.all([
-            editedThread,
-            Promise.all(
-              input.filesToUpload.map(file =>
-                uploadImage(file, 'threads', editedThread.id)
-              )
-            ),
-          ]);
-        })
-        .then(([newThread, urls]) => {
-          if (!urls || urls.length === 0) return newThread;
-
-          // Replace the local image srcs with the remote image src
-          const body = JSON.parse(newThread.content.body);
-          const imageKeys = Object.keys(body.entityMap).filter(
-            key => body.entityMap[key].type === 'image'
-          );
-          urls.forEach((url, index) => {
-            if (!body.entityMap[imageKeys[index]]) return;
-            body.entityMap[imageKeys[index]].data.src = url;
-          });
-
-          // Update the thread with the new links
-          return editThread({
-            threadId: newThread.id,
-            content: {
-              ...newThread.content,
-              body: JSON.stringify(body),
-            },
+      let attachments = [];
+      // if the thread came in with attachments
+      if (input.attachments) {
+        // iterate through them and construct a new attachment object
+        input.attachments.map(attachment => {
+          attachments.push({
+            attachmentType: attachment.attachmentType,
+            data: JSON.parse(attachment.data),
           });
         });
+      }
+
+      const newInput = Object.assign({}, input, {
+        attachments,
+      });
+
+      const editedThread = await editThread(newInput);
+      if (!input.filesToUpload) return editedThread;
+
+      const urls = await Promise.all(
+        input.filesToUpload.map(file =>
+          uploadImage(file, 'threads', editedThread.id)
+        )
+      );
+
+      if (!urls || urls.length === 0) return editedThread;
+
+      // Replace the local image srcs with the remote image src
+      const body = JSON.parse(editedThread.content.body);
+      const imageKeys = Object.keys(body.entityMap).filter(
+        key => body.entityMap[key].type === 'image'
+      );
+      urls.forEach((url, index) => {
+        if (!body.entityMap[imageKeys[index]]) return;
+        body.entityMap[imageKeys[index]].data.src = url;
+      });
+
+      // Update the thread with the new links
+      return editThread({
+        threadId: editedThread.id,
+        content: {
+          ...editedThread.content,
+          body: JSON.stringify(body),
+        },
+      });
     },
     deleteThread: (_, { threadId }, { user }) => {
       const currentUser = user;
