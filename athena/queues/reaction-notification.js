c84b5824d2cbc468cd536463dabf5c85a632a360
Brian Lovin <briandlovin@gmail.com>
2017-11-05 16:51:39 -0800

    Refactor reaction notification to async/await

--- athena/queues/reaction-notification.js
+++ athena/queues/reaction-notification.js
@@ -25,7 +25,7 @@ type JobData = {
     userId: string,
   },
 };
-export default (job: JobData) => {
+export default async (job: JobData) => {
   const incomingReaction = job.data.reaction;
   const currentUserId = job.data.userId;
 
@@ -39,128 +39,100 @@ new job for ${incomingReaction.id} by ${currentUserId}`
 		- actor
 		- context
 		- entity
-	*/
-  const promises = [
-    // get the user who left the reaction
-    fetchPayload('USER', incomingReaction.userId),
-    // get the message the reaction was left on
-    fetchPayload('MESSAGE', incomingReaction.messageId),
-    // create an entity payload with the incoming reaction
-    createPayload('REACTION', incomingReaction),
-  ];
+  */
+  const actor = await fetchPayload('USER', incomingReaction.userId);
+  const context = await fetchPayload('MESSAGE', incomingReaction.messageId);
+  const entity = await createPayload('REACTION', incomingReaction);
 
   // Check to see if an existing notif exists by matching the 'event' type, with the context of the notification, within a certain time period.
-  return checkForExistingNotification(
+  const notification = await checkForExistingNotification(
     'REACTION_CREATED',
     incomingReaction.messageId
-  )
-    .then(notification => {
-      if (notification) {
-        debug('found existing notification');
-        // if an existing notification exists, update it with the newest actor + entities
-        return Promise.all([
-          notification,
-          ...promises,
-        ]).then(([notification, actor, context, entity]) => {
-          // actors should always be distinct to make client side rendering easier
-          const distinctActors = getDistinctActors([
-            ...notification.actors,
-            actor,
-          ]);
+  );
 
-          // create a new notification
-          const newNotification = Object.assign({}, notification, {
-            actors: [...distinctActors],
-            context,
-            entities: [...notification.entities, entity],
-          });
+  if (notification) {
+    // if an existing notification exists, update it with the newest actor + entities
+    debug('found existing notification');
 
-          debug('update existing notification in database with new data');
+    // actors should always be distinct to make client side rendering easier
+    const distinctActors = getDistinctActors([...notification.actors, actor]);
 
-          // update the notification in the db
-          return updateNotification(newNotification)
-            .then(notification => {
-              // get the original message where the reaction was left
-              const message = getMessageById(notification.context.id);
+    // create a new notification
+    const newNotification = Object.assign({}, notification, {
+      actors: [...distinctActors],
+      context,
+      entities: [...notification.entities, entity],
+    });
 
-              debug('get message');
+    debug('update existing notification in database with new data');
 
-              return Promise.all([notification, message]);
-            })
-            .then(([notification, message]) => {
-              // make sure that the person who left the original message still has notification permissions in this thread. We have to check the threadtype to determine if the reaction was left in a story thread or a direct message thread
-              // TODO: In the future we'll want reactions in direct message threads to trigger push notifications, but for now it introduces too much complexity so we just say false
-              const hasPermission =
-                message.threadType === 'story'
-                  ? getUserNotificationPermissionsInThread(
-                      message.senderId,
-                      message.threadId
-                    )
-                  : false;
+    // update the notification in the db
+    const updatedNotification = await updateNotification(newNotification);
 
-              debug('check permission of sender');
+    // get the original message where the reaction was left
+    const message = await getMessageById(updatedNotification.context.id);
+    debug('get message');
 
-              return Promise.all([notification, message, hasPermission]);
-            })
-            .then(([notification, message, hasPermission]) => {
-              if (!hasPermission) return;
+    // make sure that the person who left the original message still has notification permissions in this thread. We have to check the threadtype to determine if the reaction was left in a story thread or a direct message thread
+    // TODO: In the future we'll want reactions in direct message threads to trigger push notifications, but for now it introduces too much complexity so we just say false
+    const hasPermission =
+      message.threadType === 'story'
+        ? await getUserNotificationPermissionsInThread(
+            message.senderId,
+            message.threadId
+          )
+        : false;
 
-              debug('mark notification as new for sender');
-              // if the user is allowed to recieve notifications, update their notification
-              return markUsersNotificationsAsNew(
-                notification.id,
-                message.senderId
-              );
-            });
-        });
-      } else {
-        // if no notification was found that matches our bundling criteria, create a new notification
-        return Promise.all([...promises]).then(([actor, context, entity]) => {
-          // create the notification record
-          const notification = {
-            actors: [actor],
-            event: 'REACTION_CREATED',
-            context,
-            entities: [entity],
-          };
+    debug('check permission of sender');
 
-          debug('create notification in db');
+    if (!hasPermission) return;
 
-          return storeNotification(notification)
-            .then(notification => {
-              // get the original message where the reaction was left
-              const message = getMessageById(notification.context.id);
+    debug('mark notification as new for sender');
 
-              debug('get message');
-
-              return Promise.all([notification, message]);
-            })
-            .then(([notification, message]) => {
-              // make sure that the person who left the original message still has notification permissions in this thread. We have to check the threadtype to determine if the reaction was left in a story thread or a direct message thread
-              // TODO: In the future we'll want reactions in direct message threads to trigger push notifications, but for now it introduces too much complexity so we just say false
-              const hasPermission =
-                message.threadType === 'story'
-                  ? getUserNotificationPermissionsInThread(
-                      message.senderId,
-                      message.threadId
-                    )
-                  : false;
-
-              debug('check permission of sender');
-
-              return Promise.all([notification, message, hasPermission]);
-            })
-            .then(([notification, message, hasPermission]) => {
-              if (!hasPermission) return;
-
-              debug('mark notification as new for sender');
-              return storeUsersNotifications(notification.id, message.senderId);
-            });
-        });
-      }
-    })
-    .catch(err => {
+    // if the user is allowed to recieve notifications, update their notification
+    return Promise.all([
+      markUsersNotificationsAsNew(updatedNotification.id, message.senderId),
+    ]).catch(err => {
       Raven.captureException(err);
       console.log(err);
     });
+  } else {
+    // if no notification was found that matches our bundling criteria, create a new notification
+    // create the notification record
+    const notification = {
+      actors: [actor],
+      event: 'REACTION_CREATED',
+      context,
+      entities: [entity],
+    };
+    debug('create notification in db');
+
+    const updatedNotification = await storeNotification(notification);
+
+    // get the original message where the reaction was left
+    const message = await getMessageById(updatedNotification.context.id);
+    debug('get message');
+
+    // make sure that the person who left the original message still has notification permissions in this thread. We have to check the threadtype to determine if the reaction was left in a story thread or a direct message thread
+    // TODO: In the future we'll want reactions in direct message threads to trigger push notifications, but for now it introduces too much complexity so we just say false
+    const hasPermission =
+      message.threadType === 'story'
+        ? await getUserNotificationPermissionsInThread(
+            message.senderId,
+            message.threadId
+          )
+        : false;
+
+    debug('check permission of sender');
+    if (!hasPermission) return;
+
+    debug('mark notification as new for sender');
+
+    return Promise.all([
+      storeUsersNotifications(updatedNotification.id, message.senderId),
+    ]).catch(err => {
+      Raven.captureException(err);
+      console.log(err);
+    });
+  }
 };
