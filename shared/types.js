1b54fed8bb77e29c00bf654f0941947bbdf9872c
Brian Lovin <briandlovin@gmail.com>
2017-11-08 14:39:11 -0800

    Type checking

--- shared/types.js
+++ shared/types.js
@@ -69,10 +69,33 @@ export type DBMessage = {
   timestamp: Date,
 };
 
+export type NotificationPayloadType =
+  | 'REACTION'
+  | 'MESSAGE'
+  | 'THREAD'
+  | 'CHANNEL'
+  | 'COMMUNITY'
+  | 'USER'
+  | 'DIRECT_MESSAGE_THREAD';
+
+export type NotificationEventType =
+  | 'REACTION_CREATED'
+  | 'MESSAGE_CREATED'
+  | 'THREAD_CREATED'
+  | 'THREAD_EDITED'
+  | 'CHANNEL_CREATED'
+  | 'DIRECT_MESSAGE_THREAD_CREATED'
+  | 'USER_JOINED_COMMUNITY'
+  | 'USER_REQUESTED_TO_JOIN_PRIVATE_CHANNEL'
+  | 'USER_APPROVED_TO_JOIN_PRIVATE_CHANNEL'
+  | 'THREAD_LOCKED_BY_OWNER'
+  | 'THREAD_DELETED_BY_OWNER'
+  | 'COMMUNITY_INVITATION';
+
 type NotificationPayload = {
   id: string,
   payload: string,
-  type: string,
+  type: NotificationPayloadType,
 };
 export type DBNotification = {
   id: string,
@@ -80,7 +103,7 @@ export type DBNotification = {
   context: NotificationPayload,
   createdAt: Date,
   entities: Array<NotificationPayload>,
-  event: string,
+  event: NotificationEventType,
   modifiedAt: Date,
 };
 
