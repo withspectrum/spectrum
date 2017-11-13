4669162f413222dc2f14e480662cb99243618734
uberbryn <hi@bryn.io>
2017-11-13 14:53:32 -0800

    adjusted byline styling a little bit.

--- src/components/messageGroup/style.js
+++ src/components/messageGroup/style.js
@@ -34,27 +34,32 @@ export const MessageGroup = styled.div`
 
 export const Byline = styled.span`
   display: flex;
-  font-size: 11px;
+  font-size: 13px;
   line-height: 16px;
   font-weight: 700;
+  flex-direction: row;
+  align-items: center;
   margin-bottom: 0;
   -webkit-user-select: none; /* Chrome/Safari */
   -moz-user-select: none; /* Firefox */
   -ms-user-select: none; /* IE10+ */
   color: ${props =>
-    props.op ? props.theme.brand.default : props.theme.text.alt};
+    props.op ? props.theme.brand.default : props.theme.text.default};
   max-width: 100%;
 `;
 
 export const Name = styled.span`
-  margin-right: 4px;
+  margin-right: 2px;
   &:hover {
     color: ${({ theme }) => theme.brand.default};
     cursor: pointer;
   }
 `;
 
-export const Username = styled(Name)`font-weight: 400;`;
+export const Username = styled(Name)`
+  font-weight: 500;
+  color: ${props => props.theme.text.alt};
+`;
 
 export const Timestamp = styled(HorizontalRule)`
   margin: 16px 0 8px;
