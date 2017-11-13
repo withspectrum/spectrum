908d016bcb865621fe83b0f527809294379bf83f
Brian Lovin <briandlovin@gmail.com>
2017-11-13 11:17:45 -0800

    Refactor auth logic

---
+++ src/views/authViewHandler/index.js
@@ -0,0 +1,93 @@
+// @flow
+import * as React from 'react';
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import { getCurrentUserProfile, editUserMutation } from '../../api/user';
+import { saveUserDataToLocalStorage } from '../../actions/authentication';
+import { removeItemFromStorage } from '../../helpers/localStorage';
+import NewUserOnboarding from '../../views/newUserOnboarding';
+
+type Props = {
+  currentUser?: Object,
+  dispatch: Function,
+  match: Object,
+  history: Object,
+  editUser: Function,
+  data: {
+    user: {
+      id: string,
+    },
+  },
+};
+
+type State = {
+  showNewUserOnboarding: boolean,
+};
+
+class AuthViewHandler extends React.Component<Props, State> {
+  state = {
+    showNewUserOnboarding: false,
+  };
+
+  setNewUserOnboarding = val => {
+    return this.setState({
+      showNewUserOnboarding: val,
+    });
+  };
+
+  componentDidUpdate(prev) {
+    const { data: { user }, editUser, dispatch, history, match } = this.props;
+    // if no user was found, escape
+
+    if (!user) {
+      // clear localstorage first
+      return removeItemFromStorage('spectrum');
+    }
+
+    if (prev.data.user !== user && user !== null) {
+      // user has no timezone set
+      if (!user.timezone) {
+        editUser({ timezone: new Date().getTimezoneOffset() * -1 });
+      }
+
+      // save the user data to localstorage
+      dispatch(saveUserDataToLocalStorage(user));
+
+      // if the user doesn't have a username, show the onboarding
+      if (!user.username) {
+        this.setNewUserOnboarding(true);
+      }
+
+      // if the user lands on /home, it means they just logged in. If this code
+      // runs, we know a user was returned successfully and set to localStorage,
+      // so we can redirect to the root url
+      if (match.url === '/home') {
+        history.push('/');
+      }
+    }
+  }
+
+  render() {
+    const { currentUser } = this.props;
+    const { showNewUserOnboarding } = this.state;
+
+    if (showNewUserOnboarding)
+      return (
+        <NewUserOnboarding
+          close={() => this.setNewUserOnboarding(false)}
+          currentUser={currentUser}
+          noCloseButton
+        />
+      );
+
+    return null;
+  }
+}
+
+const map = state => ({ currentUser: state.users.currentUser });
+export default compose(
+  // $FlowIssue
+  connect(map),
+  getCurrentUserProfile,
+  editUserMutation
+)(AuthViewHandler);
