75736df8fcb40913f58a6c38a203ce7c997fccb7
uberbryn <hi@bryn.io>
2017-11-21 23:13:43 -0800

    creating a pricing page

---
+++ src/views/splash/pricing.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { track } from '../../helpers/events';
+import { storeItem, getItemFromStorage } from '../../helpers/localStorage';
+import {
+  Overview,
+  Centralized,
+  CommunitySearch,
+  Chat,
+  Sell,
+  Yours,
+  PageFooter,
+} from './view';
+import { Wrapper } from './style';
+
+class Pricing extends Component {
+  state: {
+    preferredSigninMethod: string,
+  };
+
+  constructor() {
+    super();
+
+    const preferredSigninMethod = getItemFromStorage('preferred_signin_method');
+
+    this.state = {
+      preferredSigninMethod,
+    };
+  }
+
+  componentDidMount() {
+    track('pricing', 'viewed', null);
+  }
+
+  trackSignin = (type, method) => {
+    track('pricing', 'logged in', type);
+    storeItem('preferred_signin_method', method);
+  };
+
+  render() {
+    return (
+      <Wrapper data-e2e-id="pricing-page">
+        <Overview />
+        <Centralized />
+        <CommunitySearch />
+        <Chat />
+        <Sell />
+        <Yours />
+        <PageFooter />
+      </Wrapper>
+    );
+  }
+}
+export default Pricing;
