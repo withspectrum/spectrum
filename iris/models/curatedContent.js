24e0af427bb2af49c9cc1a0b99bbd0c3e74492f2
Brian Lovin <briandlovin@gmail.com>
2017-11-29 15:56:53 -0800

    Remove unneeded function

--- iris/models/curatedContent.js
+++ iris/models/curatedContent.js
@@ -13,12 +13,3 @@ export const getCuratedCommunities = (
     .then(results => (results && results.length > 0 ? results[0] : null))
     .then(result => result && getCommunitiesBySlug(result.data));
 };
-
-export const getTopCommunities = (): Promise<Array<DBCommunity>> => {
-  return db
-    .table('curatedContent')
-    .filter({ type: 'top-communities-by-members' })
-    .map(community => community('id'))
-    .run()
-    .then(communities => getCommunitiesBySlug(communities));
-};
