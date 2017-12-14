90f26c62f1d7001e9db5f7c996e487f7381ad7e9
Brian Lovin <briandlovin@gmail.com>
2017-12-14 10:48:31 -0800

    Remove flow type

--- shared/algolia/index.js
+++ shared/algolia/index.js
@@ -4,7 +4,7 @@ var ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
 var ALGOLIA_API_SECRET = process.env.ALGOLIA_API_SECRET;
 var algoliasearch = require('algoliasearch');
 var algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_SECRET);
-var initIndex = function(index: string) {
+var initIndex = function(index) {
   return algolia.initIndex(IS_PROD ? index : 'dev_' + index);
 };
 module.exports = initIndex;
