693476eeea9d9a66a255d4c4a5fa7b9d93cea596
Brian Lovin <briandlovin@gmail.com>
2017-12-14 10:47:17 -0800

    Attempt to convert shared filed to not use es5

--- shared/algolia/index.js
+++ shared/algolia/index.js
@@ -1,9 +1,10 @@
-// @flow
 require('now-env');
-const IS_PROD = process.env.NODE_ENV === 'production';
-const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
-const ALGOLIA_API_SECRET = process.env.ALGOLIA_API_SECRET;
-const algoliasearch = require('algoliasearch');
-const algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_SECRET);
-export default (index: string) =>
-  algolia.initIndex(IS_PROD ? index : 'dev_' + index);
+var IS_PROD = process.env.NODE_ENV === 'production';
+var ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+var ALGOLIA_API_SECRET = process.env.ALGOLIA_API_SECRET;
+var algoliasearch = require('algoliasearch');
+var algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_SECRET);
+var initIndex = function(index: string) {
+  return algolia.initIndex(IS_PROD ? index : 'dev_' + index);
+};
+module.exports = initIndex;
