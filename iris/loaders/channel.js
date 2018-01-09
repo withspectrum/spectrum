9b5a3feeedbf96df0741f0ff24db937fb4f84a1a
Maximilian Stoiber <contact@mxstbr.com>
2018-01-09 11:53:05 +0100

    Disable DataLoader caching for subscriptions
    Due to the DataLoader caching users were getting stale data after the
    first time a subscription sent anything to the client.
    
    This closes #2004

--- iris/loaders/channel.js
+++ iris/loaders/channel.js
@@ -8,17 +8,24 @@ import createLoader from './create-loader';
 import { getPendingUsersInChannels } from '../models/usersChannels';
 import type { Loader } from './types';
 
-export const __createChannelLoader = () =>
-  createLoader(channels => getChannels(channels));
+export const __createChannelLoader = createLoader(channels =>
+  getChannels(channels)
+);
 
-export const __createChannelThreadCountLoader = () =>
-  createLoader(channels => getChannelsThreadCounts(channels), 'group');
+export const __createChannelThreadCountLoader = createLoader(
+  channels => getChannelsThreadCounts(channels),
+  'group'
+);
 
-export const __createChannelMemberCountLoader = () =>
-  createLoader(channels => getChannelsMemberCounts(channels), 'group');
+export const __createChannelMemberCountLoader = createLoader(
+  channels => getChannelsMemberCounts(channels),
+  'group'
+);
 
-export const __createChannelPendingMembersLoader = () =>
-  createLoader(channels => getPendingUsersInChannels(channels), 'group');
+export const __createChannelPendingMembersLoader = createLoader(
+  channels => getPendingUsersInChannels(channels),
+  'group'
+);
 
 export default () => {
   throw new Error(
