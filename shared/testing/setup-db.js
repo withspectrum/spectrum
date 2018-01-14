e8d506ed3cee95f91c58db1df9906c1c4ddc83e0
Maximilian Stoiber <contact@mxstbr.com>
2018-01-14 16:42:27 +0100

    Create separate db for each test suite

--- shared/testing/setup-db.js
+++ shared/testing/setup-db.js
@@ -1,28 +1,35 @@
 import data from './data';
-import db from './db';
+const debug = require('debug')('testing:setup-db');
 const tables = Object.keys(data);
 
-console.log('\nAdding test data to db...');
 /**
  * This is run before all tests
  */
 export const setup = db => {
+  debug('inserting test data into db');
   // Create the tables
   return Promise.all(
-    tables.map(
-      table =>
-        console.log(`Insert data into ${table}`) ||
+    tables.map(table => {
+      debug(`inserting test data into ${table}`);
+      // Soft durability for all tables by default because that's faster, we don't
+      // actually care about writing stuff to disk while testing
+      return (
         db
           .table(table)
-          .insert(data[table])
+          .config()
+          .update({ durability: 'soft' })
           .run()
-    )
+          // Then insert the data
+          .then(() =>
+            db
+              .table(table)
+              .insert(data[table])
+              .run()
+          )
+      );
+    })
   ).catch(err => {
     console.log(err);
     throw err;
   });
 };
-
-setup(db).then(
-  () => console.log('Finished adding test data to db.') || process.exit()
-);
