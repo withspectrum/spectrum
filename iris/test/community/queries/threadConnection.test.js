7dfa28d4d32ca17aa7d33c1185436a40c7dbc29a
Maximilian Stoiber <contact@mxstbr.com>
2018-01-16 19:41:23 +0100

    Test community.threadConnection pagination

--- iris/test/community/queries/threadConnection.test.js
+++ iris/test/community/queries/threadConnection.test.js
@@ -1,10 +1,13 @@
 // @flow
 import { request } from '../../utils';
+import { threads, communities } from 'shared/testing/data';
+
+const community = communities[0];
 
 it('should fetch a communities threads', async () => {
   const query = /* GraphQL */ `
     {
-      community(id: "ce2b4488-4c75-47e0-8ebc-2539c1e6a191") {
+      community(id: "${community.id}") {
         threadConnection {
           edges {
             node {
@@ -18,8 +21,95 @@ it('should fetch a communities threads', async () => {
     }
   `;
 
-  expect.assertions(1);
+  expect.hasAssertions();
   const result = await request(query);
 
   expect(result).toMatchSnapshot();
+});
+
+it('should fetch a communities threads in lastActive order', async () => {
+  const query = /* GraphQL */ `
+    {
+      community(id: "${community.id}") {
+        threadConnection {
+          edges {
+            node {
+              lastActive
+              id
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  expect.hasAssertions();
+  const result = await request(query);
+
+  // Expect the manually by lastActive sorted result to equal the original result
+  expect(
+    result.data.community.threadConnection.edges.sort(
+      (a, b) =>
+        new Date(a.node.lastActive).getTime() - new Date(b.lastActive).getTime()
+    )
+  ).toEqual(result.data.community.threadConnection.edges);
+});
+
+describe('pagination', () => {
+  it('should paginate threads', async () => {
+    expect.hasAssertions();
+    const query = /* GraphQL */ `
+      {
+        community(id: "${community.id}") {
+          threadConnection(first: 1) {
+            edges {
+              cursor
+              node {
+                lastActive
+                id
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const firstThread = await request(query);
+
+    expect(firstThread.data.community.threadConnection.edges.length).toEqual(1);
+    expect(firstThread).toMatchSnapshot();
+
+    // Construct a query with the cursor of the first thread
+    const secondQuery = /* GraphQL */ `
+      {
+        community(id: "${community.id}") {
+          threadConnection(first: 1, after: "${
+            firstThread.data.community.threadConnection.edges[0].cursor
+          }") {
+            edges {
+              cursor
+              node {
+                lastActive
+                id
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const secondThread = await request(secondQuery);
+
+    expect(secondThread).not.toEqual(firstThread);
+    expect(
+      new Date(
+        secondThread.data.community.threadConnection.edges[0].node.lastActive
+      ).getTime()
+    ).toBeLessThan(
+      new Date(
+        firstThread.data.community.threadConnection.edges[0].node.lastActive
+      ).getTime()
+    );
+    expect(secondThread).toMatchSnapshot();
+  });
 });
