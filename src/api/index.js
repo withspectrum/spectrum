d386d3c500b7f3cc4dd60fc155ebd1f00b888e14
Maximilian Stoiber <contact@mxstbr.com>
2018-01-17 18:47:01 +0100

    Upload + subscriptions done

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,16 @@
 // @flow
 import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
+import { createUploadLink } from 'apollo-upload-client';
 import {
   InMemoryCache,
   IntrospectionFragmentMatcher,
 } from 'apollo-cache-inmemory';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import introspectionQueryResultData from './schema.json';
 import getSharedApolloClientOptions from 'shared/graphql/apollo-client-options';
 
-// import { createBatchingNetworkInterface } from 'apollo-upload-client';
 // import {
 //   SubscriptionClient,
 //   addGraphQLSubscriptions,
@@ -17,6 +19,7 @@ import getSharedApolloClientOptions from 'shared/graphql/apollo-client-options';
 const IS_PROD = process.env.NODE_ENV === 'production';
 // In production the API is at the same URL, in development it's at a different port
 const API_URI = IS_PROD ? '/api' : 'http://localhost:3001/api';
+
 const cache = new InMemoryCache({
   fragmentMatcher: new IntrospectionFragmentMatcher({
     introspectionQueryResultData,
@@ -24,11 +27,34 @@ const cache = new InMemoryCache({
   ...getSharedApolloClientOptions(),
 });
 
+// HTTP Link for queries and mutations including file uploads
+const httpLink = createUploadLink({
+  uri: API_URI,
+  credentials: 'include',
+});
+
+// Websocket link for subscriptions
+const wsLink = new WebSocketLink({
+  uri: `${
+    IS_PROD ? `wss://${window.location.host}` : 'ws://localhost:3001'
+  }/websocket`,
+  options: {
+    reconnect: true,
+  },
+});
+
+// Switch between the two links based on operation
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: API_URI,
-    credentials: 'include',
-  }),
+  link,
   cache: window.__SERVER_STATE__
     ? cache.restore(window.__SERVER_STATE__.apollo)
     : cache,
@@ -37,36 +63,6 @@ const client = new ApolloClient({
 });
 
 export { client };
-
-// const networkInterface = createBatchingNetworkInterface({
-//   uri: API_URI,
-//   batchInterval: 10,
-//   opts: {
-//     credentials: 'include',
-//   },
-// });
-// const wsClient = new SubscriptionClient(
-//   `${IS_PROD
-//     ? `wss://${window.location.host}`
-//     : 'ws://localhost:3001'}/websocket`,
-//   { reconnect: true }
-// );
-
-// const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-//   networkInterface,
-//   wsClient
-// );
-
-// const
-// export const client = new ApolloClient({
-//   networkInterface: networkInterfaceWithSubscriptions,
-//   fragmentMatcher,
-//   initialState: window.__SERVER_STATE__ && {
-//     apollo: window.__SERVER_STATE__.apollo,
-//   },
-//   ssrForceFetchDelay: 100,
-//   ...getSharedApolloClientOptions(),
-// });
 
 export const clearApolloStore = () => {
   try {
