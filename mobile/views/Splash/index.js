ea2e6f5246ecc56116823c6d4737163966c7a56f
Maximilian Stoiber <contact@mxstbr.com>
2018-01-19 11:12:35 +0100

    WIP: Mobile authentication
    This does not yet work because I don't get access to the returned
    cookies from the sign in process. Gotta figure out how to get those to
    pass to Apollo Client!

--- mobile/views/Splash/index.js
+++ mobile/views/Splash/index.js
@@ -2,18 +2,50 @@
 import * as React from 'react';
 import { Text, View, Button } from 'react-native';
 import compose from 'recompose/compose';
+import { AuthSession } from 'expo';
 import withSafeView from '../../components/SafeAreaView';
 
 import { Wrapper } from './style';
+
+const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://spectrum.chat'
+    : 'http://localhost:3001';
 
 type Props = {
   navigation: Object,
 };
 class Splash extends React.Component<Props> {
+  authenticate = (provider: 'twitter' | 'facebook' | 'google') => async () => {
+    const redirectUrl = AuthSession.getRedirectUrl();
+    const result = await AuthSession.startAsync({
+      authUrl: `${API_URL}/auth/${provider}?r=${redirectUrl}`,
+    });
+    if (result.type === 'error') {
+      // Do something with result.errorCode and result.event
+    }
+    if (result.type === 'success') {
+      // Yay we're signed in!
+    }
+    // The request was cancelled by the user
+  };
+
   render() {
     return (
       <Wrapper>
         <View testID="welcome">
+          <Button
+            title="Login/Signup with Twitter"
+            onPress={this.authenticate('twitter')}
+          />
+          <Button
+            title="Login/Signup with Google"
+            onPress={this.authenticate('google')}
+          />
+          <Button
+            title="Login/Signup with Facebook"
+            onPress={this.authenticate('facebook')}
+          />
           <Button
             title={'thread'}
             onPress={() =>
