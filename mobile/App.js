b96e3cdac894e82680de106bca4a8d1c304da5d6
Maximilian Stoiber <contact@mxstbr.com>
2018-01-19 10:40:19 +0100

    Add Sentry to mobile apps
     Closes #2301

--- mobile/App.js
+++ mobile/App.js
@@ -1,4 +1,5 @@
 // @flow
+import Sentry from 'sentry-expo';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { ThemeProvider } from 'styled-components';
@@ -6,6 +7,10 @@ import { ThemeProvider } from 'styled-components';
 import theme from './components/theme';
 import { client } from '../shared/graphql';
 import TabBar from './views/TabBar';
+
+Sentry.config(
+  'https://3bd8523edd5d43d7998f9b85562d6924@sentry.io/154812'
+).install();
 
 const App = () => (
   <ApolloProvider client={client}>
