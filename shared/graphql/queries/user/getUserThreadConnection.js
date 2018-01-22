669651aa8c458b32e5db34cbc9227e7f65b2aca9
Brian Lovin <briandlovin@gmail.com>
2018-01-22 15:37:45 +1000

    Fix user profile threadConnection query

--- shared/graphql/queries/user/getUserThreadConnection.js
+++ shared/graphql/queries/user/getUserThreadConnection.js
@@ -46,93 +46,92 @@ const getUserThreadConnectionQuery = gql`
 const getUserThreadConnectionOptions = {
   props: ({
     ownProps,
-    data: { fetchMore, error, loading, user, networkStatus, subscribeToMore },
-    data,
-  }) =>
-    console.log('data', data) || {
-      data: {
-        error,
-        loading,
-        networkStatus,
-        user,
-        threads: user ? console.log(user) || user.threadConnection.edges : [],
-        hasNextPage: user ? user.threadConnection.pageInfo.hasNextPage : false,
-        feed: user && user.id,
-        subscribeToUpdatedThreads: () => {
-          return subscribeToMore({
-            document: subscribeToUpdatedThreads,
-            updateQuery: (prev, { subscriptionData }) => {
-              const updatedThread = subscriptionData.data.threadUpdated;
-              if (!updatedThread) return prev;
+    data: { fetchMore, error, loading, networkStatus, user, subscribeToMore },
+  }) => ({
+    data: {
+      error,
+      loading,
+      user,
+      networkStatus,
+      threads: user ? user.threadConnection.edges : '',
+      hasNextPage: user ? user.threadConnection.pageInfo.hasNextPage : false,
+      subscribeToUpdatedThreads: () => {
+        return subscribeToMore({
+          document: subscribeToUpdatedThreads,
+          updateQuery: (prev, { subscriptionData }) => {
+            const updatedThread = subscriptionData.data.threadUpdated;
+            if (!updatedThread) return prev;
 
-              const thisuserId = ownProps.user.id;
-              const updatedThreadShouldAppearInContext =
-                thisuserId === updatedThread.user.id;
+            const thisUserId = ownProps.userId;
+            const updatedThreadShouldAppearInContext =
+              thisUserId === updatedThread.creator.id;
 
-              const newThreads = updatedThreadShouldAppearInContext
-                ? parseRealtimeThreads(
-                    prev.user.threadConnection.edges,
-                    updatedThread
-                  ).filter(thread => thread.node.user.id === thisuserId)
-                : [...prev.user.threadConnection.edges];
+            const newThreads = updatedThreadShouldAppearInContext
+              ? parseRealtimeThreads(
+                  prev.user.threadConnection.edges,
+                  updatedThread,
+                  ownProps.dispatch
+                ).filter(thread => thread.node.creator.id === thisUserId)
+              : [...prev.user.threadConnection.edges];
 
-              return {
-                ...prev,
-                user: {
-                  ...prev.user,
-                  threadConnection: {
-                    ...prev.user.threadConnection,
-                    pageInfo: {
-                      ...prev.user.threadConnection.pageInfo,
-                    },
-                    edges: newThreads,
+            return {
+              ...prev,
+              user: {
+                ...prev.user,
+                threadConnection: {
+                  ...prev.user.threadConnection,
+                  pageInfo: {
+                    ...prev.user.threadConnection.pageInfo,
                   },
+                  edges: newThreads,
                 },
-              };
-            },
-          });
-        },
-        fetchMore: () =>
-          fetchMore({
-            query: LoadMoreThreads,
-            variables: {
-              after:
-                user.threadConnection.edges[
-                  user.threadConnection.edges.length - 1
-                ].cursor,
-              username: user.username,
-            },
-            updateQuery: (prev, { fetchMoreResult }) => {
-              if (!fetchMoreResult.user) {
-                return prev;
-              }
-              return {
-                ...prev,
-                user: {
-                  ...prev.user,
-                  threadConnection: {
-                    ...prev.user.threadConnection,
-                    pageInfo: {
-                      ...prev.user.threadConnection.pageInfo,
-                      ...fetchMoreResult.user.threadConnection.pageInfo,
-                    },
-                    edges: [
-                      ...prev.user.threadConnection.edges,
-                      ...fetchMoreResult.user.threadConnection.edges,
-                    ],
-                  },
-                },
-              };
-            },
-          }),
+              },
+            };
+          },
+        });
       },
+      fetchMore: () =>
+        fetchMore({
+          query: LoadMoreThreads,
+          variables: {
+            after:
+              user.threadConnection.edges[
+                user.threadConnection.edges.length - 1
+              ].cursor,
+            id: user.id,
+            kind: ownProps.kind,
+          },
+          updateQuery: (prev, { fetchMoreResult }) => {
+            if (!fetchMoreResult.user) {
+              return prev;
+            }
+            return {
+              ...prev,
+              user: {
+                ...prev.user,
+                threadConnection: {
+                  ...prev.user.threadConnection,
+                  pageInfo: {
+                    ...prev.user.threadConnection.pageInfo,
+                    ...fetchMoreResult.user.threadConnection.pageInfo,
+                  },
+                  edges: [
+                    ...prev.user.threadConnection.edges,
+                    ...fetchMoreResult.user.threadConnection.edges,
+                  ],
+                },
+              },
+            };
+          },
+        }),
     },
+  }),
   options: ({ id, kind }) => ({
     variables: {
       id,
       kind,
     },
-    fetchPolicy: 'cache-and-network',
+    fetchPolicy: 'cache-first',
   }),
 };
 
