cc77a629b33f1def26459d2a3faca9d0fc673df8
Maximilian Stoiber <contact@mxstbr.com>
2018-02-03 10:55:28 +0100

    Cleanup

--- shared/middlewares/cors.js
+++ shared/middlewares/cors.js
@@ -1,13 +1,17 @@
 // @flow
 import cors from 'cors';
 
+// Only these domains can query the API
 export default cors({
   origin:
     process.env.NODE_ENV === 'production'
       ? [
+          // Main site
           'https://spectrum.chat',
+          // Deployment URL
           process.env.NOW_URL || undefined,
-          /spectrum\.chat$/,
+          // Any spectrum.chat subdomain
+          /\.spectrum\.chat$/,
         ].filter(Boolean)
       : [/localhost/],
   credentials: true,
