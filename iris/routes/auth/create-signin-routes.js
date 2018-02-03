157a35d869712944502301e2e56753e9914029a8
Maximilian Stoiber <contact@mxstbr.com>
2018-02-03 11:43:48 +0100

    Refactor signin routes; send tokens as access_token param
    @brianlovin I refactored the signin routes to a reusable util since they
    were all the same and we were forgetting to update them with the changes.

---
+++ iris/routes/auth/create-signin-routes.js
@@ -0,0 +1,78 @@
+/* @flow
+ *
+ * A reusable set of routes for signing in with different providers. Handles token-based authentication.
+ * Usage:
+ *
+ * const { main, callbacks } = createSigninRoutes('facebook');
+ * facebookRouter.get('/', main);
+ * facebookRouter.get('/callback', ...callbacks);
+ */
+import passport from 'passport';
+import { URL } from 'url';
+import isSpectrumUrl, { isExpoUrl } from '../../utils/is-spectrum-url';
+
+const IS_PROD = process.env.NODE_ENV === 'production';
+const FALLBACK_URL = IS_PROD
+  ? 'https://spectrum.chat/home'
+  : 'http://localhost:3000/home';
+
+type Strategy = 'twitter' | 'facebook' | 'github' | 'google';
+
+export const createSigninRoutes = (
+  strategy: Strategy,
+  strategyOptions?: Object
+) => {
+  return {
+    // The main route takes care of storing the redirect URL in the session
+    // and passing the right options
+    main: (req: express$Request, ...rest: any) => {
+      let url = FALLBACK_URL;
+      if (typeof req.query.r === 'string' && isSpectrumUrl(req.query.r)) {
+        url = req.query.r;
+      }
+
+      // Attach the redirectURL to the session so we have it in the /auth/twitter/callback route
+      // $FlowIssue
+      req.session.redirectUrl = url;
+
+      return passport.authenticate(strategy, strategyOptions)(req, ...rest);
+    },
+    // The callbacks take care of authenticating, setting the response cookies,
+    // redirecting to the right place and handling tokens
+    callbacks: [
+      passport.authenticate(strategy, {
+        failureRedirect: IS_PROD ? '/' : 'http://localhost:3000/',
+      }),
+      (req: express$Request, res: express$Response) => {
+        // req.session.redirectURL is set in the /auth/twitter route
+        if (!req.session || !req.session.redirectUrl)
+          return res.redirect(FALLBACK_URL);
+
+        // $FlowIssue
+        const redirectUrl = new URL(req.session.redirectUrl);
+        redirectUrl.searchParams.append('authed', 'true');
+
+        // Add the session cookies to the query params if authenticating from mobile
+        if (
+          // $FlowIssue
+          isExpoUrl(req.session.redirectUrl) &&
+          req.cookies &&
+          req.cookies.session
+        ) {
+          redirectUrl.searchParams.append(
+            'access_token',
+            `session=${req.cookies.session}; session.sig=${
+              req.cookies['session.sig']
+            }`
+          );
+        }
+
+        // Delete the redirectURL from the session again so we don't redirect
+        // to the old URL the next time around
+        // $FlowIssue
+        req.session.redirectUrl = undefined;
+        return res.redirect(redirectUrl.href);
+      },
+    ],
+  };
+};
--- iris/routes/auth/google.js
+++ iris/routes/auth/google.js
@@ -1,67 +1,17 @@
+// @flow
 import { Router } from 'express';
-import passport from 'passport';
-import { URL } from 'url';
-import isSpectrumUrl, { isExpoUrl } from '../../utils/is-spectrum-url';
-
-const IS_PROD = process.env.NODE_ENV === 'production';
-const FALLBACK_URL = IS_PROD
-  ? 'https://spectrum.chat/home'
-  : 'http://localhost:3000/home';
+import { createSigninRoutes } from './create-signin-routes';
 
 const googleAuthRouter = Router();
-
-// Redirect the user to Twitter for authentication. When complete, Twitter
-// will redirect the user back to the application at /auth/twitter/callback
-googleAuthRouter.get('/', (req, ...rest) => {
-  let url = FALLBACK_URL;
-  if (req.query.r && isSpectrumUrl(req.query.r)) {
-    url = req.query.r;
-  }
-
-  // Attach the redirectURL to the session so we have it in the /auth/twitter/callback route
-  req.session.redirectUrl = url;
-
-  return passport.authenticate('google', {
-    scope: [
-      'https://www.googleapis.com/auth/plus.login',
-      'https://www.googleapis.com/auth/plus.profile.emails.read',
-    ],
-  })(req, ...rest);
+const { main, callbacks } = createSigninRoutes('google', {
+  scope: [
+    'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read',
+  ],
 });
 
-// Twitter will redirect the user to this URL after approval. Finish the
-// authentication process by attempting to obtain an access token. If
-// access was granted, the user will be logged in. Otherwise, authentication has failed.
-googleAuthRouter.get(
-  '/callback',
-  passport.authenticate('google', {
-    failureRedirect: IS_PROD ? '/' : 'http://localhost:3000/',
-  }),
-  (req, res) => {
-    // req.session.redirectURL is set in the /auth/twitter route
-    if (!req.session.redirectUrl) return res.redirect(FALLBACK_URL);
+googleAuthRouter.get('/', main);
 
-    const redirectUrl = new URL(req.session.redirectUrl);
-    redirectUrl.searchParams.append('authed', 'true');
-
-    // Add the session cookies to the query params if authenticating from mobile
-    if (
-      isExpoUrl(req.session.redirectUrl) &&
-      req.cookies &&
-      req.cookies.session
-    ) {
-      redirectUrl.searchParams.append('session', req.cookies.session);
-      redirectUrl.searchParams.append(
-        'session.sig',
-        req.cookies['session.sig']
-      );
-    }
-
-    // Delete the redirectURL from the session again so we don't redirect
-    // to the old URL the next time around
-    req.session.redirectUrl = undefined;
-    return res.redirect(redirectUrl.href);
-  }
-);
+googleAuthRouter.get('/callback', ...callbacks);
 
 export default googleAuthRouter;
