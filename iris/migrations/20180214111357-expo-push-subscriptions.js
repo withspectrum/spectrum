f0ffe0020d02e027da6ba66b61d40c10504976f3
Maximilian Stoiber <contact@mxstbr.com>
2018-02-14 12:15:31 +0100

    Add expoPushSubscriptions table

---
+++ iris/migrations/20180214111357-expo-push-subscriptions.js
@@ -0,0 +1,25 @@
+'use strict';
+
+exports.up = function(r, conn) {
+  return r
+    .tableCreate('expoPushSubscriptions')
+    .run(conn)
+    .then(() =>
+      r
+        .table('expoPushSubscriptions')
+        .indexCreate('userId')
+        .run(conn)
+    )
+    .catch(err => {
+      throw new Error(err);
+    });
+};
+
+exports.down = function(r, conn) {
+  return r
+    .tableDrop('expoPushSubscriptions')
+    .run(conn)
+    .catch(err => {
+      throw new Error(err);
+    });
+};
