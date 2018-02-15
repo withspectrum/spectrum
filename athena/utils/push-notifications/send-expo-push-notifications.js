eeca210a71ccfac4a2a0b04dee507d6c113b24c2
Maximilian Stoiber <contact@mxstbr.com>
2018-02-15 14:09:36 +0100

    WIP error handling in the push sub util

--- athena/utils/push-notifications/send-expo-push-notifications.js
+++ athena/utils/push-notifications/send-expo-push-notifications.js
@@ -1,6 +1,8 @@
 // @flow
 const debug = require('debug')('athena:utils:send-expo-push-notifications');
 import Expo from 'expo-server-sdk';
+import { removeExpoSubscription } from 'iris/models/expo-push-subscription';
+import Raven from 'shared/raven';
 import type { DBExpoPushSubscription } from 'shared/types';
 
 const expo = new Expo();
@@ -32,6 +34,7 @@ export const sendExpoPushNotifications = (
     // TODO(@mxstbr): Remove invalid subscriptions from db
     if (!Expo.isExpoPushToken(token)) {
       debug(`invalid expo token ${token}, not sending push notification.`);
+      removeExpoSubscription(token);
       return;
     }
 
@@ -49,6 +52,30 @@ export const sendExpoPushNotifications = (
 
   const chunks = expo.chunkPushNotifications(messages);
 
-  // TODO(@mxstbr): Catch errors here and remove subscriptions, see https://docs.expo.io/versions/latest/guides/push-notifications.html#response-format
-  return chunks.map(chunk => expo.sendPushNotificationAsync(chunk));
+  return chunks.map(chunk =>
+    expo
+      .sendPushNotificationAsync(chunk)
+      .then(receipts => {
+        if (!receipts.data || receipts.data.length === 0) return;
+        return receipts.data
+          .filter(({ status }) => status === 'error')
+          .map(receipt => {
+            if (receipt.details && receipt.details.error) {
+              switch (receipt.details.error) {
+                case 'DeviceNotRegistered': {
+                  // TODO(@mxstbr): Remove the subscription here
+                  // This is blocked upstream because the expo push service doesn't return the token
+                }
+                default: {
+                  return Promise.resolve();
+                }
+              }
+            }
+          });
+      })
+      .catch(err => {
+        // This means Expo is down or something
+        Raven.captureException(err);
+      })
+  );
 };
