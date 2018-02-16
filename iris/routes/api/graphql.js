34b1f827976491b0a7a300d686de892a38237d9d
Maximilian Stoiber <contact@mxstbr.com>
2018-02-16 14:51:39 +0100

    Integrate graphql-cost-analysis
    This is a pretty cool module that will analyze a GraphQL queries cost
    based on custom directives we specify in the types. (will tackle next)

--- iris/routes/api/graphql.js
+++ iris/routes/api/graphql.js
@@ -1,6 +1,7 @@
 // @flow
 import { graphqlExpress } from 'graphql-server-express';
 import depthLimit from 'graphql-depth-limit';
+import costAnalysis from 'graphql-cost-analysis';
 import createLoaders from '../../loaders/';
 
 import createErrorFormatter from '../../utils/create-graphql-error-formatter';
@@ -13,6 +14,13 @@ export default graphqlExpress(req => ({
     user: req.user,
     loaders: createLoaders(),
   },
-  validationRules: [depthLimit(10)],
+  validationRules: [
+    depthLimit(10),
+    costAnalysis({
+      variables: req.body.variables,
+      maximumCost: 750,
+      defaultCost: 1,
+    }),
+  ],
   tracing: true,
 }));
