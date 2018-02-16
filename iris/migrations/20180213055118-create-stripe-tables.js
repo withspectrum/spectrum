20f95cad023c0bb3bcc186f9a0ad8877d567c024
Brian Lovin <briandlovin@gmail.com>
2018-02-15 23:24:04 -0800

    Clean up unneeded tables in migration scripts

--- iris/migrations/20180213055118-create-stripe-tables.js
+++ iris/migrations/20180213055118-create-stripe-tables.js
@@ -3,40 +3,14 @@ require('now-env');
 exports.up = function(r, conn) {
   const createCustomersTable = () =>
     r.tableCreate('stripeCustomers', { primaryKey: 'customerId' }).run(conn);
-  const createSubscriptionsTable = () =>
-    r
-      .tableCreate('stripeSubscriptions', { primaryKey: 'subscriptionId' })
-      .run(conn);
-  const createChargesTable = () =>
-    r.tableCreate('stripeCharges', { primaryKey: 'chargeId' }).run(conn);
   const createInvoicesTable = () =>
     r.tableCreate('stripeInvoices', { primaryKey: 'invoiceId' }).run(conn);
-  const createSourcesTable = () =>
-    r.tableCreate('stripeSources', { primaryKey: 'sourceId' }).run(conn);
 
-  return Promise.all([
-    createCustomersTable(),
-    createSubscriptionsTable(),
-    createChargesTable(),
-    createInvoicesTable(),
-    createSourcesTable(),
-  ])
+  return Promise.all([createCustomersTable(), createInvoicesTable()])
     .then(() =>
       Promise.all([
         r
-          .table('stripeSubscriptions')
-          .indexCreate('customerId')
-          .run(conn),
-        r
-          .table('stripeCharges')
-          .indexCreate('customerId')
-          .run(conn),
-        r
           .table('stripeInvoices')
-          .indexCreate('customerId')
-          .run(conn),
-        r
-          .table('stripeSources')
           .indexCreate('customerId')
           .run(conn),
       ])
@@ -45,12 +19,8 @@ exports.up = function(r, conn) {
 };
 
 exports.down = function(r, conn) {
-  // Not spending any time undoing this
   return Promise.all([
     r.tableDrop('stripeCustomers').run(conn),
-    r.tableDrop('stripeSubscriptions').run(conn),
-    r.tableDrop('stripeCharges').run(conn),
     r.tableDrop('stripeInvoices').run(conn),
-    r.tableDrop('stripeSources').run(conn),
   ]);
 };
