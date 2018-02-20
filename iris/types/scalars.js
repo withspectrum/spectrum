cc9ab0d171b427f74da8cbb70b8acf2b26ad6dbc
Maximilian Stoiber <contact@mxstbr.com>
2018-02-20 10:38:21 +0100

    Add PaginationAmount scalar

--- iris/types/scalars.js
+++ iris/types/scalars.js
@@ -3,14 +3,28 @@
  * Custom scalars (data types, like Int, String,...) live in this file,
  * both their type definitions and their resolvers
  */
-const GraphQLDate = require('graphql-date');
+import GraphQLDate from 'graphql-date';
+import { GraphQLInputInt } from 'graphql-input-number';
+import UserError from '../utils/UserError';
 
 const typeDefs = /* GraphQL */ `
 	scalar Date
+  scalar PaginationAmount
 `;
 
 const resolvers = {
   Date: GraphQLDate,
+  PaginationAmount: GraphQLInputInt({
+    name: 'PaginationAmount',
+    description: 'An Int between 1 and 100, not more and not less.',
+    error: ({ value }) => {
+      throw new UserError(
+        `Invalid amount ${value} specified, has to be more than 1 and less than 100.`
+      );
+    },
+    min: 1,
+    max: 100,
+  }),
 };
 
 module.exports = {
