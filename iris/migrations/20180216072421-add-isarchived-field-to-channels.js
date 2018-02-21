76b3ccb2755022d965541937905d99018d716385
Brian Lovin <briandlovin@gmail.com>
2018-02-20 16:44:35 -0800

    Tweaks to migrations

--- iris/migrations/20180216072421-add-isarchived-field-to-channels.js
+++ iris/migrations/20180216072421-add-isarchived-field-to-channels.js
@@ -1,5 +1,9 @@
 require('now-env');
 
+// creates a new field on channels which indicates it is read-only, which
+// is much easier to write logic around than our current method of always
+// checking to see a communities payments status before determining the
+// writeability of a channel
 exports.up = function(r, conn) {
   return Promise.all([
     r
