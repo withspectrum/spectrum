eeaa4876dd4e3995eebb6866f40102e3672aea4c
Maximilian Stoiber <contact@mxstbr.com>
2018-02-21 14:22:31 +0100

    Move tests

--- shared/clients/draft-js/mentions-decorator/test/mentions-decorator.test.js
+++ shared/clients/draft-js/mentions-decorator/test/mentions-decorator.test.js
@@ -1,6 +1,6 @@
 // @flow
 import { genKey, ContentBlock } from 'draft-js';
-import mentionsDecorator from '../mentions-decorator/index.web.js';
+import mentionsDecorator from '../index.web.js';
 import type { ContentBlock as ContentBlockType } from 'draft-js/lib/ContentBlock';
 
 type Mention = string;
