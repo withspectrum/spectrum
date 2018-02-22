d90853cfffaaca617c89ab3c9f55ff86d5dae487
Maximilian Stoiber <contact@mxstbr.com>
2018-02-22 13:59:30 +0100

    Rework the asyncify utility
    Based on [the code used in grapqhl-subscriptions to convert an
    EventEmitter to an AsyncIterator](https://github.com/apollographql/graphql-subscriptions/blob/master/src/event-emitter-to-async-iterator.ts)
    this patch reworks our `asyncify` util, which is used in all subscriptions.
    
    Rather than having a queue of queues and a single database listener
    (which obviously didn't work and led to memory leaks due to people
    unsubscribing and the queue sticking around) we now listen to changes
    _per user and subscriptions_. This allows us to open new listeners
    whenever a user connects, and close the listener whenever a user
    disconnects.
    
    This should lead to much less memory problems, although it might lead to
    database problems. (we need to test that in prod) If it does lead to
    more frequent connection drops with RethinkDB I'll rework this again to
    only use one database listener for all users.

--- iris/utils/asyncify.js
+++ iris/utils/asyncify.js
@@ -1,34 +1,86 @@
 // @flow
 // Turn a callback-based listener into an async iterator
-const Queue = require('then-queue');
+// Based on https://github.com/apollographql/graphql-subscriptions/blob/master/src/event-emitter-to-async-iterator.ts
+const debug = require('debug')('iris:utils:asyncify');
+import { $$asyncIterator } from 'iterall';
+import Raven from 'shared/raven';
 
-const asyncify = (
-  listener: (...args: any[]) => any,
-  onError?: Error => void
+import type { GraphQLContext } from '../';
+import type { GraphQLResolveInfo } from 'graphql';
+import type { Cursor } from 'rethinkdbdash';
+
+type Listener = ((arg: any) => void) => Promise<Cursor>;
+
+const asyncify = (listener: Listener, onError?: Error => void) => (
+  _: any,
+  __: any,
+  { user }: GraphQLContext,
+  info: GraphQLResolveInfo
 ) => {
-  let queues = [];
-  listener((...args) => queues.map(q => q.push(...args)));
+  try {
+    debug(
+      user ? `@${user.username || user.id}` : 'anonymous',
+      'listening to',
+      info.fieldName
+    );
+    const pullQueue = [];
+    const pushQueue = [];
+    let listening = true;
+    let cursor;
+    // Start listener
+    listener(value => pushValue(value)).then(c => {
+      cursor = c;
+    });
 
-  return () => {
-    const queue = new Queue();
-    queues.push(queue);
-
-    // I have no idea how to type generators...
-    // $FlowFixMe
-    async function* AsyncGenerator() {
-      try {
-        while (true) {
-          yield await queue.pop();
-        }
-      } catch (err) {
-        // Remove the error'ing queue from the queues array
-        queues = queues.filter(q => q !== queue);
-        onError && onError(err);
+    function pushValue(value) {
+      if (pullQueue.length !== 0) {
+        pullQueue.shift()({ value, done: false });
+      } else {
+        pushQueue.push(value);
       }
     }
 
-    return AsyncGenerator();
-  };
+    function pullValue() {
+      return new Promise(resolve => {
+        if (pushQueue.length !== 0) {
+          resolve({ value: pushQueue.shift(), done: false });
+        } else {
+          pullQueue.push(resolve);
+        }
+      });
+    }
+
+    function emptyQueue() {
+      if (listening) {
+        listening = false;
+        pullQueue.forEach(resolve => resolve({ value: undefined, done: true }));
+        pullQueue.length = 0;
+        pushQueue.length = 0;
+        if (cursor) cursor.close();
+      }
+    }
+
+    return ({
+      next() {
+        return listening ? pullValue() : this.return();
+      },
+      return() {
+        emptyQueue();
+        return Promise.resolve({ value: undefined, done: true });
+      },
+      throw(error) {
+        emptyQueue();
+        onError && onError(error);
+        return Promise.reject(error);
+      },
+      [$$asyncIterator]() {
+        return this;
+      },
+    }: any);
+  } catch (err) {
+    debug(err);
+    onError && onError(err);
+  }
 };
 
 export default asyncify;
