652237877e241de1a276f0015a0444e842957cad
Maximilian Stoiber <contact@mxstbr.com>
2018-02-22 14:38:27 +0100

    Refactor asyncify to only run one db listener
    Previously we'd start one db listener for each user and subscription.
    While we should do that eventually, it should be without listening to
    changes of an entire table, instead only listening to relevant changes.
    
    While that's possible, it's a bit too much work right now, so this patch
    makes it so we only start a db listener once and then only give each
    user a new queue.

--- iris/utils/asyncify.js
+++ iris/utils/asyncify.js
@@ -11,72 +11,88 @@ import type { Cursor } from 'rethinkdbdash';
 
 type Listener = ((arg: any) => void) => Promise<Cursor>;
 
-const asyncify = (listener: Listener, onError?: Error => void) => (
-  _: any,
-  __: any,
-  { user }: GraphQLContext,
-  info: GraphQLResolveInfo
-) => {
+const asyncify = (listener: Listener, onError?: Error => void) => {
   try {
-    debug(
-      user ? `@${user.username || user.id}` : 'anonymous',
-      'listening to',
-      info.fieldName
-    );
-    const pullQueue = [];
-    const pushQueue = [];
-    let listening = true;
+    let queues = [];
     let cursor;
+    debug('starting listener');
     // Start listener
     listener(value => pushValue(value)).then(c => {
       cursor = c;
     });
 
     function pushValue(value) {
-      if (pullQueue.length !== 0) {
-        pullQueue.shift()({ value, done: false });
-      } else {
-        pushQueue.push(value);
-      }
-    }
-
-    function pullValue() {
-      return new Promise(resolve => {
-        if (pushQueue.length !== 0) {
-          resolve({ value: pushQueue.shift(), done: false });
+      queues.forEach(({ pullQueue, pushQueue }) => {
+        if (pullQueue.length !== 0) {
+          pullQueue.shift()({ value, done: false });
         } else {
-          pullQueue.push(resolve);
+          pushQueue.push(value);
         }
       });
     }
 
-    function emptyQueue() {
-      if (listening) {
-        listening = false;
-        pullQueue.forEach(resolve => resolve({ value: undefined, done: true }));
-        pullQueue.length = 0;
-        pushQueue.length = 0;
-        if (cursor) cursor.close();
-      }
-    }
+    return (
+      _: any,
+      __: any,
+      { user }: GraphQLContext,
+      info: GraphQLResolveInfo
+    ) => {
+      debug(
+        user ? `@${user.username || user.id}` : 'anonymous',
+        'listening to',
+        info.fieldName
+      );
 
-    return ({
-      next() {
-        return listening ? pullValue() : this.return();
-      },
-      return() {
-        emptyQueue();
-        return Promise.resolve({ value: undefined, done: true });
-      },
-      throw(error) {
-        emptyQueue();
-        onError && onError(error);
-        return Promise.reject(error);
-      },
-      [$$asyncIterator]() {
-        return this;
-      },
-    }: any);
+      // Create a new queue pair per subscriber
+      let pullQueue = [];
+      let pushQueue = [];
+      let listening = true;
+      let myQueues = { pullQueue, pushQueue };
+      queues.push(myQueues);
+
+      function emptyQueue() {
+        if (listening) {
+          listening = false;
+          // Clean up the queues
+          pullQueue.forEach(resolve =>
+            resolve({ value: undefined, done: true })
+          );
+          pullQueue = [];
+          pushQueue = [];
+          myQueues = undefined;
+          // Remove the queues from the list of queues
+          queues = queues.filter(queue => queue !== myQueues);
+        }
+      }
+
+      function pullValue() {
+        return new Promise(resolve => {
+          if (pushQueue.length !== 0) {
+            resolve({ value: pushQueue.shift(), done: false });
+          } else {
+            pullQueue.push(resolve);
+          }
+        });
+      }
+
+      return {
+        next() {
+          return listening ? pullValue() : this.return();
+        },
+        return() {
+          emptyQueue();
+          return Promise.resolve({ value: undefined, done: true });
+        },
+        throw(error) {
+          emptyQueue();
+          onError && onError(error);
+          return Promise.reject(error);
+        },
+        [$$asyncIterator]() {
+          return this;
+        },
+      };
+    };
   } catch (err) {
     debug(err);
     onError && onError(err);
