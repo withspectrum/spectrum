219a8a14cf6a3596620783cfc8d095d97e8fd557
Maximilian Stoiber <contact@mxstbr.com>
2018-02-22 14:44:57 +0100

    Revert "Refactor asyncify to only run one db listener"
    This reverts commit 652237877e241de1a276f0015a0444e842957cad.

--- iris/utils/asyncify.js
+++ iris/utils/asyncify.js
@@ -11,88 +11,72 @@ import type { Cursor } from 'rethinkdbdash';
 
 type Listener = ((arg: any) => void) => Promise<Cursor>;
 
-const asyncify = (listener: Listener, onError?: Error => void) => {
+const asyncify = (listener: Listener, onError?: Error => void) => (
+  _: any,
+  __: any,
+  { user }: GraphQLContext,
+  info: GraphQLResolveInfo
+) => {
   try {
-    let queues = [];
+    debug(
+      user ? `@${user.username || user.id}` : 'anonymous',
+      'listening to',
+      info.fieldName
+    );
+    const pullQueue = [];
+    const pushQueue = [];
+    let listening = true;
     let cursor;
-    debug('starting listener');
     // Start listener
     listener(value => pushValue(value)).then(c => {
       cursor = c;
     });
 
     function pushValue(value) {
-      queues.forEach(({ pullQueue, pushQueue }) => {
-        if (pullQueue.length !== 0) {
-          pullQueue.shift()({ value, done: false });
+      if (pullQueue.length !== 0) {
+        pullQueue.shift()({ value, done: false });
+      } else {
+        pushQueue.push(value);
+      }
+    }
+
+    function pullValue() {
+      return new Promise(resolve => {
+        if (pushQueue.length !== 0) {
+          resolve({ value: pushQueue.shift(), done: false });
         } else {
-          pushQueue.push(value);
+          pullQueue.push(resolve);
         }
       });
     }
 
-    return (
-      _: any,
-      __: any,
-      { user }: GraphQLContext,
-      info: GraphQLResolveInfo
-    ) => {
-      debug(
-        user ? `@${user.username || user.id}` : 'anonymous',
-        'listening to',
-        info.fieldName
-      );
-
-      // Create a new queue pair per subscriber
-      let pullQueue = [];
-      let pushQueue = [];
-      let listening = true;
-      let myQueues = { pullQueue, pushQueue };
-      queues.push(myQueues);
-
-      function emptyQueue() {
-        if (listening) {
-          listening = false;
-          // Clean up the queues
-          pullQueue.forEach(resolve =>
-            resolve({ value: undefined, done: true })
-          );
-          pullQueue = [];
-          pushQueue = [];
-          myQueues = undefined;
-          // Remove the queues from the list of queues
-          queues = queues.filter(queue => queue !== myQueues);
-        }
+    function emptyQueue() {
+      if (listening) {
+        listening = false;
+        pullQueue.forEach(resolve => resolve({ value: undefined, done: true }));
+        pullQueue.length = 0;
+        pushQueue.length = 0;
+        if (cursor) cursor.close();
       }
+    }
 
-      function pullValue() {
-        return new Promise(resolve => {
-          if (pushQueue.length !== 0) {
-            resolve({ value: pushQueue.shift(), done: false });
-          } else {
-            pullQueue.push(resolve);
-          }
-        });
-      }
-
-      return {
-        next() {
-          return listening ? pullValue() : this.return();
-        },
-        return() {
-          emptyQueue();
-          return Promise.resolve({ value: undefined, done: true });
-        },
-        throw(error) {
-          emptyQueue();
-          onError && onError(error);
-          return Promise.reject(error);
-        },
-        [$$asyncIterator]() {
-          return this;
-        },
-      };
-    };
+    return ({
+      next() {
+        return listening ? pullValue() : this.return();
+      },
+      return() {
+        emptyQueue();
+        return Promise.resolve({ value: undefined, done: true });
+      },
+      throw(error) {
+        emptyQueue();
+        onError && onError(error);
+        return Promise.reject(error);
+      },
+      [$$asyncIterator]() {
+        return this;
+      },
+    }: any);
   } catch (err) {
     debug(err);
     onError && onError(err);
