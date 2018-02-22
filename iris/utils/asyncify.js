70d270895b71114f7111e2dc63f43a4f6932883b
Maximilian Stoiber <contact@mxstbr.com>
2018-02-22 15:16:44 +0100

    Create a specific changefeed per user/subscription
    This is based on #2433.
    
    It reverts the second change made on that branch (6522378) so that we
    have one db changefeed per user and subscription again.
    
    Previously we would have one changefeed listening to all new
    messages in the messages table and filter them after the fact.
    
    This patch implements a changfeed per user and subscription,
    where the changefeed is automatically filtered to only include
    the changes we care about. (in this case per thread)

--- iris/utils/asyncify.js
+++ iris/utils/asyncify.js
@@ -5,26 +5,14 @@ const debug = require('debug')('iris:utils:asyncify');
 import { $$asyncIterator } from 'iterall';
 import Raven from 'shared/raven';
 
-import type { GraphQLContext } from '../';
-import type { GraphQLResolveInfo } from 'graphql';
 import type { Cursor } from 'rethinkdbdash';
 
 type Listener = ((arg: any) => void) => Promise<Cursor>;
 
-const asyncify = (listener: Listener, onError?: Error => void) => (
-  _: any,
-  __: any,
-  { user }: GraphQLContext,
-  info: GraphQLResolveInfo
-) => {
+const asyncify = (listener: Listener, onError?: Error => void) => {
   try {
-    debug(
-      user ? `@${user.username || user.id}` : 'anonymous',
-      'listening to',
-      info.fieldName
-    );
-    const pullQueue = [];
-    const pushQueue = [];
+    let pullQueue = [];
+    let pushQueue = [];
     let listening = true;
     let cursor;
     // Start listener
@@ -54,8 +42,8 @@ const asyncify = (listener: Listener, onError?: Error => void) => (
       if (listening) {
         listening = false;
         pullQueue.forEach(resolve => resolve({ value: undefined, done: true }));
-        pullQueue.length = 0;
-        pushQueue.length = 0;
+        pullQueue = [];
+        pushQueue = [];
         if (cursor) cursor.close();
       }
     }
