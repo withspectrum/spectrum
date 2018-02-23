2a39ed2c7d9ef7c0e8811aabfee7ed6e936e02bd
Maximilian Stoiber <contact@mxstbr.com>
2018-02-23 09:32:45 +0100

    Multiple changefeed for notifications subscriptions

--- iris/subscriptions/notification.js
+++ iris/subscriptions/notification.js
@@ -1,35 +1,54 @@
 // @flow
-/**
- * Define the notification subscription resolvers
- */
+const debug = require('debug')('iris:subscriptions:notification');
 import { withFilter } from 'graphql-subscriptions';
 const {
   listenToNewNotifications,
   listenToNewDirectMessageNotifications,
 } = require('../models/notification');
 import asyncify from '../utils/asyncify';
+import UserError from '../utils/UserError';
+import type { GraphQLContext } from '../';
+import type { GraphQLResolveInfo } from 'graphql';
 
 module.exports = {
   Subscription: {
     notificationAdded: {
       resolve: (notification: any) => notification,
-      subscribe: withFilter(
-        asyncify(listenToNewNotifications, err => {
+      subscribe: async (
+        _: any,
+        { thread }: { thread: string },
+        { user }: GraphQLContext,
+        info: GraphQLResolveInfo
+      ) => {
+        if (!user || !user.id)
+          throw new UserError(
+            'Can only listen to notifications when signed in.'
+          );
+
+        debug(`@${user.username || user.id} listening to notifications`);
+        return asyncify(listenToNewNotifications(user.id), err => {
           throw new Error(err);
-        }),
-        (notification, _, { user }) =>
-          notification && user && user.id === notification.userId
-      ),
+        });
+      },
     },
     dmNotificationAdded: {
       resolve: (notification: any) => notification,
-      subscribe: withFilter(
-        asyncify(listenToNewDirectMessageNotifications, err => {
+      subscribe: async (
+        _: any,
+        { thread }: { thread: string },
+        { user }: GraphQLContext,
+        info: GraphQLResolveInfo
+      ) => {
+        if (!user || !user.id)
+          throw new UserError(
+            'Can only listen to notifications when signed in.'
+          );
+
+        debug(`@${user.username || user.id} listening to DM notifications`);
+        return asyncify(listenToNewDirectMessageNotifications(user.id), err => {
           throw new Error(err);
-        }),
-        (notification, _, { user }) =>
-          notification && user.id === notification.userId
-      ),
+        });
+      },
     },
   },
 };
