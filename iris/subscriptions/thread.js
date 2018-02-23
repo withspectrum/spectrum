04c625a6dff5e7521f136fb18919fc26912cd42f
Maximilian Stoiber <contact@mxstbr.com>
2018-02-23 09:59:29 +0100

    Multiple changefeeds for thread subscriptions

--- iris/subscriptions/thread.js
+++ iris/subscriptions/thread.js
@@ -1,23 +1,39 @@
 // @flow
-/**
- * Define the thread subscription resolvers
- */
+const debug = require('debug')('iris:subscriptions:notification');
 import { withFilter } from 'graphql-subscriptions';
 import { userIsMemberOfChannel } from './utils';
-const { listenToUpdatedThreads } = require('../models/thread');
+import { listenToUpdatedThreads } from '../models/thread';
+import { getUserUsersChannels } from '../models/usersChannels';
 import asyncify from '../utils/asyncify';
+import UserError from '../utils/UserError';
+import type { GraphQLContext } from '../';
+import type { GraphQLResolveInfo } from 'graphql';
 
 module.exports = {
   Subscription: {
     threadUpdated: {
       resolve: (thread: any) => thread,
-      subscribe: withFilter(
-        asyncify(listenToUpdatedThreads, err => {
-          throw new Error(err);
-        }),
-        async (thread, _, { user }) =>
-          await userIsMemberOfChannel(thread.channelId, user && user.id)
-      ),
+      subscribe: async (
+        _: any,
+        { thread }: { thread: string },
+        { user }: GraphQLContext,
+        info: GraphQLResolveInfo
+      ) => {
+        if (!user || !user.id)
+          throw new UserError(
+            'Cannot subscribe to updated threads when not signed in.'
+          );
+
+        const channels = await getUserUsersChannels(user.id);
+
+        debug(`@${user.username || user.id} listening to new threads`);
+        return asyncify(
+          listenToUpdatedThreads(channels.map(({ channelId }) => channelId)),
+          err => {
+            throw new Error(err);
+          }
+        );
+      },
     },
   },
 };
