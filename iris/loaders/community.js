e4a5d93341900bad353d6652b99353e81d9e1c31
Brian Lovin <briandlovin@gmail.com>
2018-03-03 18:51:49 -0800

    Revert "Database caching"

--- iris/loaders/community.js
+++ iris/loaders/community.js
@@ -8,12 +8,9 @@ import {
 import { getCommunitiesRecurringPayments } from '../models/recurringPayment';
 import createLoader from './create-loader';
 
-const FIVE_MINUTES = 300000;
-const THIRTY_MINUTES = 1800000;
-
 export const __createCommunityRecurringPaymentsLoader = createLoader(
   communities => getCommunitiesRecurringPayments(communities),
-  { getKeyFromResult: 'group' }
+  'group'
 );
 
 export const __createCommunityLoader = createLoader(communities =>
@@ -22,17 +19,17 @@ export const __createCommunityLoader = createLoader(communities =>
 
 export const __createCommunityBySlugLoader = createLoader(
   communities => getCommunitiesBySlug(communities),
-  { getKeyFromResult: 'slug' }
+  'slug'
 );
 
 export const __createCommunityMemberCountLoader = createLoader(
   communityIds => getCommunitiesMemberCounts(communityIds),
-  { getKeyFromResult: 'group', cacheExpiryTime: THIRTY_MINUTES }
+  'group'
 );
 
 export const __createCommunityChannelCountLoader = createLoader(
   communityIds => getCommunitiesChannelCounts(communityIds),
-  { getKeyFromResult: 'group', cacheExpiryTime: THIRTY_MINUTES }
+  'group'
 );
 
 export default () => {
