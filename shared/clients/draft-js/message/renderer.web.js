c1aa79c186369b4cd8829bf39374ad4deecd131e
Maximilian Stoiber <contact@mxstbr.com>
2018-03-05 10:30:27 +0100

    Fix import

--- shared/clients/draft-js/message/renderer.web.js
+++ shared/clients/draft-js/message/renderer.web.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
-import mentionsDecorator from '../mentions-decorator';
-import linksDecorator from '../links-decorator';
+import mentionsDecorator from '../mentions-decorator/index.web';
+import linksDecorator from '../links-decorator/index.web';
 import { Line, Paragraph } from 'src/components/message/style';
 import type { Node } from 'react';
 
