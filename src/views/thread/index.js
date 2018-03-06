b2aee1d5409b6cf215b841400bfd875f7353d198
Brian Lovin <briandlovin@gmail.com>
2018-03-06 10:57:00 -0800

    Hide join upsells if thread is locked

--- src/views/thread/index.js
+++ src/views/thread/index.js
@@ -449,6 +449,7 @@ class ThreadContainer extends React.Component<Props, State> {
 
                 {!isEditing &&
                   isLoggedIn &&
+                  !isLocked &&
                   !canSendMessages && (
                     <JoinChannel
                       community={thread.community}
@@ -457,7 +458,8 @@ class ThreadContainer extends React.Component<Props, State> {
                   )}
 
                 {!isEditing &&
-                  !isLoggedIn && (
+                  !isLoggedIn &&
+                  !isLocked && (
                     <UpsellSignIn
                       title={`Join the ${thread.community.name} community`}
                       glyph={'message-new'}
