69653afea5b027593788c6dbafe97f478d6164f5
Maximilian Stoiber <contact@mxstbr.com>
2018-03-06 21:00:36 +0100

    Fix channel.communityPermissions

--- iris/queries/channel/communityPermissions.js
+++ iris/queries/channel/communityPermissions.js
@@ -3,7 +3,7 @@ import type { GraphQLContext } from '../../';
 import type { DBChannel } from 'shared/types';
 
 export default (root: DBChannel, _: any, { user, loaders }: GraphQLContext) => {
-  const communityId = root.id || root.communityId;
+  const communityId = root.communityId || root.id;
   if (!communityId || !user) {
     return {
       isOwner: false,
