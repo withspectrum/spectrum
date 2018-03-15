d01b007577dfb8ce072442ecb1611e75bfd341e7
Maximilian Stoiber <contact@mxstbr.com>
2018-03-15 15:11:14 +0100

    Switch to using the AWS SDK directly for image uploads
    The s3-image-uploader module doesn't do what we need it to as it can't
    handle file streams. The AWS SDK can handle them though, so this patch
    switches us to using that directly.
    
    Also removes all that fs code. /cc @brianlovin

--- iris/utils/s3.js
+++ iris/utils/s3.js
@@ -1,9 +1,7 @@
 // @flow
 require('now-env');
-const Uploader = require('s3-image-uploader');
-import mkdirp from 'mkdirp';
+import AWS from 'aws-sdk';
 import shortid from 'shortid';
-import fs from 'fs';
 const IS_PROD = process.env.NODE_ENV === 'production';
 
 import type { FileUpload } from 'shared/types';
@@ -13,95 +11,63 @@ let S3_TOKEN = process.env.S3_TOKEN;
 let S3_SECRET = process.env.S3_SECRET;
 
 if (!IS_PROD) {
-  // In development or testing default the tokens to some garbage
-  // so that the s3-image-uploader doesn't throw an error
   S3_TOKEN = S3_TOKEN || 'asdf123';
   S3_SECRET = S3_SECRET || 'asdf123';
 }
 
-const uploader = new Uploader({
-  aws: {
-    key: S3_TOKEN,
-    secret: S3_SECRET,
+AWS.config.update({
+  accessKeyId: S3_TOKEN,
+  secretAccessKey: S3_SECRET,
+  apiVersions: {
+    s3: 'latest',
   },
-  websockets: false,
 });
+const s3 = new AWS.S3();
 
-const uploadDir = './uploads';
-mkdirp.sync(uploadDir);
-
-const removeFS = path => {
-  return fs.unlinkSync(path);
-};
-
-const storeFS = ({ stream, filename }) => {
-  const path = `${uploadDir}/${filename}`;
-  return new Promise((resolve, reject) =>
-    stream
-      .on('error', error => {
-        if (stream.truncated)
-          // Delete the truncated file
-          fs.unlinkSync(path);
-        reject(error);
-      })
-      .on('end', () => resolve({ path }))
-      .pipe(fs.createWriteStream(path))
-  );
-};
-
-const getPath = async file => {
-  const { stream, filename } = await file;
-  const name = `${filename}-${shortid.generate()}`;
-  const { path } = await storeFS({ stream, filename: name });
-  return { path, name };
-};
-
-const generateImageUrl = path => {
+const generateImageUrl = (path, name) => {
   // remove the bucket name from the path
-  const newPath = path.replace('/spectrum-chat', '');
+  const newPath = path.replace(/\/?spectrum-chat\/?/, '');
 
   // this is the default source for our imgix account, which starts
   // at the bucket root, thus we remove the bucket from the path
   const imgixBase = 'https://spectrum.imgix.net';
 
   // return a new url to update the user object
-  return imgixBase + newPath;
+  return imgixBase + '/' + newPath + '/' + name;
 };
 
-const upload = (file: FileUpload, entity: EntityTypes, id: string) =>
-  new Promise(async (resolve, reject) => {
-    const { path, name } = await getPath(file);
-
-    return await uploader.upload(
+const upload = async (
+  file: FileUpload,
+  entity: EntityTypes,
+  id: string
+): Promise<string> => {
+  const result = await file;
+  const { filename, stream } = result;
+  return new Promise(res => {
+    const path = `spectrum-chat/${entity}/${id}`;
+    const fileKey = `${shortid.generate()}-${filename}`;
+    return s3.upload(
       {
-        fileId: name,
-        bucket: `spectrum-chat/${entity}/${id}`,
-        source: path,
-        name: name,
+        Bucket: path,
+        Key: fileKey,
+        Body: stream,
+        ACL: 'public-read',
       },
-      data => {
-        const url = generateImageUrl(data.path);
-        return resolve({
-          path: path,
-          url: encodeURI(url),
-        });
-      },
-      (errMsg, errObject) => {
-        console.error('unable to upload: ' + errMsg + ':', errObject);
-        return reject(errMsg);
+      (err, data) => {
+        if (err) throw new Error(err);
+        const url = generateImageUrl(data.Bucket, data.Key);
+        res(encodeURI(url));
       }
     );
   });
+};
 
 export const uploadImage = async (
   file: FileUpload,
   entity: EntityTypes,
   id: string
-) => {
-  return await upload(file, entity, id)
-    .then(({ url, path }) => {
-      removeFS(path);
-      return url;
-    })
-    .catch(err => err);
+): Promise<string> => {
+  return await upload(file, entity, id).catch(err => {
+    throw new Error(err);
+  });
 };
