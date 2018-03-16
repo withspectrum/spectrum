2cd2075c5df9f4c8167392629ecd6fc2c07531af
Brian Lovin <briandlovin@gmail.com>
2018-03-16 14:22:58 -0700

    Add mutations to enable, disable, and reset token based joining

---
+++ shared/graphql/queries/channel/getChannelSettings.js
@@ -0,0 +1,41 @@
+// @flow
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import channelInfoFragment, {
+  type ChannelInfoType,
+} from '../../fragments/channel/channelInfo';
+
+export type GetChannelSettingsType = {
+  ...$Exact<ChannelInfoType>,
+  joinSettings: {
+    tokenJoinEnabled: boolean,
+    token: string,
+  },
+};
+
+export const getChannelSettingsByIdQuery = gql`
+  query getChannel($id: ID) {
+    channel(id: $id) {
+      ...channelInfo
+      joinSettings {
+        tokenJoinEnabled
+        token
+      }
+    }
+  }
+  ${channelInfoFragment}
+`;
+
+const getChannelSettingsByIdOptions = {
+  options: ({ id }) => ({
+    variables: {
+      id,
+    },
+    fetchPolicy: 'cache-first',
+  }),
+};
+
+export default graphql(
+  getChannelSettingsByIdQuery,
+  getChannelSettingsByIdOptions
+);
