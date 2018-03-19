3f2517716da46e5e8e6994ed95a84b74b29a304e
Brian Lovin <briandlovin@gmail.com>
2018-03-19 10:14:50 -0700

    Add a test for communitySettings

---
+++ iris/test/community/queries/communitySettings.test.js
@@ -0,0 +1,24 @@
+//@flow
+import { request } from '../../utils';
+
+it('should fetch a communitys settings', async () => {
+  const query = /* GraphQL */ `
+    {
+      community(id: "ce2b4488-4c75-47e0-8ebc-2539c1e6a191") {
+        id
+        brandedLogin {
+          isEnabled
+          message
+        }
+      }
+    }
+  `;
+
+  expect.assertions(2);
+  const result = await request(query);
+  console.log('result', result);
+  const { data: { community } } = result;
+
+  expect(community.brandedLogin.isEnabled).toEqual(false);
+  expect(result).toMatchSnapshot();
+});
