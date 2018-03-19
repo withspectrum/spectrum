6190d376c6b484f46d2f6ab2ba384235a4785f57
Brian Lovin <briandlovin@gmail.com>
2018-03-19 12:40:31 -0700

    Add direct message button to members list

--- src/views/community/components/memberGrid.js
+++ src/views/community/components/memberGrid.js
@@ -1,7 +1,11 @@
 // @flow
 import * as React from 'react';
 import compose from 'recompose/compose';
+import { connect } from 'react-redux';
 import InfiniteList from 'react-infinite-scroller-with-scroll-element';
+import Icon from 'src/components/icons';
+import { initNewThreadWithUser } from 'src/actions/directMessageThreads';
+import { withRouter } from 'react-router';
 import getCommunityMembersQuery, {
   type GetCommunityMembersType,
 } from 'shared/graphql/queries/community/getCommunityMembers';
@@ -11,14 +15,17 @@ import { LoadingList, LoadingListItem } from 'src/components/loading';
 import { UserListItem } from 'src/components/listItems';
 import viewNetworkHandler from 'src/components/viewNetworkHandler';
 import ViewError from 'src/components/viewError';
+import { MessageIconContainer } from '../style';
 
 type Props = {
   data: {
     community: GetCommunityMembersType,
     fetchMore: Function,
   },
+  dispatch: Function,
   isLoading: boolean,
   isFetchingMore: boolean,
+  history: Object,
 };
 
 type State = {
@@ -35,6 +42,11 @@ class CommunityMemberGrid extends React.Component<Props, State> {
       scrollElement: document.getElementById('scroller-for-thread-feed'),
     });
   }
+
+  initMessage = user => {
+    this.props.dispatch(initNewThreadWithUser(user));
+    return this.props.history.push('/messages/new');
+  };
 
   shouldComponentUpdate(nextProps) {
     const curr = this.props;
@@ -58,7 +70,11 @@ class CommunityMemberGrid extends React.Component<Props, State> {
           pageStart={0}
           loadMore={this.props.data.fetchMore}
           hasMore={hasNextPage}
-          loader={<LoadingListItem />}
+          loader={
+            <div style={{ padding: '0 16px', background: '#fff' }}>
+              <LoadingListItem />
+            </div>
+          }
           useWindow={false}
           initialLoad={false}
           scrollElement={scrollElement}
@@ -67,8 +83,18 @@ class CommunityMemberGrid extends React.Component<Props, State> {
           {nodes.map(node => {
             if (!node) return null;
             return (
-              <div style={{ padding: '0 16px' }} key={node.user.id}>
-                <UserListItem key={node.user.id} user={node.user} />
+              <div
+                style={{ padding: '0 16px', background: '#fff' }}
+                key={node.user.id}
+              >
+                <UserListItem key={node.user.id} user={node.user}>
+                  <MessageIconContainer>
+                    <Icon
+                      glyph={'message'}
+                      onClick={() => this.initMessage(node.user)}
+                    />
+                  </MessageIconContainer>
+                </UserListItem>
               </div>
             );
           })}
@@ -91,6 +117,9 @@ class CommunityMemberGrid extends React.Component<Props, State> {
   }
 }
 
-export default compose(getCommunityMembersQuery, viewNetworkHandler)(
-  CommunityMemberGrid
-);
+export default compose(
+  connect(),
+  withRouter,
+  getCommunityMembersQuery,
+  viewNetworkHandler
+)(CommunityMemberGrid);
