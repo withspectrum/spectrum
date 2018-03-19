e09c7690eddcdd14312084517c5060a1c9742e6a
Maximilian Stoiber <contact@mxstbr.com>
2018-03-19 13:31:19 +0100

    Switch to debug in backpack.config.js

--- backpack.config.js
+++ backpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const debug = require('debug')('build:backpack');
 
 const dir = process.env.DIR;
 
 if (!dir) throw new Error('Define directory to build with the -d option.');
-console.log(
+debug(
   `> Building ${dir}, entry: ${dir}/index.js, output: build-${dir}/main.js`
 );
 
