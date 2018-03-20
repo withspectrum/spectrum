1999608a319ca99e1de004a4dc0135e06bafbc86
Maximilian Stoiber <contact@mxstbr.com>
2018-03-20 15:49:40 +0100

    Test community settings members view

--- cypress/integration/community_settings_spec.js
+++ cypress/integration/community_settings_spec.js
@@ -1,6 +1,11 @@
 import data from '../../shared/testing/data';
 
 const community = data.communities[0];
+const members = data.usersCommunities
+  .filter(
+    ({ communityId, isMember }) => community.id === communityId && isMember
+  )
+  .map(({ userId }) => data.users.find(({ id }) => id === userId));
 const { userId: ownerId } = data.usersCommunities.find(
   ({ communityId, isOwner }) => communityId === community.id && isOwner
 );
@@ -92,6 +97,23 @@ describe('Community Settings View', () => {
       cy.get('button[type="submit"]').click();
       cy.location('pathname').should('eq', `/${community.slug}`);
       cy.contains(community.website);
+    });
+  });
+
+  describe('Members', () => {
+    beforeEach(() => {
+      cy.auth(ownerId);
+    });
+
+    it('should be reachable from the overview and have a list of the members', () => {
+      cy.visit(`/${community.slug}/settings`);
+      cy
+        .get(`[href="/${community.slug}/settings/members"]`)
+        .should('be.visible')
+        .click();
+      members.forEach(member => {
+        cy.contains(member.name).should('be.visible');
+      });
     });
   });
 });
