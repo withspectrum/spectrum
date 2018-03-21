8046613e66f1fd6e2bf3b2354c29a9a37947a5cd
Brian Lovin <briandlovin@gmail.com>
2018-03-21 12:39:10 -0700

    Also fix this behavior for community settings

--- iris/mutations/community/disableBrandedLogin.js
+++ iris/mutations/community/disableBrandedLogin.js
@@ -33,7 +33,8 @@ export default async (
 
   loaders.communitySettings.clear(communityId);
 
-  if (settings) {
+  // settings.id tells us that a channelSettings record exists in the db
+  if (settings.id) {
     return await disableCommunityBrandedLogin(communityId);
   } else {
     return await createCommunitySettings(communityId);
