de6fe5de56d3142c5c4b767c686b2a1c8d7c33d5
Brian Lovin <briandlovin@gmail.com>
2018-03-21 20:34:03 -0700

    Change splash spec to home spec

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,10 +1,10 @@
-describe('Splash View', () => {
+describe('Home View', () => {
   before(() => {
     cy.visit('/');
   });
 
-  it('should render the splash page', () => {
-    cy.get('[data-e2e-id="splash-page"]').should('be.visible');
+  it('should render the home page', () => {
+    cy.get('[data-e2e-id="home-page"]').should('be.visible');
     cy.get('[href*="/login"]').should('be.visible');
     cy.get('[href*="/new/community"]').should('be.visible');
   });
