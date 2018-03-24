6cb88d655851d228975aedffba4db5d06d79db4e
Brian Lovin <briandlovin@gmail.com>
2018-03-23 17:08:19 -0700

    Add check for github auth button on login view

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -8,6 +8,7 @@ describe('Login View', () => {
     cy.get('[href*="/auth/twitter"]').should('be.visible');
     cy.get('[href*="/auth/facebook"]').should('be.visible');
     cy.get('[href*="/auth/google"]').should('be.visible');
+    cy.get('[href*="/auth/github"]').should('be.visible');
     cy
       .get('[href*="github.com/withspectrum/code-of-conduct"]')
       .should('be.visible');
