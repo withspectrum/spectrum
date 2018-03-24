0b92674db4819d3db7ae9083b7eafcbd617ec886
Brian Lovin <briandlovin@gmail.com>
2018-03-23 17:08:30 -0700

    Add test for channel deletion

---
+++ cypress/integration/channel/settings/delete_spec.js
@@ -0,0 +1,75 @@
+// @flow
+import data from '../../../../shared/testing/data';
+
+const channel = data.channels[0];
+const privateChannel = data.channels[1];
+
+const community = data.communities.find(
+  community => community.id === channel.communityId
+);
+
+const privateCommunity = data.communities.find(
+  community => community.id === privateChannel.communityId
+);
+
+const { userId: ownerInChannelId } = data.usersChannels.find(
+  ({ channelId, isOwner }) => channelId === channel.id && isOwner
+);
+
+const { userId: ownerInPrivateChannelId } = data.usersChannels.find(
+  ({ channelId, isOwner }) => channelId === privateChannel.id && isOwner
+);
+
+describe('deleting general channel', () => {
+  before(() => {
+    cy.auth(ownerInChannelId);
+    cy.visit(`/${community.slug}/${channel.slug}`);
+  });
+
+  it('should not allow general channel to be deleted', () => {
+    cy
+      .get('[data-cy="channel-settings-button"]')
+      .should('be.visible')
+      .click();
+
+    cy.get('[data-cy="channel-overview"]').should('be.visible');
+
+    cy.get('[data-cy="delete-channel-button"]').should('not.be.visible');
+  });
+});
+
+describe('deleting a channel', () => {
+  before(() => {
+    cy.auth(ownerInPrivateChannelId);
+    cy.visit(`/${privateCommunity.slug}/${privateChannel.slug}`);
+  });
+
+  it('should delete a channel', () => {
+    cy
+      .get('[data-cy="channel-settings-button"]')
+      .should('be.visible')
+      .click();
+
+    cy.get('[data-cy="channel-overview"]').should('be.visible');
+
+    cy
+      .get('[data-cy="delete-channel-button"]')
+      .should('be.visible')
+      .click();
+
+    cy
+      .get('[data-cy="delete-button"]')
+      .should('be.visible')
+      .click();
+
+    cy.visit(`/${privateCommunity.slug}/${privateChannel.slug}`);
+
+    cy.get('channel-view').should('not.be.visible');
+
+    cy.get('[data-cy="channel-not-found"]').should('be.visible');
+
+    cy.visit(`/${privateCommunity.slug}/settings`);
+    cy.get('[data-cy="channel-list"]').should('be.visible');
+    cy.contains('Private').should('not.exist');
+  });
+});
