9857baedbc9c221d3dfd96ce3ddbda3d3405b742
Maximilian Stoiber <contact@mxstbr.com>
2018-03-30 11:58:08 +0200

    Fix e2e tests
    @brianlovin we need to run resetdb both before() and beforeEach(),
    because otherwise the cy.visit('/whatever') runs before the beforeEach()
    so it'd 404.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -17,10 +17,9 @@
 import './commands';
 
 before(() => {
-  cy.exec(
-    `node -e "const teardown = require('./shared/testing/teardown.js')().then(() => process.exit())"`
-  );
-  cy.exec(
-    `node -e "const setup = require('./shared/testing/setup.js')().then(() => process.exit())"`
-  );
+  cy.resetdb();
+});
+
+beforeEach(() => {
+  cy.resetdb();
 });
