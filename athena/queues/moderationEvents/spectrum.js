7170895d7fcfd64df9f6f98fce8e252a45f5b28c
Brian Lovin <briandlovin@gmail.com>
2018-04-10 13:42:12 -0700

    Swap spectrum secrets

--- athena/queues/moderationEvents/spectrum.js
+++ athena/queues/moderationEvents/spectrum.js
@@ -1,12 +1,21 @@
 // @flow
+const debug = require('debug')('athena:queue:moderation-events:spectrum');
+require('now-env');
 import axios from 'axios';
+const SPECTRUM_MODERATION_API_KEY = process.env.SPECTRUM_MODERATION_API_KEY;
+
+if (!SPECTRUM_MODERATION_API_KEY) {
+  debug('No API key for Spectrum provided, not sending moderation events.');
+}
 
 export default async (text: string, contextId: string, userId: string) => {
+  if (!SPECTRUM_MODERATION_API_KEY) return;
+
   const request = await axios({
     method: 'post',
     url: 'https://api.prod.getspectrum.io/api/v1/classification',
     headers: {
-      Authorization: 'Apikey 205276812f89fcec2856d48c9192b2588',
+      Authorization: `Apikey ${SPECTRUM_MODERATION_API_KEY}`,
       'Content-Type': 'application/json',
     },
     data: {
