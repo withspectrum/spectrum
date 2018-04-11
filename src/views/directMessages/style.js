bd595c9f9fea16e91d36a05b223627a43c00429a
uberbryn <hi@bryn.io>
2018-04-11 13:42:57 -0700

    managing height against the tab bar is not ideal. v excited to convert views to grid/subgrid to manage this kind of thing...

--- src/views/directMessages/style.js
+++ src/views/directMessages/style.js
@@ -9,7 +9,7 @@ export const View = styled.main`
   align-items: stretch;
   background: #fff;
   flex: auto;
-  max-height: calc(100% - 48px);
+  height: calc(100vh - 48px);
 
   @media (max-width: 768px) {
     flex-direction: column;
@@ -34,9 +34,11 @@ export const MessagesList = styled(FlexCol)`
   min-width: 320px;
   background: ${props => props.theme.bg.default};
   border-right: 1px solid ${props => props.theme.bg.border};
+  flex: 1 0 auto;
+  max-height: 100%;
 
   @media (max-width: 768px) {
-    flex: auto;
+    max-height: calc(100% - 48px);
     min-width: 320px;
     border-right: none;
     max-width: 100%;
@@ -46,14 +48,15 @@ export const MessagesList = styled(FlexCol)`
 
 export const MessagesContainer = styled(FlexCol)`
   flex: auto;
+  max-height: 100%;
 
   @media (max-width: 768px) {
-    max-height: 100%;
-    ${props =>
-      props.hideOnMobile &&
-      css`
-        display: none;
-      `};
+    max-height: calc(100% - 48px)
+      ${props =>
+        props.hideOnMobile &&
+        css`
+          display: none;
+        `};
   }
 `;
 
