8422ccde27a8196926d1afeca00246d018d4dff3
Maximilian Stoiber <contact@mxstbr.com>
2018-04-12 19:53:48 +0200

    [WIP] Run integration tests against hyperion

--- src/helpers/events.js
+++ src/helpers/events.js
@@ -1,5 +1,9 @@
 import Raven from 'raven-js';
-if (process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN_CLIENT) {
+if (
+  process.env.NODE_ENV === 'production' &&
+  !process.env.FORCE_DEV &&
+  process.env.SENTRY_DSN_CLIENT
+) {
   Raven.config(process.env.SENTRY_DSN_CLIENT, {
     whitelistUrls: [/spectrum\.chat/, /www\.spectrum\.chat/],
     environment: process.env.NODE_ENV,
