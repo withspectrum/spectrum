0e84ebdf79da177a11c24a90e58b0a87aafe3ab7
Brian Lovin <briandlovin@gmail.com>
2018-04-12 22:03:33 -0700

    Fix tests

--- cypress/integration/user/delete_user_spec.js
+++ cypress/integration/user/delete_user_spec.js
@@ -8,38 +8,46 @@ describe('can view delete controls in settings', () => {
   });
 
   it('should render delete account section', () => {
+    // scroll to delete account segment
     cy
       .get('[data-cy="delete-account-container"]')
       .scrollIntoView()
       .should('be.visible');
+
+    // should warn about owning communities since users[0] is max
     cy.get('[data-cy="owns-communities-notice"]').should('be.visible');
+
+    // init delete
     cy
       .get('[data-cy="delete-account-init-button"]')
       .should('be.visible')
       .click();
 
+    // should see option to confirm or cancel
     cy.get('[data-cy="delete-account-confirm-button"]').should('be.visible');
+    // click cancel
     cy
       .get('[data-cy="delete-account-cancel-button"]')
       .should('be.visible')
       .click();
 
-    cy
-      .get('[data-cy="delete-account-confirm-button"]')
-      .should('not.be.visible');
+    // after canceling it should reset
     cy
       .get('[data-cy="delete-account-init-button"]')
       .should('be.visible')
       .click();
 
+    // actually delete the account
     cy
       .get('[data-cy="delete-account-confirm-button"]')
       .should('be.visible')
       .click();
 
+    // should sign out and go to home page
     cy.get('[data-cy="home-page"]').should('be.visible');
 
-    cy.visit(`/users/${user.username}/settings`);
+    // user should be deleted
+    cy.visit(`/users/${user.username}`);
     cy.get('[data-cy="user-not-found"]').should('be.visible');
   });
 });
