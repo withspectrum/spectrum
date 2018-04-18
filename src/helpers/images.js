6932479a0bb5d1d494f5fea4c6fff1b44576abdd
Alejandro Nanez <alejonanez@gmail.com>
2018-04-18 17:24:56 -0500

    make generateHeaderImageFromImgix configurable

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -23,5 +23,10 @@ export const PRO_USER_MAX_IMAGE_SIZE_STRING = `${Math.floor(
   PRO_USER_MAX_IMAGE_SIZE_BYTES / 1000000
 )}mb`;
 
-export const generateHeaderImageFromImgix = (imageSrc: string): string =>
-  `${imageSrc}?txtsize=60.6&txtalign=center&txtfont=Helvetica,Bold`;
+export const generateHeaderImageFromImgix = (
+  imageSrc: string,
+  txtsize: number = 60,
+  txtalign: string = 'center',
+  txtfont: string = 'Helvetica,Bold'
+): string =>
+  `${imageSrc}?txtsize=${txtsize}&txtalign=${txtalign}&txtfont=${txtfont}`;
