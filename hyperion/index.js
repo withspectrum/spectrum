3e75dbc614e8f88882424fff4e7996d0ae442f14
Brian Lovin <briandlovin@gmail.com>
2018-04-23 08:57:27 -0700

    Fix consoles and flow

--- hyperion/index.js
+++ hyperion/index.js
@@ -1,6 +1,6 @@
 // @flow
-console.log('Hyperion starting...');
 const debug = require('debug')('hyperion');
+debug('Hyperion starting...');
 debug('logging with debug enabled');
 // $FlowFixMe
 require('isomorphic-fetch');
@@ -148,7 +148,7 @@ process.on('uncaughtException', async err => {
 
 Loadable.preloadAll().then(() => {
   app.listen(PORT);
-  console.log(
+  debug(
     `Hyperion, the server-side renderer, running at http://localhost:${PORT}`
   );
 });
--- shared/graphql/index.js
+++ shared/graphql/index.js
@@ -112,6 +112,6 @@ export const clearApolloStore = () => {
   try {
     client.resetStore();
   } catch (e) {
-    console.log('error clearing store');
+    console.error('error clearing store');
   }
 };
