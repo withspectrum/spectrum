f995c49170fb837360d3661cdbe1efdef54a5332
Brian Lovin <briandlovin@gmail.com>
2018-04-26 13:25:52 -0700

    Add mutation resolver to update channel slack bot settings

---
+++ api/mutations/channel/updateChannelSlackBotConnection.js
@@ -0,0 +1,25 @@
+// @flow
+import type { GraphQLContext } from '../../';
+import UserError from '../../utils/UserError';
+import { userCanManageChannel } from './utils';
+import { updateChannelSlackBotConnection } from '../../models/channelSettings';
+
+export type UpdateChannelSlackBotConnectionInput = {
+  input: {
+    channelId: string,
+    slackChannelId: ?string,
+    eventType: 'THREAD_CREATED',
+  },
+};
+
+export default async (
+  _: any,
+  { input }: UpdateChannelSlackBotConnectionInput,
+  { user }: GraphQLContext
+) => {
+  if (await !userCanManageChannel(user.id, input.channelId)) {
+    return new UserError('You don’t have permission to manage this channel');
+  }
+
+  return await updateChannelSlackBotConnection(input);
+};
