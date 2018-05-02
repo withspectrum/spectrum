5743014319c9bbdc9140899e70519655670d3c98
Brian Lovin <briandlovin@gmail.com>
2018-05-01 20:40:57 -0700

    Add tests for querying channel slack settings

--- api/queries/channel/slackSettings.js
+++ api/queries/channel/slackSettings.js
@@ -2,16 +2,17 @@
 import type { DBChannel } from 'shared/types';
 import type { GraphQLContext } from '../../';
 import UserError from '../../utils/UserError';
-import { canModerateChannel } from '../../utils/permissions';
+import {
+  canModerateChannel,
+  isAuthedResolver as requireAuth,
+} from '../../utils/permissions';
 
-export default async (
-  { id }: DBChannel,
-  _: any,
-  { loaders, user }: GraphQLContext
-) => {
-  if (!await canModerateChannel(user.id, id, loaders)) {
-    return new UserError('You don’t have permission to manage this channel');
+export default requireAuth(
+  async ({ id }: DBChannel, _: any, { loaders, user }: GraphQLContext) => {
+    if (!await canModerateChannel(user.id, id, loaders)) {
+      return new UserError('You don’t have permission to manage this channel');
+    }
+
+    return await loaders.channelSettings.load(id);
   }
-
-  return await loaders.channelSettings.load(id);
-};
+);
