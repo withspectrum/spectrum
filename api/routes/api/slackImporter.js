321967c42a79f454f96c2d9e154554dd24a405cb
Brian Lovin <briandlovin@gmail.com>
2018-05-02 11:58:59 -0700

    Revert "Attempt fallback to allow alpha redirects for slack connections"

--- api/routes/api/slackImporter.js
+++ api/routes/api/slackImporter.js
@@ -13,11 +13,8 @@ slackRouter.get('/', (req: any, res: any) => {
   const code = req.query.code;
   const communityId = req.query.state;
   const connectedBy = req.user.id;
-  const clientUrl = req.query.clientUrl;
   const returnURI = IS_PROD
-    ? clientUrl.indexOf('alpha') >= 0
-      ? 'https://alpha.spectrum.chat/api/slack'
-      : 'https://spectrum.chat/api/slack'
+    ? 'https://spectrum.chat/api/slack'
     : 'http://localhost:3001/api/slack';
 
   // generate an oauth token. This token will be used to communicate with the Slack API to get user information, and we'll store the token in the db record to allow for the user to access their Slack team info in the future.
@@ -40,9 +37,7 @@ slackRouter.get('/', (req: any, res: any) => {
     .then(community => community.slug)
     .then(slug => {
       return IS_PROD
-        ? clientUrl.indexOf('alpha') >= 0
-          ? res.redirect(`https://alpha.spectrum.chat/${slug}/settings`)
-          : res.redirect(`https://spectrum.chat/${slug}/settings`)
+        ? res.redirect(`https://spectrum.chat/${slug}/settings`)
         : res.redirect(`http://localhost:3000/${slug}/settings`);
     });
 });
@@ -52,11 +47,8 @@ slackRouter.get('/onboarding', (req: any, res: any) => {
   const code = req.query.code;
   const communityId = req.query.state;
   const connectedBy = req.user.id;
-  const clientUrl = req.query.clientUrl;
   const returnURI = IS_PROD
-    ? clientUrl.indexOf('alpha') >= 0
-      ? 'https://alpha.spectrum.chat/api/slack/onboarding'
-      : 'https://spectrum.chat/api/slack/onboarding'
+    ? 'https://spectrum.chat/api/slack/onboarding'
     : 'http://localhost:3001/api/slack/onboarding';
 
   // generate an oauth token. This token will be used to communicate with the Slack API to get user information, and we'll store the token in the db record to allow for the user to access their Slack team info in the future.
@@ -79,11 +71,7 @@ slackRouter.get('/onboarding', (req: any, res: any) => {
     .then(community => community.id)
     .then(id => {
       return IS_PROD
-        ? clientUrl.indexOf('alpha') >= 0
-          ? res.redirect(
-              `https://alpha.spectrum.chat/new/community?s=2&id=${id}`
-            )
-          : res.redirect(`https://spectrum.chat/new/community?s=2&id=${id}`)
+        ? res.redirect(`https://spectrum.chat/new/community?s=2&id=${id}`)
         : res.redirect(`http://localhost:3000/new/community?s=2&id=${id}`);
     });
 });
