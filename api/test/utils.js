1f7b86df4bf8cc60af2d4b89f498d4acf7d8fefb
Brian Lovin <briandlovin@gmail.com>
2018-05-01 17:16:45 -0700

    Fix tests context construction

--- api/test/utils.js
+++ api/test/utils.js
@@ -2,7 +2,6 @@
 import { graphql } from 'graphql';
 import createLoaders from '../loaders';
 import schema from '../schema';
-import permissions from '../routes/api/permissions';
 
 type Options = {
   context?: {
@@ -13,46 +12,11 @@ type Options = {
 
 // Nice little helper function for tests
 export const request = (query: mixed, { context, variables }: Options = {}) => {
-  const loaders = createLoaders();
-
-  let currentUser =
-    context && context.user && !context.user.bannedAt ? context.user : null;
-
-  if (context && context.user && context.user.id && !context.user.bannedAt) {
-    const thisPermissions = permissions(context.user.id, loaders);
-
-    const {
-      communityPermissions,
-      channelPermissions,
-      canAdministerChannel,
-      canModerateChannel,
-      canAdministerCommunity,
-      canModerateCommunity,
-    } = thisPermissions;
-
-    currentUser = Object.assign({}, currentUser, {
-      communityPermissions,
-      channelPermissions,
-      canAdministerChannel,
-      canModerateChannel,
-      canAdministerCommunity,
-      canModerateCommunity,
-    });
-  }
-
-  const newContext = Object.assign({}, context, {
-    user: currentUser
-      ? {
-          ...currentUser,
-        }
-      : null,
-  });
-
   return graphql(
     schema,
     query,
     undefined,
-    { loaders: createLoaders(), ...newContext },
+    { loaders: createLoaders(), ...context },
     variables
   );
 };
