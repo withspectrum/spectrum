0b702d3dacee0ba0762f164e886f83b412924efa
Maximilian Stoiber <contact@mxstbr.com>
2018-05-03 10:45:04 +0200

    Implement graphql-crunch
    Closes #2994

--- api/routes/api/graphql.js
+++ api/routes/api/graphql.js
@@ -2,6 +2,7 @@
 import { graphqlExpress } from 'graphql-server-express';
 import depthLimit from 'graphql-depth-limit';
 import costAnalysis from 'graphql-cost-analysis';
+import { crunch } from 'graphql-crunch';
 import UserError from '../../utils/UserError';
 import createLoaders from '../../loaders/';
 import createErrorFormatter from '../../utils/create-graphql-error-formatter';
@@ -17,6 +18,17 @@ export default graphqlExpress(req => {
   return {
     schema,
     formatError: createErrorFormatter(req),
+    formatResponse: response => {
+      if (
+        req.query.crunch !== undefined &&
+        response.data &&
+        !response.data.__schema
+      ) {
+        response.data = crunch(response.data);
+      }
+
+      return response;
+    },
     context: {
       loaders,
       user: currentUser,
