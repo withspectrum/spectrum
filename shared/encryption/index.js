ec2e7aa0b1ae4fce3dec384a372f83e85d973f0f
Brian Lovin <briandlovin@gmail.com>
2018-05-03 17:25:49 -0700

    Create general purpose encryption and decryption with new secret

---
+++ shared/encryption/index.js
@@ -0,0 +1,45 @@
+// @flow
+require('now-env');
+const crypto = require('crypto');
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // Must be 256 bytes (32 characters)
+const IV_LENGTH = 16; // For AES, this is always 16
+
+const encrypt = (text: string) => {
+  if (!text || text.length === 0) return text;
+
+  let iv = crypto.randomBytes(IV_LENGTH);
+  // $FlowFixMe
+  let cipher = crypto.createCipheriv(
+    'aes-256-cbc',
+    new Buffer(ENCRYPTION_KEY),
+    iv
+  );
+  // $FlowFixMe
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+};
+
+const decrypt = (text: string) => {
+  if (!text || text.length === 0) return text;
+
+  let textParts = text.split(':');
+  let iv = new Buffer(textParts.shift(), 'hex');
+  let encryptedText = new Buffer(textParts.join(':'), 'hex');
+  // $FlowFixMe
+  let decipher = crypto.createDecipheriv(
+    'aes-256-cbc',
+    new Buffer(ENCRYPTION_KEY),
+    iv
+  );
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return decrypted.toString();
+};
+
+module.exports = { decrypt, encrypt };
