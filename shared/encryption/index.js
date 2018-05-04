d241f87d7236e2c631bc7b7aac5071c1b8d1abaa
Brian Lovin <briandlovin@gmail.com>
2018-05-03 18:18:08 -0700

    Make encryption functions run in migrations

--- shared/encryption/index.js
+++ shared/encryption/index.js
@@ -1,47 +1,47 @@
 // @flow
 require('now-env');
-const crypto = require('crypto');
+var crypto = require('crypto');
 
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // Must be 256 bytes (32 characters)
-const IV_LENGTH = 16; // For AES, this is always 16
+var ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // Must be 256 bytes (32 characters)
+var IV_LENGTH = 16; // For AES, this is always 16
 
-const encrypt = (text: string): string => {
+function encrypt(text /*: string */) /*: string */ {
   if (!text || text.length === 0) return text;
 
-  let iv = crypto.randomBytes(IV_LENGTH);
+  var iv = crypto.randomBytes(IV_LENGTH);
 
-  let cipher = crypto.createCipheriv(
+  var cipher = crypto.createCipheriv(
     'aes-256-cbc',
     // $FlowFixMe
     new Buffer(ENCRYPTION_KEY),
     iv
   );
   // $FlowFixMe
-  let encrypted = cipher.update(text);
+  var encrypted = cipher.update(text);
 
   encrypted = Buffer.concat([encrypted, cipher.final()]);
 
   return iv.toString('hex') + ':' + encrypted.toString('hex');
-};
+}
 
-const decrypt = (text: string): string => {
+function decrypt(text /*: string */) /*: string */ {
   if (!text || text.length === 0) return text;
 
-  let textParts = text.split(':');
-  let iv = new Buffer(textParts.shift(), 'hex');
-  let encryptedText = new Buffer(textParts.join(':'), 'hex');
+  var textParts = text.split(':');
+  var iv = new Buffer(textParts.shift(), 'hex');
+  var encryptedText = new Buffer(textParts.join(':'), 'hex');
 
-  let decipher = crypto.createDecipheriv(
+  var decipher = crypto.createDecipheriv(
     'aes-256-cbc',
     // $FlowFixMe
     new Buffer(ENCRYPTION_KEY),
     iv
   );
-  let decrypted = decipher.update(encryptedText);
+  var decrypted = decipher.update(encryptedText);
 
   decrypted = Buffer.concat([decrypted, decipher.final()]);
 
   return decrypted.toString();
-};
+}
 
 module.exports = { decrypt, encrypt };
