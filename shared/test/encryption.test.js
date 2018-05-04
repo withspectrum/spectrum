9bb49c60933e656ac6f21aa58d1ac0a0352424fb
Brian Lovin <briandlovin@gmail.com>
2018-05-04 12:44:34 -0700

    Improve tests

--- shared/test/encryption.test.js
+++ shared/test/encryption.test.js
@@ -12,13 +12,19 @@ const inputObject = { foo: 'bar', biz: 'baz' };
 it('should encrypt and decrypt a string', () => {
   const encrypted = encryptString(inputString);
   const decrypted = decryptString(encrypted);
+
   expect(encrypted).not.toEqual(inputString);
   expect(decrypted).toEqual(inputString);
+  expect(typeof decrypted).toEqual('string');
+  expect(typeof encrypted).toEqual('string');
 });
 
 it('should encrypt and decrypt a object', () => {
   const encrypted = encryptObject(inputObject);
   const decrypted = decryptObject(encrypted);
+
   expect(encrypted).not.toEqual(inputObject);
   expect(decrypted).toEqual(inputObject);
+  expect(typeof decrypted).toEqual('object');
+  expect(typeof encrypted).toEqual('string');
 });
