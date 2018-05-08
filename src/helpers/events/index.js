11e9c9ff7b3e56bd527a4b31ce2a04c924cd76b1
Brian Lovin <briandlovin@gmail.com>
2018-05-08 08:23:48 -0700

    Set client default property on client events

--- src/helpers/events/index.js
+++ src/helpers/events/index.js
@@ -15,7 +15,10 @@ const amplitude = window.amplitude;
 export const track = (eventType: string, eventProperties?: Object = {}) => {
   const amplitudePromise = () => {
     console.warn(`[Amplitude] Tracking ${eventType}`);
-    return amplitude.getInstance().logEvent(eventType, eventProperties);
+    return amplitude.getInstance().logEvent(eventType, {
+      ...eventProperties,
+      client: 'web',
+    });
   };
 
   return Promise.all([amplitudePromise()]);
