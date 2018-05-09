d3a2ee993b6d33875306e5b3ddb393572fbd09b9
Brian Lovin <briandlovin@gmail.com>
2018-05-09 16:07:26 -0700

    Implement tracking job

--- analytics/queues/track-analytics.js
+++ analytics/queues/track-analytics.js
@@ -1,7 +1,15 @@
 // @flow
 const debug = require('debug')('analytics:queues:track');
+import Raven from 'shared/raven';
+import type { Job, TrackAnalyticsData } from 'shared/bull/types';
+import { getContext, track, hash } from '../utils';
 
-const processJob = async (job: Job<TrackAnalyticsData>) => {};
+const processJob = async (job: Job<TrackAnalyticsData>) => {
+  const { userId, event, data } = job.data;
+  const eventProperties = await getContext({ userId, ...data });
+
+  return await track(hash(userId), event, eventProperties);
+};
 
 export default async (job: Job<TrackAnalyticsData>) => {
   try {
