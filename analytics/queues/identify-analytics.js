48f546ba90d394548c1b62f3b75fd0eac7f902af
Brian Lovin <briandlovin@gmail.com>
2018-05-09 16:07:35 -0700

    Implement identifying job

--- analytics/queues/identify-analytics.js
+++ analytics/queues/identify-analytics.js
@@ -1,7 +1,17 @@
 // @flow
-const debug = require('debug')('analytics:queues:track');
+const debug = require('debug')('analytics:queues:identify');
+import Raven from 'shared/raven';
+import type { Job, IdentifyAnalyticsData } from 'shared/bull/types';
+import { getUserById } from '../models/user';
+import { identify, transformations, hash } from '../utils';
 
-const processJob = async (job: Job<IdentifyAnalyticsData>) => {};
+const processJob = async (job: Job<IdentifyAnalyticsData>) => {
+  const { userId } = job.data;
+  const user = await getUserById(userId);
+  const analyticsUser = transformations.analyticsUser(user);
+
+  return await identify(hash(userId), analyticsUser);
+};
 
 export default async (job: Job<IdentifyAnalyticsData>) => {
   try {
