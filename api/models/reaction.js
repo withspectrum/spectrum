c17b0e9ad813bbbbab88987421986389334b9996
Brian Lovin <briandlovin@gmail.com>
2018-05-09 16:08:53 -0700

    Refactor reactions model to not actually delete reactions

--- api/models/reaction.js
+++ api/models/reaction.js
@@ -4,8 +4,8 @@ import {
   sendReactionNotificationQueue,
   processReputationEventQueue,
 } from 'shared/bull/queues';
-import { track, events } from 'shared/analytics';
-import { getEntityDataForAnalytics } from '../utils/analytics';
+import { events } from 'shared/analytics';
+import { trackQueue } from 'shared/bull/queues';
 
 type ReactionType = 'like';
 
@@ -29,6 +29,7 @@ export const getReactions = (
   return db
     .table('reactions')
     .getAll(...distinctMessageIds, { index: 'messageId' })
+    .filter(row => row.hasFields('deletedAt').not())
     .group('messageId')
     .run();
 };
@@ -60,60 +61,89 @@ export const toggleReaction = (
     .filter({ userId })
     .run()
     .then(async result => {
-      // this user has already reacted to the message, remove the reaction
-      if (result.length > 0) {
-        const existing = result[0];
+      // user has already reacted
+      if (result && result.length > 0) {
+        const thisReaction = result[0];
 
-        const eventProperties = await getEntityDataForAnalytics(loaders)({
-          reactionId: existing.id,
+        // user is re-reacting
+        if (thisReaction.deletedAt) {
+          trackQueue.add({
+            userId,
+            event: events.REACTION_CREATED,
+            data: {
+              reactionId: thisReaction.id,
+            },
+          });
+
+          processReputationEventQueue.add({
+            userId,
+            type: 'reaction created',
+            entityId: thisReaction.messageId,
+          });
+
+          return db
+            .table('reactions')
+            .get(thisReaction.id)
+            .update({
+              deletedAt: db.literal(),
+            })
+            .run();
+        }
+
+        // deleting reaction
+        trackQueue.add({
           userId,
+          event: events.REACTION_DELETED,
+          data: {
+            reactionId: thisReaction.id,
+          },
         });
-
-        track(userId, events.REACTION_DELETED, eventProperties);
 
         processReputationEventQueue.add({
           userId,
           type: 'reaction deleted',
-          entityId: existing.messageId,
+          entityId: thisReaction.messageId,
         });
 
         return db
           .table('reactions')
-          .get(existing.id)
-          .delete()
+          .get(thisReaction.id)
+          .update({
+            deletedAt: new Date(),
+          })
           .run();
-      } else {
-        processReputationEventQueue.add({
-          userId,
-          type: 'reaction created',
-          entityId: reaction.messageId,
-        });
-
-        return db
-          .table('reactions')
-          .insert(
-            {
-              ...reaction,
-              userId,
-              timestamp: Date.now(),
-            },
-            { returnChanges: true }
-          )
-          .run()
-          .then(result => result.changes[0].new_val)
-          .then(async reaction => {
-            const eventProperties = await getEntityDataForAnalytics(loaders)({
-              reactionId: reaction.id,
-              userId,
-            });
-
-            track(userId, events.REACTION_CREATED, eventProperties);
-
-            sendReactionNotificationQueue.add({ reaction, userId });
-
-            return reaction;
-          });
       }
+
+      // user has not reacted yet
+      return db
+        .table('reactions')
+        .insert(
+          {
+            ...reaction,
+            userId,
+            timestamp: Date.now(),
+          },
+          { returnChanges: true }
+        )
+        .run()
+        .then(result => result.changes[0].new_val)
+        .then(reaction => {
+          trackQueue.add({
+            userId,
+            event: events.REACTION_CREATED,
+            data: { reactionId: reaction.id },
+          });
+
+          sendReactionNotificationQueue.add({ reaction, userId });
+
+          processReputationEventQueue.add({
+            userId,
+            type: 'reaction created',
+            entityId: reaction.messageId,
+          });
+
+          return reaction;
+        });
     })
     .then(() => {
       // return the message object itself in order to more easily update the UI with the apollo store
