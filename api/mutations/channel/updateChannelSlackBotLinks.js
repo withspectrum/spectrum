ec73814f2ebee1f8ec511b6413704346ab9cdcca
Brian Lovin <briandlovin@gmail.com>
2018-05-08 22:52:52 -0700

    Add tracking for channel slack bot link settings

--- api/mutations/channel/updateChannelSlackBotLinks.js
+++ api/mutations/channel/updateChannelSlackBotLinks.js
@@ -6,6 +6,8 @@ import {
   isAuthedResolver as requireAuth,
   canModerateChannel,
 } from '../../utils/permissions';
+import { events } from 'shared/analytics';
+import { getEntityDataForAnalytics } from '../../utils/analytics';
 
 export type UpdateChannelSlackBotLinksInput = {
   input: {
@@ -19,11 +21,23 @@ export default requireAuth(
   async (
     _: any,
     { input }: UpdateChannelSlackBotLinksInput,
-    { user, loaders }: GraphQLContext
+    { user, track, loaders }: GraphQLContext
   ) => {
+    const defaultTrackingData = await getEntityDataForAnalytics(loaders)({
+      channelId: input.channelId,
+      userId: user.id,
+    });
+
     if (!await canModerateChannel(user.id, input.channelId, loaders)) {
+      track(events.CHANNEL_SLACK_BOT_LINK_UPDATED_FAILED, {
+        ...defaultTrackingData,
+        reason: 'no permission',
+      });
+
       return new UserError('You don’t have permission to manage this channel');
     }
+
+    track(events.CHANNEL_SLACK_BOT_LINK_UPDATED, defaultTrackingData);
 
     return await updateChannelSlackBotLinks(input);
   }
