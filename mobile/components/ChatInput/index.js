5b7a119d6483156156a729db726ad495d2c77d02
Maximilian Stoiber <contact@mxstbr.com>
2018-05-15 16:20:32 +0200

    Style the chat input

--- mobile/components/ChatInput/index.js
+++ mobile/components/ChatInput/index.js
@@ -2,6 +2,11 @@
 import React from 'react';
 import { TextInput, View, Button } from 'react-native';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
+import {
+  ChatInputWrapper,
+  ChatInputTextInputWrapper,
+  SendButton,
+} from './style';
 
 type Props = {
   onSubmit: (text: string) => void,
@@ -27,17 +32,21 @@ class ChatInput extends React.Component<Props, State> {
 
   render() {
     return (
-      <View>
-        <TextInput
-          value={this.state.value}
-          onChangeText={this.onChangeText}
-          placeholder="Your message here..."
-          multiline
-          autoFocus
-          disableFullscreenUI
-          onSubmitEditing={this.submit}
-        />
-        <Button onPress={this.submit} title="Send" />
+      <View style={{ width: '100%' }}>
+        <ChatInputWrapper>
+          <ChatInputTextInputWrapper>
+            <TextInput
+              value={this.state.value}
+              onChangeText={this.onChangeText}
+              placeholder="Your message here..."
+              multiline
+              autoFocus
+              disableFullscreenUI
+              onSubmitEditing={this.submit}
+            />
+          </ChatInputTextInputWrapper>
+          <SendButton onPress={this.submit} size={32} />
+        </ChatInputWrapper>
         {/* NOTE(@mxstbr): Magic number, otherwise the chatinput is way above the keyboard */}
         <KeyboardSpacer topSpacing={-75} />
       </View>
