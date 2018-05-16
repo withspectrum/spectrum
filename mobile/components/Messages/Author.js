6cf9c90fc4b8de9ba494b019abce9d758e45914d
Brian Lovin <briandlovin@gmail.com>
2018-05-15 17:13:21 -0700

    Make message bylines tappable

--- mobile/components/Messages/Author.js
+++ mobile/components/Messages/Author.js
@@ -3,11 +3,16 @@ import React from 'react';
 import styled from 'styled-components/native';
 import Text from '../Text';
 import Avatar from '../Avatar';
+import compose from 'recompose/compose';
+import { withNavigation } from 'react-navigation';
 import type { ThreadParticipantType } from '../../../shared/graphql/fragments/thread/threadParticipant';
+import type { Navigation } from '../../utils/types';
+import { TouchableHighlight } from 'react-native';
 
 type Props = {
   author: ThreadParticipantType,
   me: boolean,
+  navigation: Navigation,
 };
 
 const AuthorWrapper = styled.View`
@@ -17,13 +22,19 @@ const AuthorWrapper = styled.View`
   align-items: flex-end;
 `;
 
-export default ({ author, me }: Props) => {
+const Byline = ({ author, me, navigation }: Props) => {
   return (
-    <AuthorWrapper me={me}>
-      {!me && <Avatar src={author.user.profilePhoto} size={16} radius={8} />}
-      <Text type="footnote" color={props => props.theme.text.alt}>
-        <Text bold>{author.user.name}</Text> (@{author.user.username})
-      </Text>
-    </AuthorWrapper>
+    <TouchableHighlight
+      onPress={() => navigation.navigate(`User`, { id: author.user.id })}
+    >
+      <AuthorWrapper me={me}>
+        {!me && <Avatar src={author.user.profilePhoto} size={16} radius={8} />}
+        <Text type="footnote" color={props => props.theme.text.alt}>
+          <Text bold>{author.user.name}</Text> (@{author.user.username})
+        </Text>
+      </AuthorWrapper>
+    </TouchableHighlight>
   );
 };
+
+export default compose(withNavigation)(Byline);
