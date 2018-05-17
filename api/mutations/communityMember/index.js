9f50cfec0055967534dfa53c6983df33f2df178e
Brian Lovin <briandlovin@gmail.com>
2018-05-17 16:39:26 -0600

    Actually wire up the mutations

--- api/mutations/communityMember/index.js
+++ api/mutations/communityMember/index.js
@@ -1,18 +1,26 @@
 // @flow
 import addCommunityMember from './addCommunityMember';
+import addPendingCommunityMember from './addPendingCommunityMember';
 import removeCommunityMember from './removeCommunityMember';
+import removePendingCommunityMember from './removePendingCommunityMember';
 import addCommunityModerator from './addCommunityModerator';
+import approvePendingCommunityMember from './approvePendingCommunityMember';
 import removeCommunityModerator from './removeCommunityModerator';
 import blockCommunityMember from './blockCommunityMember';
+import blockPendingCommunityMember from './blockPendingCommunityMember';
 import unblockCommunityMember from './unblockCommunityMember';
 
 module.exports = {
   Mutation: {
     addCommunityMember,
+    addPendingCommunityMember,
     removeCommunityMember,
+    removePendingCommunityMember,
     addCommunityModerator,
+    approvePendingCommunityMember,
     removeCommunityModerator,
     blockCommunityMember,
+    blockPendingCommunityMember,
     unblockCommunityMember,
   },
 };
