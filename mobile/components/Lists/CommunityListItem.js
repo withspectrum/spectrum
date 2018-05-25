5d24cce2cc6141ab505a97c33a069365685f9c8b
Brian Lovin <briandlovin@gmail.com>
2018-05-24 23:40:03 -0700

    Add community list item component

---
+++ mobile/components/Lists/CommunityListItem.js
@@ -0,0 +1,29 @@
+// @flow
+import * as React from 'react';
+import Avatar from '../Avatar';
+import { ListItemWithAvatar } from './ListItemWithAvatar';
+import { TextColumnContainer, Title, Subtitle } from './style';
+import type { Navigation } from '../../utils/types';
+
+type CommunityListItemType = { community: Object, navigation: Navigation };
+
+export class CommunityListItem extends React.Component<CommunityListItemType> {
+  render() {
+    const { community, navigation } = this.props;
+    return (
+      <ListItemWithAvatar
+        onPress={() => navigation.navigate(`Community`, { id: community.id })}
+        AvatarComponent={() => (
+          <Avatar src={community.profilePhoto} size={40} community />
+        )}
+      >
+        <TextColumnContainer>
+          <Title numberOfLines={1}>{community.name}</Title>
+          <Subtitle numberOfLines={1}>
+            {community.metaData.members} members
+          </Subtitle>
+        </TextColumnContainer>
+      </ListItemWithAvatar>
+    );
+  }
+}
