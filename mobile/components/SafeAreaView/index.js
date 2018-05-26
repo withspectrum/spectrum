cb1aaff0501222a8ffdb2d02082618db07da3bf2
ryota-murakami <dojce1048@gmail.com>
2018-05-27 01:44:21 +0900

    fix $FlowFixMe

--- mobile/components/SafeAreaView/index.js
+++ mobile/components/SafeAreaView/index.js
@@ -3,11 +3,12 @@ import React, { type Node } from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 import { SafeAreaView } from 'react-native';
 
-export default function withSafeAreaView(WrappedComponent: Node): () => Node {
+export default function withSafeAreaView(
+  WrappedComponent: React$ComponentType<any>
+): () => Node {
   function EnhancedComponent(props: any) {
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: '#CCC' }}>
-        {/* $FlowFixMe */}
         <WrappedComponent {...props} />
       </SafeAreaView>
     );
