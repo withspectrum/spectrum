8d52f4978e69feb503b72adaccb0ccf5441928af
ryota-murakami <dojce1048@gmail.com>
2018-05-27 18:46:27 +0900

    pass currentUser to ThreadFeed

--- mobile/views/Dashboard/index.js
+++ mobile/views/Dashboard/index.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import compose from 'recompose/compose';
 import withSafeView from '../../components/SafeAreaView';
+import Loading from '../../components/Loading';
+import { Wrapper } from './style';
 import ThreadFeed from '../../components/ThreadFeed';
 import getCurrentUserEverythingFeed from '../../../shared/graphql/queries/user/getCurrentUserEverythingFeed';
 import type { Navigation } from '../../utils/types';
@@ -10,8 +12,7 @@ import {
   getCurrentUser,
   type GetUserType,
 } from '../../../shared/graphql/queries/user/getUser';
-
-import { Wrapper } from './style';
+import type { ViewNetworkHandlerProps } from '../../components/ViewNetworkHandler';
 
 const EverythingThreadFeed = compose(getCurrentUserEverythingFeed)(ThreadFeed);
 
@@ -19,15 +20,19 @@ type Props = {
   navigation: Navigation,
   data: {
     user: GetUserType,
+    ...$Exact<ViewNetworkHandlerProps>,
   },
 };
 
 class Dashboard extends Component<Props> {
   render() {
+    const { data } = this.props;
+    if (data.loading) <Loading />;
+
     return (
       <Wrapper>
         <View testID="welcome" style={{ flex: 1 }}>
-          <EverythingThreadFeed />
+          <EverythingThreadFeed currentUser={data.user} />
         </View>
       </Wrapper>
     );
