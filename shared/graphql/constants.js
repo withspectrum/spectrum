ac2e8348f259b4036ea91d2577aa62889347650e
Maximilian Stoiber <contact@mxstbr.com>
2018-06-08 10:41:16 +0200

    Query local API even when running SSR locally

--- shared/graphql/constants.js
+++ shared/graphql/constants.js
@@ -1,7 +1,8 @@
 // @flow
 // The constants for the GraphQL client on the web
 
-export const IS_PROD = process.env.NODE_ENV === 'production';
+export const IS_PROD =
+  process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV;
 // In production the API is at the same URL, in development it's at a different port
 export const API_URI = IS_PROD ? '/api' : 'http://localhost:3001/api';
 export const WS_URI = IS_PROD
