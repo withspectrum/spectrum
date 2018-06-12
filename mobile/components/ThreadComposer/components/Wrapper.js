5c48368e70a193f5b36cd061cfe3b77c4bd336de
Maximilian Stoiber <contact@mxstbr.com>
2018-06-12 11:23:20 +0200

    Add wireframe of thread composer

---
+++ mobile/components/ThreadComposer/components/Wrapper.js
@@ -0,0 +1,7 @@
+// @flow
+import styled from 'styled-components/native';
+
+export default styled.ScrollView`
+  height: 100%;
+  background-color: ${props => props.theme.bg.default};
+`;
--- mobile/components/ThreadComposer/index.js
+++ mobile/components/ThreadComposer/index.js
@@ -1,16 +1,26 @@
 // @flow
 import React, { Fragment } from 'react';
-import { TextInput } from 'react-native';
+import { TextInput, Button, Picker } from 'react-native';
+import Wrapper from './components/Wrapper';
 
 type Props = {||};
 
 class ThreadComposer extends React.Component<Props> {
   render() {
     return (
-      <Fragment>
-        <TextInput autoFocus />
-        <TextInput multiline />
-      </Fragment>
+      <Wrapper>
+        <Picker selectedValue="first">
+          <Picker.Item label="First community" value="first" />
+          <Picker.Item label="Second community" value="second" />
+        </Picker>
+        <Picker selectedValue="first">
+          <Picker.Item label="First channel" value="first" />
+          <Picker.Item label="Second channel" value="second" />
+        </Picker>
+        <TextInput autoFocus placeholder="What's up?" />
+        <TextInput multiline placeholder="Write more thoughts here..." />
+        <Button onPress={() => null} title="Publish" />
+      </Wrapper>
     );
   }
 }
