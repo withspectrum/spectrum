8d3c91310ef8ae2310adc6ba4f9ac55f2816c71a
Brian Lovin <briandlovin@gmail.com>
2018-06-12 13:55:48 -0700

    Fix test

--- cypress/integration/concierge_spec.js
+++ cypress/integration/concierge_spec.js
@@ -1,9 +1,9 @@
 describe('Renders conceirge page ', () => {
   beforeEach(() => {
-    cy.visit(`/pricing/conceirge`);
+    cy.visit(`/pricing/concierge`);
   });
 
   it('should render key conceirge page components', () => {
-    cy.get('[data-cy="conceirge-page"]').should('be.visible');
+    cy.get('[data-cy="concierge-page"]').should('be.visible');
   });
 });
