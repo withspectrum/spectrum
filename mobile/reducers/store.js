c84280d20dbc109e97638863052caf6add11e989
Brian Lovin <briandlovin@gmail.com>
2018-06-11 20:48:30 -0700

    Fix peril

--- mobile/reducers/store.js
+++ mobile/reducers/store.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+// @flow
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import getReducers from './index';
@@ -9,7 +9,9 @@ const composeEnhancers = compose;
 // init the store with the thunkMiddleware which allows us to make async actions play nicely with the store
 // Allow dependency injection of extra reducers and middleware, we need this for SSR
 export const initStore = (
+  // $FlowFixMe
   initialState,
+  // $FlowFixMe
   { middleware = [], reducers = {} } = {}
 ) => {
   if (initialState) {
