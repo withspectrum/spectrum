cf554b8e5dce793a28b8cbe8d80766d785371c9f
Brian Lovin <briandlovin@gmail.com>
2018-06-12 17:03:48 -0700

    Temporarily remove dashboard thread redirect logic

--- shared/middlewares/thread-param.js
+++ shared/middlewares/thread-param.js
@@ -2,7 +2,7 @@
 
 const threadParamRedirect = (req, res, next) => {
   // Redirect /?t=asdf123 if the user isn't logged in
-  if (!req.user && req.query.t) {
+  if (req.query.t) {
     res.redirect(`/thread/${req.query.t}`);
     // Redirect /anything?thread=asdf123
   } else if (req.query.thread) {
