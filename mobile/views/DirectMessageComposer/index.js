6b5cfc556b65a676d913aaf6b52fd997d49c1c8b
Maximilian Stoiber <contact@mxstbr.com>
2018-06-13 13:45:04 +0200

    Set up modal stack navigator for Dm composition

---
+++ mobile/views/DirectMessageComposer/index.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+import Text from '../../components/Text';
+
+type Props = {};
+
+class DirectMessageComposer extends React.Component<Props> {
+  render() {
+    return <Text>DM Composer</Text>;
+  }
+}
+
+export default DirectMessageComposer;
--- mobile/views/TabBar/DirectMessageStack.js
+++ mobile/views/TabBar/DirectMessageStack.js
@@ -1,19 +1,30 @@
 // @flow
+import React from 'react';
 import { createStackNavigator } from 'react-navigation';
+import { Button } from 'react-native';
+import { withMappedNavigationProps } from 'react-navigation-props-mapper';
 import BaseStack from './BaseStack';
 import DirectMessages from '../DirectMessages';
 import DirectMessageThread from '../DirectMessageThread';
+import DirectMessageComposer from '../DirectMessageComposer';
+import type { NavigationScreenConfigProps } from 'react-navigation';
 
 const DMStack = createStackNavigator(
   {
     DirectMessages: {
-      screen: DirectMessages,
+      screen: withMappedNavigationProps(DirectMessages),
       navigationOptions: ({ navigation }) => ({
         headerTitle: navigation.getParam('title', 'Messages'),
+        headerRight: (
+          <Button
+            onPress={() => navigation.navigate('DirectMessageComposer')}
+            title="New"
+          />
+        ),
       }),
     },
     DirectMessageThread: {
-      screen: DirectMessageThread,
+      screen: withMappedNavigationProps(DirectMessageThread),
       navigationOptions: ({ navigation }) => ({
         headerTitle: navigation.getParam('title', null),
         tabBarVisible: false,
@@ -26,4 +37,28 @@ const DMStack = createStackNavigator(
   }
 );
 
-export default DMStack;
+const ModalStack = createStackNavigator(
+  {
+    DirectMessages: {
+      screen: withMappedNavigationProps(DMStack),
+      // We don't want to show two headers, so we hide the header of the second stack
+      navigationOptions: {
+        header: null,
+      },
+    },
+    DirectMessageComposer: {
+      screen: withMappedNavigationProps(DirectMessageComposer),
+      navigationOptions: ({ navigation }: NavigationScreenConfigProps) => ({
+        headerTitle: navigation.getParam('title', 'New Direct Message'),
+        // TODO(@mxstbr): Replace with X icon
+        headerLeft: ({ onPress }) => <Button onPress={onPress} title="X" />,
+      }),
+    },
+  },
+  {
+    mode: 'modal',
+    initialRouteName: 'DirectMessages',
+  }
+);
+
+export default ModalStack;
