9f448556cb2f7587d1233ceee0c83edd42de433f
Brian Lovin <briandlovin@gmail.com>
2018-06-12 17:05:44 -0700

    Let private channel threads have copy link button

--- src/views/thread/components/actionBar.js
+++ src/views/thread/components/actionBar.js
@@ -306,56 +306,8 @@ class ActionBar extends React.Component<Props, State> {
                 Notify me
               </FollowButton>
             )}
-            {!thread.channel.isPrivate && (
+            {thread.channel.isPrivate && (
               <ShareButtons>
-                <ShareButton
-                  facebook
-                  tipText={'Share'}
-                  tipLocation={'top-left'}
-                  data-cy="thread-facebook-button"
-                >
-                  <a
-                    href={`https://www.facebook.com/sharer/sharer.php?u=https://spectrum.chat/thread/${
-                      thread.id
-                    }&t=${thread.content.title}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Icon
-                      glyph={'facebook'}
-                      size={24}
-                      onClick={() =>
-                        track(events.THREAD_SHARED, { method: 'facebook' })
-                      }
-                    />
-                  </a>
-                </ShareButton>
-
-                <ShareButton
-                  twitter
-                  tipText={'Tweet'}
-                  tipLocation={'top-left'}
-                  data-cy="thread-tweet-button"
-                >
-                  <a
-                    href={`https://twitter.com/share?text=${
-                      thread.content.title
-                    } on @withspectrum&url=https://spectrum.chat/thread/${
-                      thread.id
-                    }`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Icon
-                      glyph={'twitter'}
-                      size={24}
-                      onClick={() =>
-                        track(events.THREAD_SHARED, { method: 'twitter' })
-                      }
-                    />
-                  </a>
-                </ShareButton>
-
                 <Clipboard
                   style={{ background: 'none' }}
                   data-clipboard-text={`https://spectrum.chat/thread/${
