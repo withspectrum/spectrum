1de23fabd9cd8be9843b84bdf0fd4c312a4da546
Maximilian Stoiber <contact@mxstbr.com>
2018-06-13 14:47:29 +0200

    Implement searching for users in dm composer

--- mobile/views/DirectMessageComposer/index.js
+++ mobile/views/DirectMessageComposer/index.js
@@ -1,32 +1,63 @@
 // @flow
 import React from 'react';
-import { TextInput } from 'react-native';
+import { TextInput, View } from 'react-native';
 import Text from '../../components/Text';
 import ChatInput from '../../components/ChatInput';
 import { FullscreenNullState } from '../../components/NullStates';
 import PeopleSearchView from '../Search/PeopleSearchView';
-import { SearchView } from '../Search/style';
+import { getUserById } from '../../../shared/graphql/queries/user/getUser';
 import styled from 'styled-components/native';
 import type { NavigationProps } from 'react-navigation';
 
 const ComposerWrapper = styled.View`
   flex-direction: column;
-  justify-content: space-between;
   height: 100%;
   background-color: ${props => props.theme.bg.wash};
 `;
+
+const SearchInputArea = styled.View`
+  flex-direction: row;
+  justify-content: flex-start;
+  background-color: ${props => props.theme.bg.default};
+  border-bottom-width: 1;
+  border-bottom-color: ${props => props.theme.bg.border};
+`;
+
+const SelectedUsers = styled.View`
+  flex-direction: row;
+`;
+
+const SelectedUser = getUserById(({ data, onPressHandler }) => {
+  if (data.user) return <Text>{data.user.name}</Text>;
+
+  return null;
+});
 
 type Props = {
   ...$Exact<NavigationProps>,
 };
 
 type State = {
+  wipSearchString: string,
   searchString: string,
+  selectedUsers: string[],
 };
 
 class DirectMessageComposer extends React.Component<Props, State> {
   state = {
+    wipSearchString: '',
     searchString: '',
+    selectedUsers: [],
+  };
+
+  searchInput = {
+    clear: Function,
+  };
+
+  onChangeText = (text: string) => {
+    this.setState({
+      wipSearchString: text,
+    });
   };
 
   onFinishTyping = (e: { nativeEvent: { text: string } }) => {
@@ -35,27 +66,57 @@ class DirectMessageComposer extends React.Component<Props, State> {
     });
   };
 
+  selectUser = (userId: string) => {
+    this.setState(prev => ({
+      selectedUsers: prev.selectedUsers.concat([userId]),
+      wipSearchString: '',
+      searchString: '',
+    }));
+  };
+
+  removeSelectedUser = (userId: string) => () => {
+    this.setState(prev => ({
+      selectedUsers: prev.selectedUsers.filter(
+        selectedId => selectedId !== userId
+      ),
+    }));
+  };
+
   onSubmit = (text: string) => {};
 
   render() {
     return (
       <ComposerWrapper>
-        <TextInput
-          onEndEditing={this.onFinishTyping}
-          onSubmitEditing={this.onFinishTyping}
-          returnKeyType="search"
-        />
+        <SearchInputArea>
+          <SelectedUsers>
+            {this.state.selectedUsers.map(userId => (
+              <SelectedUser
+                key={userId}
+                id={userId}
+                onPressHandler={this.removeSelectedUser(userId)}
+              />
+            ))}
+          </SelectedUsers>
+          <TextInput
+            onChangeText={this.onChangeText}
+            onEndEditing={this.onFinishTyping}
+            onSubmitEditing={this.onFinishTyping}
+            value={this.state.wipSearchString}
+            returnKeyType="search"
+            style={{ flex: 1 }}
+          />
+        </SearchInputArea>
         {!this.state.searchString ? (
-          <SearchView>
-            <FullscreenNullState
-              title={'Search for people'}
-              subtitle={'Connect with people on Spectrum'}
-              icon={'person'}
-            />
-          </SearchView>
+          <FullscreenNullState
+            title={'Send Direct Messages'}
+            subtitle={
+              'Direct messages are private conversations between you and anyone else, including groups. Search for a person above to get started.'
+            }
+            icon={'message-new'}
+          />
         ) : (
           <PeopleSearchView
-            navigation={this.props.navigation}
+            onPress={this.selectUser}
             queryString={this.state.searchString}
           />
         )}
