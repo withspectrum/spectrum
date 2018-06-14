09ae4410f70a464bf10d76f6a38b8bcef9e968fe
Brian Lovin <briandlovin@gmail.com>
2018-06-13 21:38:52 -0700

    Simplify message and thread anchor distinction

--- mobile/components/Anchor/MessageAnchor.js
+++ mobile/components/Anchor/MessageAnchor.js
@@ -1,52 +1,8 @@
 // @flow
-import React, { type Node } from 'react';
-import { Share } from 'react-native';
-import { MessageAnchorText } from './style';
-import { WebBrowser } from 'expo';
+import Anchor from './ThreadAnchor';
+import styled from 'styled-components';
 
-type LinkProps = {
-  href: string,
-  children: Node,
-};
-
-type ButtonProps = {
-  onPress: () => void,
-  children: Node,
-};
-
-// Either URL or message has to be defined
-export type ShareContent =
-  | {
-      url: string,
-      message?: string,
-      title?: string,
-    }
-  | {
-      url?: string,
-      message: string,
-      title?: string,
-    };
-
-type ShareProps = {
-  content: ShareContent,
-  children: Node,
-};
-
-type Props = LinkProps | ButtonProps | ShareProps;
-
-export default class Anchor extends React.Component<Props> {
-  handlePress = () => {
-    if (typeof this.props.onPress === 'function') return this.props.onPress();
-    if (typeof this.props.href === 'string')
-      return WebBrowser.openBrowserAsync(this.props.href);
-    if (this.props.content) return Share.share(this.props.content);
-  };
-
-  render() {
-    return (
-      <MessageAnchorText {...this.props} onPress={this.handlePress}>
-        {this.props.children}
-      </MessageAnchorText>
-    );
-  }
-}
+export const MessageAnchor = styled(Anchor)`
+  color: ${props => props.theme.text.default};
+  font-weight: 700;
+`;
