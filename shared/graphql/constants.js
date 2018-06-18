56d6a1556e7a4985161d272a1d1338550dc67bcd
Maximilian Stoiber <contact@mxstbr.com>
2018-06-18 17:08:58 +0200

    Fix websockets in e2e tests

--- shared/graphql/constants.js
+++ shared/graphql/constants.js
@@ -1,10 +1,12 @@
 // @flow
 // The constants for the GraphQL client on the web
 
-export const IS_PROD =
-  process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV;
+export const IS_PROD = process.env.NODE_ENV === 'production';
 // In production the API is at the same URL, in development it's at a different port
 export const API_URI = IS_PROD ? '/api' : 'http://localhost:3001/api';
-export const WS_URI = IS_PROD
-  ? `wss://${window.location.host}/websocket`
-  : 'ws://localhost:3001/websocket';
+// If the production build is running locally connect to the websocket at localhost:3001
+// else connect at thisurl.whatever/websocket
+export const WS_URI =
+  IS_PROD && window.location.host !== 'localhost:3006'
+    ? `wss://${window.location.host}/websocket`
+    : 'ws://localhost:3001/websocket';
