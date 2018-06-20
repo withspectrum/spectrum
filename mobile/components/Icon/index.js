c8afc7798e74ba4c1551500adbc6d984c610c29a
Maximilian Stoiber <contact@mxstbr.com>
2018-06-20 11:22:49 +0200

    Add info icon to mobile
    /cc @uberbryn I hope I did this correctly? :sweat_smile:

--- mobile/components/Icon/index.js
+++ mobile/components/Icon/index.js
@@ -13,11 +13,12 @@ type Props = {
   glyph: GlyphTypes,
   size?: number,
   color?: (theme: Object) => string,
+  style?: Object,
   theme: Object,
 };
 
 // prettier-ignore
-export default withTheme(({ size = 64, color, glyph, theme, onPress }: Props) => {
+export default withTheme(({ size = 64, color, glyph, theme, onPress, style }: Props) => {
     const fill = color ? color(theme) : theme.text.alt;
 
     return (
@@ -27,7 +28,7 @@ export default withTheme(({ size = 64, color, glyph, theme, onPress }: Props) =>
           <TouchableOpacity onPress={onPress}>{children}</TouchableOpacity>
         )}
       >
-        <Svg width={size} height={size} viewBox={`0 0 32 32`}>
+        <Svg width={size} height={size} viewBox={`0 0 ${size} ${size}`} style={style}>
           <G>
             {getPaths(glyph, fill)}
           </G>
