77a5d9f1e1990875bece3df3d3b6f2e55d6b2927
Maximilian Stoiber <contact@mxstbr.com>
2018-06-21 09:48:45 +0200

    Remove duplicate function

--- mobile/components/Message/Reactions.js
+++ mobile/components/Message/Reactions.js
@@ -14,12 +14,11 @@ type Props = {
 const Reactions = ({ count, hasReacted, onPress, style }: Props) => {
   if (count === 0) return null;
 
-  const textColor = theme => (hasReacted ? theme.warn.default : theme.text.alt);
-  const iconColor = theme => (hasReacted ? theme.warn.default : theme.text.alt);
+  const color = theme => (hasReacted ? theme.warn.default : theme.text.alt);
   return (
     <Row alignItems="center" style={style}>
-      <Icon onPress={onPress} glyph="like-fill" size={16} color={iconColor} />
-      <Text type="caption1" color={textColor} style={{ marginLeft: 4 }}>
+      <Icon onPress={onPress} glyph="like-fill" size={16} color={color} />
+      <Text type="caption1" color={color} style={{ marginLeft: 4 }}>
         {count}
       </Text>
     </Row>
