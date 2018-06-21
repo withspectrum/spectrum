967443fbef3be2c64f0e4ee17afb7da261627689
Brian Lovin <briandlovin@gmail.com>
2018-06-20 18:02:20 -0700

    Properly return threads from mutations

--- api/mutations/thread/addReaction.js
+++ api/mutations/thread/addReaction.js
@@ -1,6 +1,7 @@
 // @flow
 import type { GraphQLContext } from '../../';
 import { addThreadReaction } from '../../models/reaction';
+import { getThreadById } from '../../models/thread';
 import { isAuthedResolver as requireAuth } from '../../utils/permissions';
 
 type Input = {
@@ -12,6 +13,8 @@ type Input = {
 
 export default requireAuth(
   async (_: any, args: Input, { user, loaders }: GraphQLContext) => {
-    return await addThreadReaction(args.input, user.id);
+    return await addThreadReaction(args.input, user.id).then(
+      async () => await getThreadById(args.input.threadId)
+    );
   }
 );
--- api/mutations/thread/removeReaction.js
+++ api/mutations/thread/removeReaction.js
@@ -1,6 +1,7 @@
 // @flow
 import type { GraphQLContext } from '../../';
 import { removeThreadReaction } from '../../models/reaction';
+import { getThreadById } from '../../models/thread';
 import { isAuthedResolver as requireAuth } from '../../utils/permissions';
 
 type Input = {
@@ -11,6 +12,8 @@ type Input = {
 
 export default requireAuth(
   async (_: any, args: Input, { user, loaders }: GraphQLContext) => {
-    return await removeThreadReaction(args.input.threadId, user.id);
+    return await removeThreadReaction(args.input.threadId, user.id).then(
+      async () => await getThreadById(args.input.threadId)
+    );
   }
 );
