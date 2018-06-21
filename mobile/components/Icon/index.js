ddadc4bd3df2396c88874b8267083f0464a0157a
Maximilian Stoiber <contact@mxstbr.com>
2018-06-21 09:48:24 +0200

    Properly type Icon component

--- mobile/components/Icon/index.js
+++ mobile/components/Icon/index.js
@@ -5,20 +5,22 @@ import ConditionalWrap from '../ConditionalWrap';
 import { Svg } from 'expo';
 const { G } = Svg;
 import { withTheme } from 'styled-components/native';
-import type { GlyphTypes } from './types';
 import getPaths from './glyphs';
+import type { ThemeType } from '../theme';
+import type { GlyphTypes } from './types';
 
 type Props = {
   onPress?: Function,
   glyph: GlyphTypes,
   size?: number,
-  color?: (theme: Object) => string,
+  color?: (theme: ThemeType) => string,
   style?: Object,
   theme: Object,
 };
 
-// prettier-ignore
-export default withTheme(({ size = 64, color, glyph, theme, onPress, style }: Props) => {
+class Icon extends React.Component<Props> {
+  render() {
+    const { size = 64, color, glyph, theme, onPress, style } = this.props;
     const fill = color ? color(theme) : theme.text.alt;
 
     return (
@@ -28,12 +30,12 @@ export default withTheme(({ size = 64, color, glyph, theme, onPress, style }: Pr
           <TouchableOpacity onPress={onPress}>{children}</TouchableOpacity>
         )}
       >
-        <Svg width={size} height={size} viewBox={`0 0 ${size} ${size}`} style={style}>
-          <G>
-            {getPaths(glyph, fill)}
-          </G>
+        <Svg width={size} height={size} viewBox={`0 0 32 32`} style={style}>
+          <G>{getPaths(glyph, fill)}</G>
         </Svg>
       </ConditionalWrap>
     );
   }
-);
+}
+
+export default withTheme(Icon);
