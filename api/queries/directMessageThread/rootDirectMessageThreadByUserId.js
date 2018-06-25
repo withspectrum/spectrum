ec81dc1a7672857dd68efb02afdf458fb6462f40
Brian Lovin <briandlovin@gmail.com>
2018-06-25 10:30:21 -0700

    Use requireauth wrapper

--- api/queries/directMessageThread/rootDirectMessageThreadByUserId.js
+++ api/queries/directMessageThread/rootDirectMessageThreadByUserId.js
@@ -1,17 +1,16 @@
 // @flow
 import type { GraphQLContext } from '../../';
 import { checkForExistingDMThread } from '../../models/directMessageThread';
+import { isAuthedResolver as requireAuth } from '../../utils/permissions';
 
 type Args = {
   userId: string,
 };
 
-export default async (_: any, args: Args, ctx: GraphQLContext) => {
+export default requireAuth(async (_: any, args: Args, ctx: GraphQLContext) => {
   // signed out users will never be able to view a dm thread
   const { user: currentUser, loaders } = ctx;
   const { userId } = args;
-
-  if (!currentUser || !currentUser.id) return null;
 
   const allMemberIds = [userId, currentUser.id];
   const existingThread = await checkForExistingDMThread(allMemberIds);
@@ -19,4 +18,4 @@ export default async (_: any, args: Args, ctx: GraphQLContext) => {
   if (!existingThread) return null;
 
   return loaders.directMessageThread.load(existingThread);
-};
+});
