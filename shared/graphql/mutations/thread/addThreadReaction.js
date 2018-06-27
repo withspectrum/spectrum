0edb2f6164e3c964bcdc294fa9e40788a31e23e6
Brian Lovin <briandlovin@gmail.com>
2018-06-27 09:56:51 -0700

    Add optimistic udpates for thread likes

--- shared/graphql/mutations/thread/addThreadReaction.js
+++ shared/graphql/mutations/thread/addThreadReaction.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import threadInfoFragment from '../../fragments/thread/threadInfo';
 import type { ThreadInfoType } from '../../fragments/thread/threadInfo';
+import { getThreadByIdQuery } from '../../queries/thread/getThread';
 
 export type AddThreadReactionType = {
   data: {
@@ -28,16 +29,52 @@ export const addThreadReactionQuery = gql`
 `;
 
 const addThreadReactionOptions = {
-  props: ({ mutate }) => ({
-    addThreadReaction: ({ input }) =>
-      mutate({
+  props: ({ ownProps, mutate }) => ({
+    addThreadReaction: ({ input }) => {
+      const fakeId = Math.round(Math.random() * -1000000);
+
+      return mutate({
         variables: {
           input: {
             ...input,
             type: 'like',
           },
         },
-      }),
+        optimisticResponse: {
+          __typename: 'Mutation',
+          addThreadReaction: {
+            id: fakeId,
+            threadId: ownProps.thread.id,
+            type: 'like',
+            __typename: 'ThreadReaction',
+          },
+        },
+        update: (store, { data: { addThreadReaction } }) => {
+          const data = store.readQuery({
+            query: getThreadByIdQuery,
+            variables: {
+              id: ownProps.thread.id,
+            },
+          });
+
+          // ignore the server response and only update the cache with the
+          // optimistic response
+          if (typeof addThreadReaction.id === 'string') return;
+
+          data.thread.reactions.count++;
+          data.thread.reactions.hasReacted = true;
+
+          // Write our data back to the cache.
+          store.writeQuery({
+            query: getThreadByIdQuery,
+            data,
+            variables: {
+              id: ownProps.thread.id,
+            },
+          });
+        },
+      });
+    },
   }),
 };
 
--- shared/graphql/mutations/thread/removeThreadReaction.js
+++ shared/graphql/mutations/thread/removeThreadReaction.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import threadInfoFragment from '../../fragments/thread/threadInfo';
 import type { ThreadInfoType } from '../../fragments/thread/threadInfo';
+import { getThreadByIdQuery } from '../../queries/thread/getThread';
 
 export type RemoveThreadReactionType = {
   data: {
@@ -28,13 +29,49 @@ export const removeThreadReactionQuery = gql`
 `;
 
 const removeThreadReactionOptions = {
-  props: ({ mutate }) => ({
-    removeThreadReaction: ({ input }) =>
-      mutate({
+  props: ({ ownProps, mutate }) => ({
+    removeThreadReaction: ({ input }) => {
+      const fakeId = Math.round(Math.random() * -1000000);
+
+      return mutate({
         variables: {
           input,
         },
-      }),
+        optimisticResponse: {
+          __typename: 'Mutation',
+          removeThreadReaction: {
+            id: fakeId,
+            threadId: ownProps.thread.id,
+            type: 'like',
+            __typename: 'ThreadReaction',
+          },
+        },
+        update: (store, { data: { removeThreadReaction } }) => {
+          const data = store.readQuery({
+            query: getThreadByIdQuery,
+            variables: {
+              id: ownProps.thread.id,
+            },
+          });
+
+          // ignore the server response and only update the cache with the
+          // optimistic response
+          if (typeof removeThreadReaction.id === 'string') return;
+
+          data.thread.reactions.count--;
+          data.thread.reactions.hasReacted = false;
+
+          // Write our data back to the cache.
+          store.writeQuery({
+            query: getThreadByIdQuery,
+            data,
+            variables: {
+              id: ownProps.thread.id,
+            },
+          });
+        },
+      });
+    },
   }),
 };
 
