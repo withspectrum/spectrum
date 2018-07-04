72f5a3f86833595725322c5e58a5801457de9b57
Brian Lovin <briandlovin@gmail.com>
2018-07-04 06:31:29 -0600

    Revert badges styling

--- src/components/badges/style.js
+++ src/components/badges/style.js
@@ -1,15 +1,15 @@
 // @flow
 import styled from 'styled-components';
-import { Tooltip } from '../globals';
+import { Tooltip, Gradient } from '../globals';
 
 export const Span = styled.span`
   color: ${({ theme }) => theme.text.reverse};
   background-color: ${props => props.theme.text.alt};
   text-transform: uppercase;
-  padding: 2px 6px 1px;
+  padding: 2px 6px;
   margin-left: 4px;
   font-size: 9px;
-  font-weight: 900;
+  font-weight: 800;
   border-radius: 4px;
   ${props => (props.tipText ? Tooltip(props) : '')};
   letter-spacing: 0.6px;
@@ -19,6 +19,8 @@ export const Span = styled.span`
 
 export const ProBadge = styled(Span)`
   background-color: ${props => props.theme.special.default};
+  background-image: ${props =>
+    Gradient(props.theme.special.alt, props.theme.special.default)};
   cursor: pointer;
 
   &:hover {
@@ -28,10 +30,14 @@ export const ProBadge = styled(Span)`
 
 export const TeamBadge = styled(Span)`
   background-color: ${props => props.theme.success.default};
+  background-image: ${props =>
+    Gradient(props.theme.success.alt, props.theme.success.default)};
 `;
 
 export const BlockedBadge = styled(Span)`
   background-color: ${props => props.theme.warn.alt};
+  background-image: ${props =>
+    Gradient(props.theme.warn.alt, props.theme.warn.default)};
   cursor: pointer;
 
   &:hover {
@@ -41,6 +47,8 @@ export const BlockedBadge = styled(Span)`
 
 export const PendingBadge = styled(Span)`
   background-color: ${props => props.theme.special.alt};
+  background-image: ${props =>
+    Gradient(props.theme.special.alt, props.theme.special.default)};
   cursor: pointer;
 
   &:hover {
@@ -49,7 +57,9 @@ export const PendingBadge = styled(Span)`
 `;
 
 export const DefaultPaymentMethodBadge = styled(Span)`
-  background-color: ${props => props.theme.space.alt};
+  background-color: ${props => props.theme.space.default};
+  background-image: ${props =>
+    Gradient(props.theme.space.default, props.theme.space.default)};
   cursor: pointer;
   margin-left: 8px;
 
