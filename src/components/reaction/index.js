d6b1f75a51d5505caa727feef92378ee040184b2
Brian Lovin <briandlovin@gmail.com>
2018-07-03 21:07:58 -0600

    Refactor reactions to not need local state, use render prop

--- src/components/reaction/index.js
+++ src/components/reaction/index.js
@@ -1,8 +1,6 @@
 // @flow
 import * as React from 'react';
-import Icon from '../icons';
 import { addToastWithTimeout } from '../../actions/toasts';
-import { ReactionWrapper } from '../message/style';
 import type { GetMessageType } from 'shared/graphql/queries/message/getMessage';
 import type { Dispatch } from 'redux';
 
@@ -12,25 +10,10 @@ type Props = {
   currentUser?: Object,
   me: boolean,
   message: GetMessageType,
+  render: Function,
 };
 
-type State = {
-  count: number,
-  hasReacted: boolean,
-};
-
-class Reaction extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      count: props.message.reactions.count,
-      hasReacted: props.message.reactions.hasReacted,
-    };
-  }
-
-  doNothing = () => {};
-
+class Reaction extends React.Component<Props> {
   triggerMutation = () => {
     const { toggleReaction, message, dispatch, currentUser } = this.props;
 
@@ -40,62 +23,17 @@ class Reaction extends React.Component<Props, State> {
       );
     }
 
-    const hasReacted = this.state.hasReacted;
-    const count = this.state.count;
-
-    this.setState({
-      hasReacted: !hasReacted,
-      count: hasReacted ? count - 1 : count + 1,
-    });
-
-    toggleReaction({
+    return toggleReaction({
       messageId: message.id,
       type: 'like',
-    })
-      // after the mutation occurs, it will either return an error or the new
-      // thread that was published
-      .then(({ data }) => {
-        // can do something with the returned reaction here
-      })
-      .catch(error => {
-        // TODO add some kind of dispatch here to show an error to the user
-        dispatch(
-          addToastWithTimeout(
-            'error',
-            "Couldn't quite save that reaction, try again?"
-          )
-        );
-
-        this.setState({
-          hasReacted,
-          count,
-        });
-      });
+    });
   };
 
   render() {
-    const { me, currentUser } = this.props;
-    const { hasReacted, count } = this.state;
+    const { me, message: { reactions: { hasReacted, count } } } = this.props;
+    const mutation = me ? () => {} : this.triggerMutation;
 
-    return (
-      <ReactionWrapper
-        hasCount={count}
-        hasReacted={hasReacted}
-        me={me}
-        hide={(me || !currentUser) && count === 0}
-        onClick={me ? this.doNothing : this.triggerMutation}
-        dummy={false}
-      >
-        <Icon
-          glyph="like-fill"
-          size={16}
-          color={'text.reverse'}
-          tipText={me ? 'Likes' : hasReacted ? 'Unlike' : 'Like'}
-          tipLocation={me ? 'top-left' : 'top-right'}
-        />
-        <span>{count}</span>
-      </ReactionWrapper>
-    );
+    return this.props.render({ me, hasReacted, count, mutation });
   }
 }
 
