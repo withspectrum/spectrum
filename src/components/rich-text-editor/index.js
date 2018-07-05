88ebc3efabdee9934e2ac7537d489a7578ddb04e
Brian Lovin <briandlovin@gmail.com>
2018-07-05 17:27:03 -0600

    Add thread loading state back

--- src/components/rich-text-editor/index.js
+++ src/components/rich-text-editor/index.js
@@ -1,10 +1,11 @@
 // @flow
+import React from 'react';
 import Loadable from 'react-loadable';
-
+import { Loading } from '../loading';
 /* prettier-ignore */
 const RichTextEditor = Loadable({
   loader: () => import('./container'/* webpackChunkName: "RichTextEditor" */),
-  loading: ({ isLoading }) => isLoading && null,
+  loading: ({ isLoading }) => isLoading && <Loading />,
 });
 
 export default RichTextEditor;
