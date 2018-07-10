63cddfd2a3f436bce2b56883b6e4cfd13c0022b4
Brian Lovin <briandlovin@gmail.com>
2018-07-10 13:55:39 -0600

    Add hover profile to message byline, make timstamp clickable

--- src/components/message/authorByline.js
+++ src/components/message/authorByline.js
@@ -4,28 +4,32 @@ import { convertTimestampToTime } from 'shared/time-formatting';
 import Link from 'src/components/link';
 import Badge from '../badges';
 import { Byline, Name, Username, GutterTimestamp } from './style';
+import { UserHoverProfile } from 'src/components/hoverProfile';
 
 type Props = {
   user: Object,
   timestamp: string,
   roles?: Array<string>,
+  messageUrl: string,
 };
 
 export default (props: Props) => {
-  const { user, roles, timestamp } = props;
+  const { user, roles, timestamp, messageUrl } = props;
   return (
     <Byline>
       {user.username ? (
-        <Link to={`/users/${user.username}`}>
-          <Name>{user.name}</Name>{' '}
-          <Username>{user.username && `@${user.username}`}</Username>
-        </Link>
+        <UserHoverProfile username={user.username}>
+          <Link to={`/users/${user.username}`}>
+            <Name>{user.name}</Name>{' '}
+            <Username>{user.username && `@${user.username}`}</Username>
+          </Link>
+        </UserHoverProfile>
       ) : (
         <Name>{user.name}</Name>
       )}
       {roles && roles.map((role, index) => <Badge type={role} key={index} />)}
       {user.isPro && <Badge type="pro" />}
-      <GutterTimestamp>
+      <GutterTimestamp to={messageUrl}>
         {convertTimestampToTime(new Date(timestamp))}
       </GutterTimestamp>
     </Byline>
