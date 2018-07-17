edfc4d0daca7562ccc81719b525cf822357bdc1a
Brian Lovin <briandlovin@gmail.com>
2018-07-16 18:05:20 -0700

    Attempt to fix missing request token in session bug

--- api/routes/auth/logout.js
+++ api/routes/auth/logout.js
@@ -1,6 +1,4 @@
 import { Router } from 'express';
-const debug = require('debug')('api:routes:auth:logout');
-import { destroySession } from '../../models/session';
 
 const IS_PROD = process.env.NODE_ENV === 'production';
 const HOME = IS_PROD ? '/' : 'http://localhost:3000/';
