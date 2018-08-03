4f25aaaf5ba2302e8f9ab062486d0fd4d52fd038
Maximilian Stoiber <contact@mxstbr.com>
2018-08-03 15:02:37 +0200

    Potentially fix CORS configuration
    Closes #3698

--- shared/middlewares/cors.js
+++ shared/middlewares/cors.js
@@ -4,11 +4,7 @@ import cors from 'cors';
 export default cors({
   origin:
     process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
-      ? [
-          'https://spectrum.chat',
-          /spectrum-(\w|-)+\.now\.sh/g,
-          /spectrum\.chat$/,
-        ]
+      ? ['https://spectrum.chat', /\.spectrum\.chat$/]
       : [/localhost/],
   credentials: true,
 });
