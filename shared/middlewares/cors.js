4dc3cd15d63476f3ed18a10ab5d2beb59192830a
Maximilian Stoiber <contact@mxstbr.com>
2018-08-03 15:14:26 +0200

    Also allow NOW_URL to request

--- shared/middlewares/cors.js
+++ shared/middlewares/cors.js
@@ -4,7 +4,11 @@ import cors from 'cors';
 export default cors({
   origin:
     process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
-      ? ['https://spectrum.chat', /\.spectrum\.chat$/]
+      ? [
+          'https://spectrum.chat',
+          /\.spectrum\.chat$/,
+          process.env.NOW_URL,
+        ].filter(Boolean)
       : [/localhost/],
   credentials: true,
 });
