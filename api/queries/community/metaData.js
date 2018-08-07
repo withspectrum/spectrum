dba202d29c479cecb96add854c6b6218c2fb4c35
Maximilian Stoiber <contact@mxstbr.com>
2018-08-07 07:26:32 +0200

    Implement Community.metaData.onlineMembers
    Counts the number of members that are `isOnline` or were active in the
    last 24 hours.

--- api/queries/community/metaData.js
+++ api/queries/community/metaData.js
@@ -7,20 +7,22 @@ export default async (root: DBCommunity, _: any, ctx: GraphQLContext) => {
   const { user, loaders } = ctx;
   const { id } = root;
 
-  if (!await canViewCommunity(user, id, loaders)) {
+  if (!(await canViewCommunity(user, id, loaders))) {
     return {
       channels: 0,
       members: 0,
     };
   }
 
-  const [channelCount, memberCount] = await Promise.all([
+  const [channelCount, memberCount, onlineMemberCount] = await Promise.all([
     loaders.communityChannelCount.load(id),
     loaders.communityMemberCount.load(id),
+    loaders.communityOnlineMemberCount.load(id),
   ]);
 
   return {
     channels: channelCount ? channelCount.reduction : 0,
     members: memberCount ? memberCount.reduction : 0,
+    onlineMembers: onlineMemberCount ? onlineMemberCount.reduction : 0,
   };
 };
