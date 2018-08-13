fb783734610db6be3940eff99b011ca0838e4907
Maximilian Stoiber <contact@mxstbr.com>
2018-08-13 11:08:04 +0200

    Add support custom slugs in thread URLs

--- src/routes.js
+++ src/routes.js
@@ -304,7 +304,13 @@ class Routes extends React.Component<Props> {
                   component={CommunityLoginView}
                 />
                 <Route
-                  path="/:communitySlug/:channelSlug/:threadId"
+                  // NOTE(@mxstbr): This custom path regexp matches threadId correctly in all cases, no matter if we prepend it with a custom slug or not.
+                  // Imagine our threadId is "id-123-id" (similar in shape to an actual UUID)
+                  // - /id-123-id => id-123-id, easy start that works
+                  // - /some-custom-slug~id-123-id => id-123-id, custom slug also works
+                  // - /~id-123-id => id-123-id => id-123-id, empty custom slug also works
+                  // - /some~custom~slug~id-123-id => id-123-id, custom slug with delimiter char in it (~) also works! :tada:
+                  path="/:communitySlug/:channelSlug/(.*~)?:threadId"
                   component={Thread}
                 />
                 <Route
--- src/views/thread/redirect-old-route.js
+++ src/views/thread/redirect-old-route.js
@@ -2,6 +2,7 @@
 // Redirect the old thread route (/thread/:threadId) to the new one (/:community/:channel/:threadId)
 import React from 'react';
 import { Redirect } from 'react-router';
+import slugg from 'slugg';
 import { getThreadByMatch } from 'shared/graphql/queries/thread/getThread';
 
 export default getThreadByMatch(props => {
@@ -9,7 +10,9 @@ export default getThreadByMatch(props => {
     const { thread } = props.data;
     return (
       <Redirect
-        to={`/${thread.community.slug}/${thread.channel.slug}/${thread.id}`}
+        to={`/${thread.community.slug}/${thread.channel.slug}/${slugg(
+          thread.content.title
+        )}~${thread.id}`}
       />
     );
   }
