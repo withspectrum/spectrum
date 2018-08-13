12e1faecbbe3c87c9a41da2bc592f5c40a5bfe32
Maximilian Stoiber <contact@mxstbr.com>
2018-08-13 08:56:29 +0200

    Fix API logging undefined over and over
    It turns out I'm a dummy and did Promise.reject(undefined) when a user
    wasn't authenticated :facepalm:
    
    This fixes #3746

--- api/utils/session-store.js
+++ api/utils/session-store.js
@@ -7,12 +7,15 @@ const ONE_DAY = 86400000;
 /**
  * Get the sessions' users' ID of a req manually, needed for websocket authentication
  */
-export const getUserIdFromReq = (req: any): Promise<string> =>
+export const getUserIdFromReq = (req: any): Promise<?string> =>
   new Promise((res, rej) => {
     session(req, {}, err => {
-      if (err) return rej(err);
-      if (!req.session || !req.session.passport || !req.session.passport.user)
-        return rej();
+      if (err) {
+        return rej(err);
+      }
+      if (!req.session || !req.session.passport || !req.session.passport.user) {
+        return res(null);
+      }
 
       return res(req.session.passport.user);
     });
