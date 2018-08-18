2578572fc9036d09f8c426594654a5c28bec6a93
Maximilian Stoiber <contact@mxstbr.com>
2018-08-18 16:02:38 +0200

    Aggressively decay based on lastActive

--- mercury/queues/calculateThreadScore.js
+++ mercury/queues/calculateThreadScore.js
@@ -1,4 +1,5 @@
 // @flow
+// Process jobs to calculate a threads score based on participant count, message count, like count and age.
 import {
   getThread,
   getParticipantCount,
@@ -13,8 +14,12 @@ const weigh = {
   participants: p => p * 10,
   messages: m => m * 1,
   reactions: r => r * 0.2,
-  age: days => Math.sqrt(days) * -5,
-  lastActive: days => Math.sqrt(days) * -10,
+  // NOTE: We Math.sqrt the days to make the age decay less drastic since we don't really care how old a thread is if it's really active right now
+  // It goes from -2.5 on the first day to -25 on the 100th day
+  age: days => Math.sqrt(days) * -2.5,
+  // NOTE: We use Math.log to aggressively decay based on days sine the last activity initially, but then slow down over time
+  // It goes from -35 on the first day to -230 on the 100th day since the last activity
+  lastActive: days => Math.log(days + 1) * -50,
 };
 // NOTE: We use Math.floor to make sure it goes from 0 to 1 once it crosses 24h, not when it crosses 12h
 const getAgeInDays = (date: Date) =>
