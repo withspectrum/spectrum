7930b11d2967cdc253af51f3b17e48587128231f
Dan Weaver <danweaver06@gmail.com>
2018-08-20 11:05:39 -0400

    removed unneccesary word boundary from mention regex

--- shared/regexps.js
+++ shared/regexps.js
@@ -1,5 +1,5 @@
 // @flow
 
-module.exports.MENTIONS = /\/?\B@[a-z0-9._-]+[^.\s]\b/gi;
+module.exports.MENTIONS = /\/?\B@[a-z0-9._-]+[^.\s]/gi;
 module.exports.URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)/gi;
 module.exports.RELATIVE_URL = /^\/([^\/].*|$)/g;
