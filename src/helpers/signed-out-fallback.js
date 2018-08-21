a8de53eefb3d409096f147ce906f59ab7bb81c7b
Maximilian Stoiber <contact@mxstbr.com>
2018-08-21 08:35:19 +0200

    Maybe fix perpetually being signed out

--- src/helpers/signed-out-fallback.js
+++ src/helpers/signed-out-fallback.js
@@ -1,26 +1,38 @@
+// @flow
 // Render a component depending on a users authentication status
 import React from 'react';
 import { connect } from 'react-redux';
 import queryString from 'query-string';
+import AuthViewHandler from 'src/views/authViewHandler';
 
 // This is the component that determines at render time what to do
 const Switch = props => {
   const { Component, FallbackComponent, currentUser, ...rest } = props;
-  const { authed } = queryString.parse(props.location.search);
-  if ((!currentUser && !authed) || !Component) {
-    return <FallbackComponent {...rest} />;
-  } else {
-    return <Component {...rest} />;
-  }
+  return (
+    <AuthViewHandler>
+      {authed => {
+        if (!authed) {
+          return <FallbackComponent {...rest} />;
+        } else {
+          return <Component {...rest} />;
+        }
+      }}
+    </AuthViewHandler>
+  );
 };
 
 // Connect that component to the Redux state
-const ConnectedSwitch = connect(state => ({
-  currentUser: state.users.currentUser,
-}))(Switch);
+const ConnectedSwitch = connect(
+  (state: *): * => ({
+    currentUser: state.users.currentUser,
+  })
+)(Switch);
 
-const signedOutFallback = (Component, FallbackComponent) => {
-  return props => (
+const signedOutFallback = (
+  Component: React$ComponentType<*>,
+  FallbackComponent: React$ComponentType<*>
+) => {
+  return (props: *) => (
     <ConnectedSwitch
       {...props}
       FallbackComponent={FallbackComponent}
