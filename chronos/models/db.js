4687a11eac388f969536b2313b1cdfbb6adf8a5c
Maximilian Stoiber <contact@mxstbr.com>
2018-08-23 11:18:42 +0200

    Mover workers to compose

--- chronos/models/db.js
+++ chronos/models/db.js
@@ -1,19 +1,27 @@
+// @flow
 /**
  * Database setup is done here
  */
+const fs = require('fs');
+const path = require('path');
 const IS_PROD = !process.env.FORCE_DEV && process.env.NODE_ENV === 'production';
 
 const DEFAULT_CONFIG = {
-  db: 'spectrum',
-  max: 100, // Maximum number of connections, default is 1000
-  buffer: 10, // Minimum number of connections open at any given moment, default is 50
+  // Connect to the test database when, well, testing
+  db: !process.env.TEST_DB ? 'spectrum' : 'testing',
+  max: 500, // Maximum number of connections, default is 1000
+  buffer: 5, // Minimum number of connections open at any given moment, default is 50
   timeoutGb: 60 * 1000, // How long should an unused connection stick around, default is an hour, this is a minute
 };
 
 const PRODUCTION_CONFIG = {
-  password: process.env.AWS_RETHINKDB_PASSWORD,
-  host: process.env.AWS_RETHINKDB_URL,
-  port: process.env.AWS_RETHINKDB_PORT,
+  password: process.env.COMPOSE_US_WEST_2_RETHINKDB_PASSWORD,
+  user: 'admin',
+  host: process.env.COMPOSE_US_WEST_2_RETHINKDB_URL,
+  port: process.env.COMPOSE_US_WEST_2_RETHINKDB_PORT,
+  ssl: {
+    ca: fs.readFileSync(path.join(process.cwd(), 'cacert')),
+  },
 };
 
 const config = IS_PROD
