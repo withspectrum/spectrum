e0f30adc3e544cc6f2d26fcad502a88e90fa0d7b
Maximilian Stoiber <contact@mxstbr.com>
2018-09-04 15:30:30 +0200

    Cache community.metaData in Redis for an hour

--- api/queries/community/metaData.js
+++ api/queries/community/metaData.js
@@ -2,6 +2,7 @@
 import type { DBCommunity } from 'shared/types';
 import type { GraphQLContext } from '../../';
 import { canViewCommunity } from '../../utils/permissions';
+import cache from 'shared/cache/redis';
 
 export default async (root: DBCommunity, _: any, ctx: GraphQLContext) => {
   const { user, loaders } = ctx;
@@ -15,15 +16,46 @@ export default async (root: DBCommunity, _: any, ctx: GraphQLContext) => {
     };
   }
 
+  const [
+    cachedChannelCount,
+    cachedMemberCount,
+    cachedOnlineMemberCount,
+  ] = await Promise.all([
+    cache.get(`community:${id}:channelCount`),
+    cache.get(`community:${id}:memberCount`),
+    cache.get(`community:${id}:onlineMemberCount`),
+  ]);
+
   const [channelCount, memberCount, onlineMemberCount] = await Promise.all([
-    loaders.communityChannelCount.load(id),
-    loaders.communityMemberCount.load(id),
-    loaders.communityOnlineMemberCount.load(id),
+    cachedChannelCount ||
+      loaders.communityChannelCount
+        .load(id)
+        .then(res => (res && res.reduction) || 0),
+    cachedMemberCount ||
+      loaders.communityMemberCount
+        .load(id)
+        .then(res => (res && res.reduction) || 0),
+    cachedOnlineMemberCount ||
+      loaders.communityOnlineMemberCount
+        .load(id)
+        .then(res => (res && res.reduction) || 0),
+  ]);
+
+  // Cache the fields for an hour
+  await Promise.all([
+    cache.set(`community:${id}:channelCount`, channelCount, 'ex', 3600),
+    cache.set(`community:${id}:memberCount`, memberCount, 'ex', 3600),
+    cache.set(
+      `community:${id}:onlineMemberCount`,
+      onlineMemberCount,
+      'ex',
+      3600
+    ),
   ]);
 
   return {
-    channels: channelCount ? channelCount.reduction : 0,
-    members: memberCount ? memberCount.reduction : 0,
-    onlineMembers: onlineMemberCount ? onlineMemberCount.reduction : 0,
+    channels: channelCount,
+    members: memberCount,
+    onlineMembers: onlineMemberCount,
   };
 };
