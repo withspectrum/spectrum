abb39b885fcc9bd0bcfc134e41509e07d5d6d0d7
Brian Lovin <briandlovin@gmail.com>
2018-09-28 11:38:22 +0800

    Fix flow

--- src/components/hoverProfile/userProfile.js
+++ src/components/hoverProfile/userProfile.js
@@ -102,8 +102,8 @@ class HoverProfile extends Component<ProfileProps> {
 }
 
 const map = state => ({ currentUser: state.users.currentUser });
-//$FlowFixMe
 export default compose(
+  //$FlowFixMe
   connect(map),
   withRouter
 )(HoverProfile);
