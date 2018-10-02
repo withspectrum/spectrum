eac0fca431a64c80352e9daa54984becec8eccf6
Maximilian Stoiber <contact@mxstbr.com>
2018-10-02 14:26:02 +0200

    Report fallback reactionCount missing to Sentry

--- api/queries/thread/reactions.js
+++ api/queries/thread/reactions.js
@@ -7,15 +7,27 @@ export default async (
   { id, reactionCount }: DBThread,
   _: any,
   { user, loaders }: GraphQLContext
-) => ({
-  count:
-    typeof reactionCount === 'number'
-      ? reactionCount
-      : await loaders.threadReaction
-          .load(id)
-          .then(
-            res =>
-              res && Array.isArray(res.reduction) ? res.reduction.length : 0
-          ),
-  hasReacted: user ? await hasReactedToThread(user.id, id) : false,
-});
+) => {
+  const hasReacted = user ? await hasReactedToThread(user.id, id) : false;
+  if (typeof reactionCount === 'number') {
+    return {
+      count: reactionCount,
+      hasReacted,
+    };
+  }
+
+  // Fallback if there's no denormalized reactionCount, also report to Sentry
+  Raven.captureException(
+    new Error(
+      `Thread with ID "${id}" does not have denormalized reactionCount.`
+    )
+  );
+  return {
+    count: await loaders.threadReaction
+      .load(id)
+      .then(
+        res => (res && Array.isArray(res.reduction) ? res.reduction.length : 0)
+      ),
+    hasReacted,
+  };
+};
