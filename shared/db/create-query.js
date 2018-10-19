5167b22acb7e8e3de2a4d5e74a9bc4e284ad5e29
Maximilian Stoiber <contact@mxstbr.com>
2018-10-19 14:30:48 +0200

    Disable caching

--- shared/db/create-query.js
+++ shared/db/create-query.js
@@ -24,15 +24,15 @@ import { READ_RUN_ERROR, WRITE_RUN_ERROR } from './constants';
 let TOTAL_QUERIES = 0;
 let CACHED_RESULTS = 0;
 
-if (debug.enabled) {
-  debug('Logging of cache hit rate enabled! (every 30s)');
-  setInterval(() => {
-    debug(
-      `Cache hit rate: ${(CACHED_RESULTS / TOTAL_QUERIES) *
-        100}% (${CACHED_RESULTS} cached of ${TOTAL_QUERIES} total)`
-    );
-  }, 30000);
-}
+// if (debug.enabled) {
+//   debug('Logging of cache hit rate enabled! (every 30s)');
+//   setInterval(() => {
+//     debug(
+//       `Cache hit rate: ${(CACHED_RESULTS / TOTAL_QUERIES) *
+//         100}% (${CACHED_RESULTS} cached of ${TOTAL_QUERIES} total)`
+//     );
+//   }, 30000);
+// }
 
 type RethinkDBQuery<O> = {
   toString: Function,
@@ -58,19 +58,19 @@ export const createReadQuery = <I: Array<*>, O: *>(
     TOTAL_QUERIES++;
     if (typeof input.query.run !== 'function') throw new Error(READ_RUN_ERROR);
 
-    const queryString = input.query.toString();
-    const cached = await queryCache.get(queryString);
-    if (cached) {
-      CACHED_RESULTS++;
-      return cached;
-    }
+    // const queryString = input.query.toString();
+    // const cached = await queryCache.get(queryString);
+    // if (cached) {
+    //   CACHED_RESULTS++;
+    //   return cached;
+    // }
 
     const result = await input.query
       .run()
       .then(input.process ? input.process : res => res);
 
-    const tags = input.tags(result).filter(Boolean);
-    await queryCache.set(queryString, result, tags);
+    // const tags = input.tags(result).filter(Boolean);
+    // await queryCache.set(queryString, result, tags);
     return result;
   };
 };
@@ -83,6 +83,8 @@ type CreateWriteQueryInput<I, O> = $Exact<{
 export const createWriteQuery = <I: Array<*>, O: *>(
   callback: (...args: I) => CreateWriteQueryInput<I, O>
 ) => {
+  return callback;
+  /*
   return async (...args: I) => {
     const input = callback(...args);
     const result = await input.query;
@@ -91,5 +93,5 @@ export const createWriteQuery = <I: Array<*>, O: *>(
     const tags = input.invalidateTags(result).filter(Boolean);
     await queryCache.invalidate(...tags);
     return result;
-  };
+  };*/
 };
