33b852dc66a470efec074957911026cd87b92cf0
Brian Lovin <briandlovin@gmail.com>
2018-10-28 22:38:39 -0700

    Add thread body resolver to sign image urls inline

--- api/queries/thread/content.js
+++ api/queries/thread/content.js
@@ -1,25 +1,10 @@
 // @flow
-import type { GraphQLContext } from '../../';
 import type { DBThread } from 'shared/types';
+import body from './content/body';
 
-export default ({ content }: DBThread, _: any, ctx: GraphQLContext) => {
-  const defaultDraftState = JSON.stringify({
-    blocks: [
-      {
-        key: 'foo',
-        text: '',
-        type: 'unstyled',
-        depth: 0,
-        inlineStyleRanges: [],
-        entityRanges: [],
-        data: {},
-      },
-    ],
-    entityMap: {},
-  });
-
+export default (thread: DBThread) => {
   return {
-    title: content.title,
-    body: content.body ? content.body : defaultDraftState,
+    title: thread.content.title,
+    body: body(thread),
   };
 };
