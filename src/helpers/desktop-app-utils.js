4c1ae98048604e38a4cbbc63b983e30b2278ceaa
Brian Lovin <briandlovin@gmail.com>
2018-11-01 16:52:09 -0700

    Add desktop app utils for client

--- src/helpers/desktop-app-utils.js
+++ src/helpers/desktop-app-utils.js
@@ -3,7 +3,18 @@
 // this is necessary because our e2e tests run in Electron (via Cypress), so a simple
 // isElectron() check also fires when that's the case
 import isElectron from 'is-electron';
+import { getItemFromStorage, storeItem } from 'src/helpers/localStorage';
 
 export const isDesktopApp = () => {
   return isElectron() && !window.Cypress && window.interop;
+};
+
+const DESKTOP_UPSELL_LS_KEY = 'dismissed-desktop-app-upsell';
+
+export const hasDismissedDesktopAppUpsell = () => {
+  return getItemFromStorage(DESKTOP_UPSELL_LS_KEY);
+};
+
+export const dismissDesktopAppUpsell = () => {
+  return storeItem(DESKTOP_UPSELL_LS_KEY, true);
 };
