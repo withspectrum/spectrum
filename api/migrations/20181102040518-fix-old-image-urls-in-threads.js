2dcabcb7cdcf6f003d3936b3015b43740d8fe742
Brian Lovin <briandlovin@gmail.com>
2018-11-01 22:01:57 -0700

    Backfill migrations to fix most broken images

---
+++ api/migrations/20181102040518-fix-old-image-urls-in-threads.js
@@ -0,0 +1,70 @@
+exports.up = async function(r, conn) {
+  const threads = await r
+    .db('spectrum')
+    .table('threads')
+    .filter(row => row('createdAt').lt(r.epochTime(1540929600)))
+    .filter(row => row('content')('body').match('spectrum.imgix.net'))
+    .filter(row => row('content')('body').match('%20'))
+    .filter(row => row.hasFields('deletedAt').not())
+    .map(row => ({ id: row('id'), body: row('content')('body') }))
+    .run(conn)
+    .then(cursor => cursor.toArray());
+
+  const threadPromises = threads
+    .filter(o => o.id === 'ff1aa2c0-5c0d-4942-a5a5-4370ffe2656d')
+    .map(async obj => {
+      const newBody = JSON.parse(obj.body);
+
+      if (newBody) {
+        const imageKeys = Object.keys(newBody.entityMap).filter(
+          key => newBody.entityMap[key].type.toLowerCase() === 'image'
+        );
+
+        const LEGACY_PREFIX = 'https://spectrum.imgix.net/';
+        const hasLegacyPrefix = url => url.startsWith(LEGACY_PREFIX, 0);
+        const stripLegacyPrefix = url => url.replace(LEGACY_PREFIX, '');
+
+        const processImageUrl = str => {
+          if (str.indexOf('https://spectrum.imgix.net') < 0) {
+            return str;
+          }
+          if (str.indexOf('%20') < 0) {
+            return str;
+          }
+
+          const split = str.split('?');
+          const imagePath = split[0];
+
+          const decoded = decodeURIComponent(imagePath);
+
+          const processed = hasLegacyPrefix(decoded)
+            ? stripLegacyPrefix(decoded)
+            : decoded;
+
+          return processed;
+        };
+
+        imageKeys.forEach((key, index) => {
+          if (!newBody.entityMap[key]) {
+            return;
+          }
+
+          const { src } = newBody.entityMap[key].data;
+          newBody.entityMap[key].data.src = processImageUrl(src);
+        });
+      }
+
+      return await r
+        .db('spectrum')
+        .table('threads')
+        .get(obj.id)
+        .update({ content: { body: JSON.stringify(newBody) } })
+        .run(conn);
+    });
+
+  return await Promise.all(threadPromises);
+};
+
+exports.down = function(r, conn) {
+  return Promise.resolve();
+};
