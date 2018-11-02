125164494b3824cf45e05ff29aa870b5304b2d3b
Brian Lovin <briandlovin@gmail.com>
2018-11-02 09:14:48 -0700

    Change vulcan to create a worker

--- vulcan/index.js
+++ vulcan/index.js
@@ -1,84 +1,23 @@
 // @flow
 const debug = require('debug')('vulcan');
-const PORT = process.env.PORT || 3007;
-import Raven from 'shared/raven';
-import {
-  newThread,
-  deletedThread,
-  movedThread,
-  editedThread,
-} from './models/thread';
-import {
-  newCommunity,
-  deletedCommunity,
-  editedCommunity,
-} from './models/community';
-import { newMessage, deletedMessage } from './models/message';
-import {
-  newUser,
-  deletedUser,
-  editedUser,
-  bannedUser,
-  unbannedUser,
-} from './models/user';
-import createServer from './server';
+const createWorker = require('shared/bull/create-worker');
+import searchEvent from './queues';
+import { SEARCH_INDEXING_EVENT } from './queues/constants';
 
-debug('\n✉️ Vulcan, the search worker, is starting...');
+const PORT = process.env.PORT || 3005;
+
+debug('\n✉️ Vulcan, the search indexing, is starting...');
 debug('Logging with debug enabled!');
-debug('');
 
-debug(
-  `🗄 Vulcan open for business ${(process.env.NODE_ENV === 'production' &&
-    // $FlowIssue
-    `at ${process.env.COMPOSE_REDIS_URL}:${process.env.COMPOSE_REDIS_PORT}`) ||
-    'locally'}`
-);
+const server = createWorker({
+  [SEARCH_INDEXING_EVENT]: searchEvent,
+});
 
-newThread();
-deletedThread();
-movedThread();
-editedThread();
-
-newCommunity();
-deletedCommunity();
-editedCommunity();
-
-newUser();
-deletedUser();
-bannedUser();
-unbannedUser();
-editedUser();
-
-newMessage();
-deletedMessage();
-
-const server = createServer();
+// $FlowIssue
 server.listen(PORT, 'localhost', () => {
   debug(
     `💉 Healthcheck server running at ${server.address().address}:${
       server.address().port
     }`
   );
-});
-
-process.on('unhandledRejection', async err => {
-  console.error('Unhandled rejection', err);
-  try {
-    await new Promise(resolve => Raven.captureException(err, resolve));
-  } catch (err) {
-    console.error('Raven error', err);
-  } finally {
-    process.exit(1);
-  }
-});
-
-process.on('uncaughtException', async err => {
-  console.error('Uncaught exception', err);
-  try {
-    await new Promise(resolve => Raven.captureException(err, resolve));
-  } catch (err) {
-    console.error('Raven error', err);
-  } finally {
-    process.exit(1);
-  }
 });
