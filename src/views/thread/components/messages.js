c4b364c06f7c588c11cf68162b9024ed666d63e6
Brian Lovin <briandlovin@gmail.com>
2018-11-06 09:51:49 -0800

    Fix flashing server error when loading messages

--- src/views/thread/components/messages.js
+++ src/views/thread/components/messages.js
@@ -48,6 +48,7 @@ type Props = {
   data: { thread: GetThreadMessageConnectionType },
   thread: GetThreadType,
   currentUser: ?Object,
+  hasError: boolean,
 };
 
 class MessagesWithData extends React.Component<Props, State> {
@@ -221,6 +222,7 @@ class MessagesWithData extends React.Component<Props, State> {
       lastSeen,
       thread,
       currentUser,
+      hasError,
     } = this.props;
 
     const hasMessagesToLoad = thread.messageCount > 0;
@@ -324,7 +326,7 @@ class MessagesWithData extends React.Component<Props, State> {
       );
     }
 
-    if (!isLoading && !messagesExist) {
+    if ((isLoading && !hasMessagesToLoad) || (!isLoading && !messagesExist)) {
       if (isLocked || !this.props.data.thread) return null;
 
       return this.getIsAuthor()
@@ -332,16 +334,23 @@ class MessagesWithData extends React.Component<Props, State> {
         : this.getNonAuthorEmptyMessage();
     }
 
-    return (
-      <NullState
-        heading="Sorry, we lost connection to the server..."
-        copy="Mind reloading the page?"
-      >
-        <Button icon="view-reload" onClick={() => window.location.reload(true)}>
-          Reload
-        </Button>
-      </NullState>
-    );
+    if (hasError) {
+      return (
+        <NullState
+          heading="Sorry, we lost connection to the server..."
+          copy="Mind reloading the page?"
+        >
+          <Button
+            icon="view-reload"
+            onClick={() => window.location.reload(true)}
+          >
+            Reload
+          </Button>
+        </NullState>
+      );
+    }
+
+    return null;
   }
 }
 
