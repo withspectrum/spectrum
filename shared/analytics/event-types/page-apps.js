819770bba163af00abc31fb36f15bd4e7a4551a1
Brian Lovin <briandlovin@gmail.com>
2018-11-06 08:34:27 -0800

    Add upsell in inbox thread feed for mac app

--- shared/analytics/event-types/page-apps.js
+++ shared/analytics/event-types/page-apps.js
@@ -3,3 +3,6 @@
 export const APPS_PAGE_VIEWED = 'Apps Page Viewed';
 export const APPS_PAGE_DOWNLOAD_WINDOWS_CLICKED = 'Apps Page Download Windows Clicked';
 export const APPS_PAGE_DOWNLOAD_MAC_CLICKED = 'Apps Page Download Mac Clicked';
+
+export const INBOX_DOWNLOAD_MAC_VIEWED = 'Inbox Download Mac Viewed'
+export const INBOX_DOWNLOAD_MAC_CLICKED = 'Inbox Download Mac Clicked'
