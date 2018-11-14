3cc262f957e3da4a7ddb9c38ff9218ab60f42c59
Brian Lovin <briandlovin@gmail.com>
2018-11-14 08:23:20 -0800

    Hit db for currentUser resolver

--- api/queries/user/rootCurrentUser.js
+++ api/queries/user/rootCurrentUser.js
@@ -1,5 +1,11 @@
 // @flow
 import type { GraphQLContext } from '../../';
+import { getUserById } from 'shared/db/queries/user';
 
-export default (_: any, __: any, { user }: GraphQLContext) =>
-  user ? (user.bannedAt ? null : user) : null;
+export default async (_: any, __: any, { user }: GraphQLContext) => {
+  if (!user || !user.id) return null;
+  const dbUser = await getUserById(user.id);
+  if (!dbUser) return null;
+  if (dbUser.bannedAt) return null;
+  return dbUser;
+};
