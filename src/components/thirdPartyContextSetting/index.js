251a67bb8573d2d7010fc01bae7a70c12ad41af0
Brian Lovin <briandlovin@gmail.com>
2018-11-18 15:26:38 -0800

    Move amplitude init to discrete component

--- src/components/thirdPartyContextSetting/index.js
+++ src/components/thirdPartyContextSetting/index.js
@@ -10,6 +10,23 @@ type Props = {
 };
 
 class AuthViewHandler extends React.Component<Props> {
+  componentDidMount() {
+    const AMPLITUDE_API_KEY =
+      process.env.NODE_ENV === 'production'
+        ? process.env.AMPLITUDE_API_KEY
+        : process.env.AMPLITUDE_API_KEY_DEVELOPMENT;
+    if (AMPLITUDE_API_KEY) {
+      try {
+        window.amplitude.getInstance().init(AMPLITUDE_API_KEY);
+        window.amplitude.getInstance().setOptOut(false);
+      } catch (err) {
+        console.warn('Unable to start tracking', err.message);
+      }
+    } else {
+      console.warn('No amplitude api key, tracking in development mode');
+    }
+  }
+
   componentDidUpdate(prev: Props) {
     const {
       data: { user },
