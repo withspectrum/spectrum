2cd115f2893ccb4e5022994cc6dad07b02473bec
Brian Lovin <briandlovin@gmail.com>
2018-11-18 22:47:42 -0800

    Possibly fix e2e

--- cypress/integration/apps_page_spec.js
+++ cypress/integration/apps_page_spec.js
@@ -1,10 +1,10 @@
-describe('Privacy View', () => {
+describe('Apps View', () => {
   describe('Loads page', () => {
     beforeEach(() => {
       cy.visit('/apps');
     });
 
-    it('should render the privacy page', () => {
+    it('should render the apps page', () => {
       cy.get('[data-cy="apps-page"]').should('be.visible');
     });
   });
