5d1046ce114d49be06eba3fcc896ddeaef27d4ad
Maximilian Stoiber <contact@mxstbr.com>
2018-11-21 10:30:00 +0100

    Add flow types to p-queue

--- src/helpers/p-queue.js
+++ src/helpers/p-queue.js
@@ -1,9 +1,12 @@
+// @flow
 // based on https://github.com/zeit/next.js/blob/82d56e063aad12ac8fee5b9d5ed24ccf725b1a5b/packages/next-server/lib/p-queue.js
-// with adapted code style
+// with adapted code style and flow types added
 // which is itself based on https://github.com/sindresorhus/p-queue (MIT)
 // modified for browser support
 
 class Queue {
+  _queue: Array<mixed>;
+
   constructor() {
     this._queue = [];
   }
@@ -13,17 +16,25 @@ class Queue {
   dequeue() {
     return this._queue.shift();
   }
-  get size() {
+  size() {
     return this._queue.length;
   }
 }
 
+type Options = {
+  concurrency: number,
+};
+
 export default class PQueue {
-  constructor(opts) {
+  queue: any;
+  _pendingCount: number;
+  _concurrency: number;
+  _resolveEmpty: Function;
+
+  constructor(opts: Options) {
     opts = Object.assign(
       {
         concurrency: Infinity,
-        queueClass: Queue,
       },
       opts
     );
@@ -34,7 +45,7 @@ export default class PQueue {
       );
     }
 
-    this.queue = new opts.queueClass(); // eslint-disable-line new-cap
+    this.queue = new Queue();
     this._pendingCount = 0;
     this._concurrency = opts.concurrency;
     this._resolveEmpty = () => {};
@@ -42,13 +53,13 @@ export default class PQueue {
   _next() {
     this._pendingCount--;
 
-    if (this.queue.size > 0) {
+    if (this.queue.size() > 0) {
       this.queue.dequeue()();
     } else {
       this._resolveEmpty();
     }
   }
-  add(fn, opts) {
+  add<R>(fn: () => Promise<R>): Promise<R> {
     return new Promise((resolve, reject) => {
       const run = () => {
         this._pendingCount++;
@@ -68,11 +79,11 @@ export default class PQueue {
       if (this._pendingCount < this._concurrency) {
         run();
       } else {
-        this.queue.enqueue(run, opts);
+        this.queue.enqueue(run);
       }
     });
   }
-  onEmpty() {
+  onEmpty(): Promise<void> {
     return new Promise(resolve => {
       const existingResolve = this._resolveEmpty;
       this._resolveEmpty = () => {
@@ -81,10 +92,10 @@ export default class PQueue {
       };
     });
   }
-  get size() {
-    return this.queue.size;
+  size() {
+    return this.queue.size();
   }
-  get pending() {
+  pending() {
     return this._pendingCount;
   }
 }
