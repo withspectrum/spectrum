54868fde08ff014b63f761e0e0268760d008cac0
Brian Lovin <briandlovin@gmail.com>
2018-11-20 21:44:08 -0800

    Resync member counts

---
+++ api/migrations/20181121054300-resync-community-member-counts.js
@@ -0,0 +1,39 @@
+exports.up = function(r, conn) {
+  return Promise.all([
+    r
+      .table('communities')
+      .update(
+        {
+          memberCount: r
+            .table('usersCommunities')
+            .getAll(r.row('id'), { index: 'communityId' })
+            .filter(row => row('isMember').eq(true))
+            .count()
+            .default(1),
+        },
+        {
+          nonAtomic: true,
+        }
+      )
+      .run(conn),
+    r
+      .table('channels')
+      .update(
+        {
+          memberCount: r
+            .table('usersChannels')
+            .getAll(r.row('id'), { index: 'channelId' })
+            .filter(row => row('isMember').eq(true))
+            .count()
+            .default(1),
+        },
+        {
+          nonAtomic: true,
+        }
+      )
+      .run(conn),
+  ]).catch(err => console.error(err));
+};
+exports.down = function(r, conn) {
+  return Promise.resolve();
+};
