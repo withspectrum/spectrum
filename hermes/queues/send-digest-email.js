3d01d865fe176eac857d55ca47d84c425d393495
Brian Lovin <briandlovin@gmail.com>
2018-11-28 14:42:40 -0800

    Stronger typing on hermes digest queue, fix data shape and formatting

--- hermes/queues/send-digest-email.js
+++ hermes/queues/send-digest-email.js
@@ -6,72 +6,26 @@ import Raven from 'shared/raven';
 import { generateUnsubscribeToken } from '../utils/generate-jwt';
 import { TYPE_DAILY_DIGEST, TYPE_WEEKLY_DIGEST } from './constants';
 import formatDate from '../utils/format-date';
+import type { Job, SendDigestEmailJobData } from 'shared/bull/types';
 
-type ChannelType = {
-  name: string,
-  slug: string,
-};
-
-type CommunityType = {
-  name: string,
-  slug: string,
-  profilePhoto: string,
-};
-
-type TopCommunityType = {
-  name: string,
-  slug: string,
-  profilePhoto: string,
-  coverPhoto: string,
-  description: string,
-  id: string,
-};
-
-type ThreadType = {
-  community: CommunityType,
-  channel: ChannelType,
-  channelId: string,
-  title: string,
-  threadId: string,
-  messageCountString: string,
-};
-
-type SendWeeklyDigestJobData = {
-  email: string,
-  name?: string,
-  username: string,
-  userId: string,
-  threads: Array<ThreadType>,
-  reputationString: string,
-  communities: ?Array<TopCommunityType>,
-  timeframe: 'daily' | 'weekly',
-};
-
-type SendWeeklyDigestJob = {
-  data: SendWeeklyDigestJobData,
-  id: string,
-};
-
-export default async (job: SendWeeklyDigestJob) => {
-  debug(`\nnew job: ${job.id}`);
-  debug(`\nsending weekly digest to: ${job.data.email}`);
-
+export default async (job: Job<SendDigestEmailJobData>) => {
   const {
     email,
-    userId,
     username,
+    userId,
     threads,
+    reputationString,
     communities,
     timeframe,
-    reputationString,
   } = job.data;
-  if (!email || !userId) {
-    debug('\nno email or userId found for this weekly digest, returning');
+
+  if (!email || !userId || !username) {
     return;
   }
 
   const unsubscribeType =
     timeframe === 'daily' ? TYPE_DAILY_DIGEST : TYPE_WEEKLY_DIGEST;
+
   const unsubscribeToken = await generateUnsubscribeToken(
     userId,
     unsubscribeType
@@ -85,19 +39,24 @@ export default async (job: SendWeeklyDigestJob) => {
       : 'send weekly digest email';
   const subjectPrefix =
     timeframe === 'daily' ? 'Spectrum Daily Digest' : 'Spectrum Weekly Digest';
+
   const subjectStart =
     threads.length > 2
-      ? `${threads[0].title}, ${threads[1].title}`
-      : `${threads[0].title}`;
+      ? `${threads[0].content.title}, ${threads[1].content.title}`
+      : `${threads[0].content.title}`;
+
   const subjectEnd = ` and ${
     threads.length > 2 ? threads.length - 2 : threads.length - 1
   } more active conversations in your communities`;
+
   const subject = `${subjectPrefix}: ${subjectStart}${subjectEnd}`;
+
   const { day, month, year } = formatDate();
+
   const preheader =
     timeframe === 'daily'
       ? `Your Spectrum daily digest · ${month} ${day}, ${year}`
-      : 'Your Spectrum weekly digest';
+      : `Your Spectrum weekly digest · ${month} ${day}, ${year}`;
 
   try {
     return sendEmail({
