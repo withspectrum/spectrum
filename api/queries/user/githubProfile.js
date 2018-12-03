2153b9297e13ac628ea0dd332b05ccf760ead734
Brian Lovin <briandlovin@gmail.com>
2018-12-03 14:01:17 -0800

    Use cache logic in query resolver for github info

--- api/queries/user/githubProfile.js
+++ api/queries/user/githubProfile.js
@@ -1,10 +1,50 @@
 // @flow
+require('now-env');
 import type { DBUser } from 'shared/types';
+import cache from 'shared/cache/redis';
+import fetch from 'isomorphic-fetch';
 
-export default ({ githubProviderId, githubUsername }: DBUser) => {
-  if (!githubProviderId || !githubUsername) return null;
-  return {
-    id: githubProviderId,
-    username: githubUsername,
-  };
+export default async ({ githubProviderId, id }: DBUser) => {
+  if (!githubProviderId) return null;
+
+  const cachedGithubProfile = await cache.get(`githubProfile:${id}`);
+
+  if (cachedGithubProfile) {
+    const parsed = JSON.parse(cachedGithubProfile);
+    return {
+      id: parsed.id,
+      username: parsed.username,
+    };
+  } else {
+    const apiUrl = 'https://api.github.com';
+    const apiRoute = '/user/';
+    const { GITHUB_OAUTH_CLIENT_ID, GITHUB_OAUTH_CLIENT_SECRET } = process.env;
+    const queryString = `?client_id=${GITHUB_OAUTH_CLIENT_ID}&client_secret=${GITHUB_OAUTH_CLIENT_SECRET}`;
+    const fetchUrl = apiUrl + apiRoute + githubProviderId + queryString;
+
+    const networkGithubProfile = await fetch(fetchUrl)
+      .then(res => res.json())
+      .catch(err => {
+        console.error('Error getting github info from github api', err);
+        return null;
+      });
+
+    if (networkGithubProfile) {
+      const cacheRecord = {
+        id: networkGithubProfile.id,
+        username: networkGithubProfile.login,
+      };
+
+      await cache.set(
+        `githubProfile:${id}`,
+        JSON.stringify(cacheRecord),
+        'ex',
+        86400
+      );
+
+      return cacheRecord;
+    }
+
+    return null;
+  }
 };
