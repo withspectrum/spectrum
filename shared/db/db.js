c1b47fbee59aa609aa48a9b13a1ad04c64d64cae
Max Stoiber <mxstbr@github.com>
2018-12-03 16:01:14 +0100

    Only allow one db connection for workers

--- shared/db/db.js
+++ shared/db/db.js
@@ -4,10 +4,15 @@
  */
 const IS_PROD = !process.env.FORCE_DEV && process.env.NODE_ENV === 'production';
 
+console.log(process.env.SENTRY_NAME);
+
 const DEFAULT_CONFIG = {
   // Connect to the test database when, well, testing
   db: !process.env.TEST_DB ? 'spectrum' : 'testing',
-  max: 20, // Maximum number of connections, default is 1000
+  max:
+    process.env.SENTRY_NAME === 'api' || process.env.SENTRY_NAME === 'hyperion'
+      ? 20
+      : 1, // Maximum number of connections, default is 1000
   buffer: 1, // Minimum number of connections open at any given moment, default is 50
   timeoutGb: 60 * 1000, // How long should an unused connection stick around, default is an hour, this is a minute
 };
