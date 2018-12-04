7b9fb6c809926675fee09eb3581d9c4ac8125e9a
Brian Lovin <briandlovin@gmail.com>
2018-12-04 11:10:07 -0800

    Revert resolver to use db fields

--- api/queries/user/githubProfile.js
+++ api/queries/user/githubProfile.js
@@ -1,60 +1,10 @@
 // @flow
-require('now-env');
 import type { DBUser } from 'shared/types';
-import cache from 'shared/cache/redis';
-import fetch from 'isomorphic-fetch';
-import { githubProfile } from 'shared/graphql-cache-keys';
 
-const apiUrl = 'https://api.github.com';
-const apiRoute = '/user/';
-const { GITHUB_OAUTH_CLIENT_ID, GITHUB_OAUTH_CLIENT_SECRET } = process.env;
-
-if (!GITHUB_OAUTH_CLIENT_ID || !GITHUB_OAUTH_CLIENT_SECRET) {
-  console.error(
-    'No github oauth client id or client secret found in environment.'
-  );
-}
-// $FlowIssue
-const queryString = `?client_id=${GITHUB_OAUTH_CLIENT_ID}&client_secret=${GITHUB_OAUTH_CLIENT_SECRET}`;
-const fetchUrl = (githubProviderId: string) =>
-  apiUrl + apiRoute + githubProviderId + queryString;
-
-export default async ({ githubProviderId, id }: DBUser) => {
-  if (!githubProviderId) return null;
-
-  const cachedGithubProfile = await cache.get(githubProfile(id));
-
-  if (cachedGithubProfile) {
-    try {
-      const parsed = JSON.parse(cachedGithubProfile);
-      return {
-        id: parsed.id,
-        username: parsed.username,
-      };
-    } catch (err) {}
-  } else {
-    const networkGithubProfile = await fetch(fetchUrl(githubProviderId))
-      .then(res => res.json())
-      .catch(err => {
-        console.error('Error getting github info from github api', err);
-        return null;
-      });
-
-    if (!networkGithubProfile) return null;
-
-    const cacheRecord = {
-      id: networkGithubProfile.id,
-      username: networkGithubProfile.login,
-    };
-
-    await cache.set(
-      githubProfile(id),
-      JSON.stringify(cacheRecord),
-      'NX',
-      'EX',
-      86400
-    );
-
-    return cacheRecord;
-  }
+export default ({ githubProviderId, githubUsername }: DBUser) => {
+  if (!githubProviderId || !githubUsername) return null;
+  return {
+    id: githubProviderId,
+    username: githubUsername,
+  };
 };
