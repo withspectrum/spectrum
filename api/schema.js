0ac5d85f4838597f5103434b9ba3a9fe3aeef714
Max Stoiber <mxstbr@github.com>
2018-12-06 16:52:27 +0100

    Update schema.js

--- api/schema.js
+++ api/schema.js
@@ -155,7 +155,7 @@ if (process.env.REACT_APP_MAINTENANCE_MODE === 'enabled') {
   console.error('\n\n⚠️ ----MAINTENANCE MODE ENABLED----⚠️\n\n');
   addSchemaLevelResolveFunction(schema, () => {
     throw new UserError(
-      "We're currently undergoing planned maintenance. We'll be back by 3pm UTC, please check https://twitter.com/withspectrum for ongoing updates!"
+      "We're currently undergoing planned maintenance. We'll be back soon, please check https://twitter.com/withspectrum for ongoing updates!"
     );
   });
 }
