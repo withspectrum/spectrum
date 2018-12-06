fc1f5e6346f585fd5015e2ec9f9b3a10649c81fb
Max Stoiber <mxstbr@github.com>
2018-12-06 16:53:08 +0100

    Remove duplicated amplitude logic

--- src/routes.js
+++ src/routes.js
@@ -162,23 +162,6 @@ type Props = {
 };
 
 class Routes extends React.Component<Props> {
-  componentDidMount() {
-    const AMPLITUDE_API_KEY =
-      process.env.NODE_ENV === 'production'
-        ? process.env.AMPLITUDE_API_KEY
-        : process.env.AMPLITUDE_API_KEY_DEVELOPMENT;
-    if (AMPLITUDE_API_KEY) {
-      try {
-        window.amplitude.getInstance().init(AMPLITUDE_API_KEY);
-        window.amplitude.getInstance().setOptOut(false);
-      } catch (err) {
-        console.warn('Unable to start tracking', err.message);
-      }
-    } else {
-      console.warn('No amplitude api key, tracking in development mode');
-    }
-  }
-
   render() {
     const { currentUser, isLoadingCurrentUser } = this.props;
     const { title, description } = generateMetaInfo();
