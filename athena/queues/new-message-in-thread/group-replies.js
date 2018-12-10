7f2259fcf70278f28252ede8c6d5f12604129647
Brian Lovin <briandlovin@gmail.com>
2018-12-09 16:10:13 -0800

    Fix styling in grouped thread replies

--- athena/queues/new-message-in-thread/group-replies.js
+++ athena/queues/new-message-in-thread/group-replies.js
@@ -26,7 +26,7 @@ export default async replies => {
         ? // prettier-ignore
           `<p class='reply-img-container' style="background: #E6ECF7; margin-bottom: 2px; border-radius: 12px; overflow: hidden; display: block; max-width: 100%; margin-top: 0!important; color: #16171A;"><img class='reply-img' style="display: block;width: 100%;border-radius: 12px;" src='${signImageUrl(reply.content.body)}' /></p>`
         : // prettier-ignore
-          `<p class='reply' style="font-weight: 400;background: #E6ECF7;margin-bottom: 2px;padding: 8px 12px;border-radius: 12px;display: block;max-width: 100%;margin-top: 0!important;color: #16171A;">${escapehtml(reply.content.body)}</p>`;
+          `<p class='reply' style="font-weight: 400; font-size: 16px;background: #E6ECF7;margin-bottom: 2px;padding: 8px 12px;border-radius: 12px;display: block;max-width: 100%;margin-top: 0!important;color: #16171A;">${escapehtml(reply.content.body)}</p>`;
 
     const newGroup = {
       ...reply,
