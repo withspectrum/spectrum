cf32a11ddae61ed9cf854eabd7237440a4fd11ac
Brian Lovin <briandlovin@gmail.com>
2018-12-11 10:18:00 -0800

    Simplify frontend-perf-improvements branch

--- api/queries/thread/reactions.js
+++ api/queries/thread/reactions.js
@@ -4,31 +4,33 @@ import Raven from 'shared/raven';
 import type { DBThread } from 'shared/types';
 import type { GraphQLContext } from '../../';
 
-export default async (
-  { id, reactionCount }: DBThread,
-  _: any,
-  { user, loaders }: GraphQLContext
-) => {
-  const hasReacted = user ? await hasReactedToThread(user.id, id) : false;
-  if (typeof reactionCount === 'number') {
-    return {
-      count: reactionCount,
-      hasReacted,
-    };
-  }
+export default async (root: DBThread, _: any, ctx: GraphQLContext) => {
+  const { user, loaders } = ctx;
+  const { id, reactionCount } = root;
 
-  // Fallback if there's no denormalized reactionCount, also report to Sentry
-  Raven.captureException(
-    new Error(
-      `Thread with ID "${id}" does not have denormalized reactionCount.`
-    )
-  );
+  const getReactionCount = async () => {
+    if (typeof reactionCount === 'number') return reactionCount;
+
+    Raven.captureException(
+      new Error(
+        `Thread with ID "${id}" does not have denormalized reactionCount.`
+      )
+    );
+
+    return await loaders.threadReaction.load(id).then(res => {
+      if (res && Array.isArray(res.reduction)) {
+        return res.reduction.length;
+      }
+
+      return 0;
+    });
+  };
+
+  const hasReacted = user ? await hasReactedToThread(user.id, id) : false;
+  const count = await getReactionCount();
+
   return {
-    count: await loaders.threadReaction
-      .load(id)
-      .then(
-        res => (res && Array.isArray(res.reduction) ? res.reduction.length : 0)
-      ),
     hasReacted,
+    count,
   };
 };
