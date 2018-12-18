f330c2fbef4519275ff3ddf4f16043186f940829
Max Stoiber <mxstbr@github.com>
2018-12-18 15:02:02 +0100

    Switch to datadog-metrics package

--- shared/statsd.js
+++ shared/statsd.js
@@ -1,27 +1,44 @@
 // @flow
 const debug = require('debug')('shared:middlewares:statsd');
-import StatsD from 'hot-shots';
+import metrics from 'datadog-metrics';
 
-export const statsd = new StatsD({
-  mock: process.env.NODE_ENV !== 'production',
-  globalTags: {
-    server: process.env.SENTRY_NAME || 'unknown_server',
-  },
-});
-
-statsd.socket.on('error', function(error) {
-  console.error('Error in socket: ', error);
-});
-
-// Log StatsD events in development by monkey-patching the internal _send method
-// because hot-shots does not expose a way to listen to events as they happen
-if (debug.enabled) {
-  const originalSend = statsd._send;
-  statsd._send = (...args) => {
-    statsd.mockBuffer.forEach(item => {
-      debug(item);
-    });
-    statsd.mockBuffer = [];
-    return originalSend.call(statsd, ...args);
+export let statsd;
+if (!process.env.DATADOG_API_KEY) {
+  console.warn('No DATADOG_API_KEY provided, not tracking metrics.');
+  statsd = {
+    histogram: () => {},
+    timing: () => {},
+    increment: () => {},
+    gauge: () => {},
   };
+} else {
+  metrics.init({
+    defaultTags: [`server:${process.env.SENTRY_NAME || 'unknown_server'}`],
+  });
+
+  const stringify = (obj?: { [key: string]: string }) => {
+    if (!obj) return obj;
+    return Object.keys(obj).reduce((arr, key) => {
+      return arr.concat([`${key}:${obj[key]}`]);
+    }, []);
+  };
+  // This is necessary for express-hot-shots to work
+  const handleObjectTags = method => {
+    const original = metrics[method];
+    metrics[method] = (key, val, tags, timestamp) => {
+      return original.call(
+        metrics,
+        key,
+        val,
+        Array.isArray(tags) ? tags : stringify(tags),
+        timestamp
+      );
+    };
+  };
+  handleObjectTags('histogram');
+  handleObjectTags('gauge');
+  handleObjectTags('increment');
+  metrics.timing = (...args) => metrics.histogram.call(metrics, ...args);
+
+  statsd = metrics;
 }
