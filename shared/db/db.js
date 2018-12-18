5ae798b915b04b67ff709635d826386c1b28b6de
Brian Lovin <briandlovin@gmail.com>
2018-12-18 13:39:35 -0800

    Set db min and max to the same amount

--- shared/db/db.js
+++ shared/db/db.js
@@ -11,8 +11,8 @@ const IS_PROD = !process.env.FORCE_DEV && process.env.NODE_ENV === 'production';
 const DEFAULT_CONFIG = {
   // Connect to the test database when, well, testing
   db: !process.env.TEST_DB ? 'spectrum' : 'testing',
-  max: 1000, // Maximum number of connections, default is 1000
-  buffer: 50, // Minimum number of connections open at any given moment, default is 50
+  max: 20, // Maximum number of connections, default is 1000
+  buffer: 20, // Minimum number of connections open at any given moment, default is 50
   timeoutGb: 60 * 60 * 1000, // How long should an unused connection stick around, default is an hour, this is a minute
   timeout: 30, // The number of seconds for a connection to be opened, default 20
 };
