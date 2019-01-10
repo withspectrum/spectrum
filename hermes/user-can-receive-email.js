991e51b48f9e568486f1ff423a62da3325f99845
Brian Lovin <briandlovin@gmail.com>
2019-01-10 09:40:19 -0800

    Add extra checks at send time for valid user recipient

---
+++ hermes/user-can-receive-email.js
@@ -0,0 +1,36 @@
+// @flow
+import { getUserById } from 'shared/db/queries/user';
+import { events } from 'shared/analytics';
+import { trackQueue } from 'shared/bull/queues';
+
+type Props = {
+  to: string,
+  userId: string,
+};
+
+export const userCanReceiveEmail = async ({ to, userId }: Props) => {
+  if (!to) {
+    if (userId) {
+      trackQueue.add({
+        userId: userId,
+        event: events.EMAIL_BOUNCED,
+        properties: { error: 'To field was not provided' },
+      });
+    }
+
+    return false;
+  }
+
+  // qq.com email addresses are isp blocked, which raises our error rate
+  // on sendgrid. prevent sending these emails at all
+  to = to.filter(toType => {
+    return toType.email.substr(to.length - 7) !== '@qq.com';
+  });
+
+  if (!to || to.length === 0) return false;
+
+  const user = await getUserById(userId);
+  if (!user || user.bannedAt || user.deletedAt || !user.email) return false;
+
+  return true;
+};
