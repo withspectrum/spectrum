1cdee71e82cd7155cc1b4fabc1242b25b3881574
Max Stoiber <mxstbr@github.com>
2019-01-17 14:43:06 +0100

    Update validate-draft-js-input.js

--- api/utils/validate-draft-js-input.js
+++ api/utils/validate-draft-js-input.js
@@ -1,5 +1,6 @@
+// @flow
 export const validateRawContentState = (input: any) => {
-  if (!input.blocks || !Array.isArray(input.blocks) || !input.entityMap) {
+  if (!input || !input.blocks || !Array.isArray(input.blocks) || !input.entityMap) {
     trackQueue.add({
       userId: user.id,
       event: eventFailed,
