fd5a2958fecf5b30a14fa24b4a5a3e22c5be181a
Maximilian Stoiber <contact@mxstbr.com>
2019-02-04 11:11:25 +0100

    Resolve unknown prop warning

--- src/components/mentionsInput/index.js
+++ src/components/mentionsInput/index.js
@@ -106,12 +106,14 @@ const SpectrumMentionsInput = (props: Props) => {
     return callback(uniqueResults.slice(0, 8));
   };
 
+  const { dataCy, ...rest } = props;
+
   return (
     <MentionsInput
       displayTransform={username => `@${username}`}
       markup="@[__id__]"
       data-cy={props.dataCy}
-      {...props}
+      {...rest}
       style={{ ...(props.style || {}), ...MentionsInputStyle }}
     >
       <Mention
