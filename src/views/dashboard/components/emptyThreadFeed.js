8e2299781669916f0079df5afa97dd59d6661af0
b1ncer <b1ncer@foxmail.com>
2019-02-17 13:43:52 +0800

    fix code style

--- src/views/dashboard/components/emptyThreadFeed.js
+++ src/views/dashboard/components/emptyThreadFeed.js
@@ -8,7 +8,7 @@ import { openComposer } from '../../../actions/composer';
 import Icon from '../../../components/icons';
 import { Button } from '../../../components/buttons';
 import { NullThreadFeed, NullHeading, OutlineButton, Hint } from '../style';
-import { isMobile } from '../../../helpers/utils'
+import { isMobile } from '../../../helpers/utils';
 
 const EmptyThreadFeed = ({ dispatch }) => (
   <NullThreadFeed>
@@ -18,13 +18,15 @@ const EmptyThreadFeed = ({ dispatch }) => (
     <NullHeading>We've got recommendations!</NullHeading>
     <Hint>Kick your community off right!</Hint>
     {/* dispatch activethread to 'new'? */}
-    {
-        isMobile() ? <Link to={'/new/thread'}><Button icon={'post'}>
-      Post your first thread
-    </Button></Link> : <Button icon={'post'} onClick={() => dispatch(openComposer())}>
-      Post your first thread
-    </Button>
-    }
+    {isMobile() ? (
+      <Link to={'/new/thread'}>
+        <Button icon={'post'}>Post your first thread</Button>
+      </Link>
+    ) : (
+      <Button icon={'post'} onClick={() => dispatch(openComposer())}>
+        Post your first thread
+      </Button>
+    )}
     <Hint>Find new friends and great conversations!</Hint>
     <Link to={'/explore'}>
       <OutlineButton>
