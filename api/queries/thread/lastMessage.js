e50ca7deb9921f8047502844e8c81fdc9c6cb90a
Brian Lovin <briandlovin@gmail.com>
2019-02-21 16:35:16 -0700

    Fix import

--- api/queries/thread/lastMessage.js
+++ api/queries/thread/lastMessage.js
@@ -1,7 +1,7 @@
 // @flow
 import type { GraphQLContext } from '../../';
 import type { DBThread } from 'shared/types';
-import { getLastMessage } from '../models/message';
+import { getLastMessage } from '../../models/message';
 
 export default async ({ id }: DBThread, _: any, ctx: GraphQLContext) => {
   return await getLastMessage(id);
