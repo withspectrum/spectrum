7814f30391f7b4431abfae8b8e9e1c14bb8be891
Brian Lovin <briandlovin@gmail.com>
2019-02-28 11:12:47 -0500

    Use a relative path so it doesn't keep extending the path if you open many threads

--- src/helpers/get-thread-link.js
+++ src/helpers/get-thread-link.js
@@ -3,7 +3,7 @@ import slugg from 'slugg';
 import type { ThreadInfoType } from 'shared/graphql/fragments/thread/threadInfo';
 
 const getThreadLink = (thread: ThreadInfoType) => {
-  return `${thread.community.slug}/${thread.channel.slug}/${slugg(
+  return `/${thread.community.slug}/${thread.channel.slug}/${slugg(
     thread.content.title
   )}~${thread.id}`;
 };
