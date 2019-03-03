4aa0b7d5535dae5e44a7944105f341f78395fc51
Brian Lovin <briandlovin@gmail.com>
2019-03-03 13:36:57 -0500

    Remove inboxthread - gotta start from scratch

--- src/views/communityNew/components/WatercoolerChat.js
+++ src/views/communityNew/components/WatercoolerChat.js
@@ -1,14 +1,9 @@
 // @flow
 import * as React from 'react';
-import { InboxThreadView } from 'src/views/thread';
 import { WatercoolerWrapper } from '../style';
 
 export const WatercoolerChat = (props: Props) => {
   const { id } = props;
 
-  return (
-    <WatercoolerWrapper>
-      <InboxThreadView threadViewContext={'inbox'} threadId={id} id={id} />
-    </WatercoolerWrapper>
-  );
+  return <WatercoolerWrapper />;
 };
