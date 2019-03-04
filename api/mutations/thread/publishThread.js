f5851100be1c008bd291965416fb75ec14f49103
Brian Lovin <briandlovin@gmail.com>
2019-03-04 11:30:20 -0500

    Remove sam sentence logic

--- api/mutations/thread/publishThread.js
+++ api/mutations/thread/publishThread.js
@@ -350,15 +350,10 @@ export default requireAuth(
     // Post a new message with a link to the new thread to the watercooler thread if one exists
     if (community.watercoolerId && !channel.isPrivate) {
       const identifier = user.username ? `@${user.username}` : user.name;
-      const sentence =
-        Array.isArray(usersPreviousPublishedThreads) &&
-        usersPreviousPublishedThreads.length === 0
-          ? `their first thread 👋`
-          : 'a new thread 📝';
       await addMessage(
         {
           content: {
-            body: `${identifier} just posted ${sentence} https://spectrum.chat/${
+            body: `${identifier} just posted a new thread: https://spectrum.chat/${
               community.slug
             }/${channel.slug}/${slugg(dbThread.content.title)}~${dbThread.id}`,
           },
