0f46623250643083b02b3d95bf7270607849d7c2
Brian Lovin <briandlovin@gmail.com>
2019-03-04 11:58:33 -0500

    Break words in thread titles

--- src/views/thread/style.js
+++ src/views/thread/style.js
@@ -183,6 +183,7 @@ export const ThreadContent = styled.div`
 export const ThreadHeading = styled(H1)`
   font-size: 28px;
   font-weight: 600;
+  word-break: break-word;
 `;
 
 export const A = styled.a`
