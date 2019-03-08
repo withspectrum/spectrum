c8cefb3694bb6ef43d98eb1a3e139e39bbc70c64
Brian Lovin <briandlovin@gmail.com>
2019-03-08 11:36:11 -0500

    Bulk filename update

---
+++ src/views/community1/containers/privateCommunity.js
@@ -0,0 +1,56 @@
+// @flow
+import React from 'react';
+import { OutlineButton, PrimaryButton } from '../components/button';
+import RequestJoinCommunity from '../components/requestJoinCommunity';
+import {
+  Emoji,
+  Heading,
+  Description,
+  ActionsRow,
+  PrivateCommunityWrapper,
+} from '../style';
+import { ViewGrid, CenteredGrid } from 'src/components/layout';
+
+export const PrivateCommunity = (props: Props) => {
+  const { community } = props;
+  const { communityPermissions } = community;
+  const { isPending } = communityPermissions;
+
+  const heading = isPending
+    ? 'Your request to join is pending'
+    : 'This community is private';
+
+  const description = isPending
+    ? 'Your request to join this community is pending. The owners have been notified, and you will be notified if your request is approved.'
+    : 'You can request to join this community below. The owners will be notified of your request, and you will be notified if your request is approved.';
+
+  const primaryAction = ({ isLoading }) =>
+    isPending ? (
+      <PrimaryButton isLoading={isLoading}>Cancel request</PrimaryButton>
+    ) : (
+      <PrimaryButton isLoading={isLoading}>Request to join</PrimaryButton>
+    );
+
+  return (
+    <ViewGrid>
+      <CenteredGrid>
+        <PrivateCommunityWrapper>
+          <Emoji role="img" aria-label="Private">
+            🔑
+          </Emoji>
+          <Heading>{heading}</Heading>
+          <Description>{description}</Description>
+          <ActionsRow>
+            <OutlineButton to={'/'}>Go home</OutlineButton>
+
+            <RequestJoinCommunity
+              isPending={isPending}
+              communityId={community.id}
+              render={({ isLoading }) => primaryAction({ isLoading })}
+            />
+          </ActionsRow>
+        </PrivateCommunityWrapper>
+      </CenteredGrid>
+    </ViewGrid>
+  );
+};
