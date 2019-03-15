08ce5a0d3c14fbddd5498c0aadc17b3316e488b5
Max Stoiber <mxstbr@github.com>
2019-03-15 20:36:36 +0100

    Handle cached data in scroller

--- src/views/thread/components/messagesSubscriber.js
+++ src/views/thread/components/messagesSubscriber.js
@@ -11,7 +11,6 @@ import viewNetworkHandler, {
 } from 'src/components/viewNetworkHandler';
 import ChatMessages from 'src/components/messageGroup';
 import { Loading } from 'src/components/loading';
-import InfiniteScroller from 'src/components/infiniteScroll';
 import NullMessages from './nullMessages';
 
 type Props = {
@@ -31,6 +30,17 @@ class Messages extends React.Component<Props> {
   unsubscribe: Function;
 
   componentDidMount() {
+    const thread = this.props.data.thread;
+    // Scroll to bottom on mount if we got cached data as getSnapshotBeforeUpdate does not fire for mounts
+    if (
+      thread &&
+      thread.messageConnection &&
+      (thread.watercooler || thread.currentUserLastSeen)
+    ) {
+      const elem = document.getElementById('main');
+      if (!elem) return;
+      elem.scrollTop = elem.scrollHeight;
+    }
     this.unsubscribe = this.props.subscribeToNewMessages();
   }
 
