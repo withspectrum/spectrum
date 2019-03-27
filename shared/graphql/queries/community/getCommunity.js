3c50662337f10d1dd93ce0c1f0613478dfc092f0
Brian Lovin <briandlovin@gmail.com>
2019-03-27 17:19:44 -0400

    Fix new community onboarding flow

--- shared/graphql/queries/community/getCommunity.js
+++ shared/graphql/queries/community/getCommunity.js
@@ -71,10 +71,23 @@ export const getCommunityByMatchQuery = gql`
 `;
 
 const getCommunityByMatchOptions = {
-  options: ({ match: { params: { communitySlug } } }) => ({
+  options: ({
+    match: {
+      params: { communitySlug },
+    },
+  }) => ({
     variables: {
       slug: communitySlug,
     },
+    /*
+      don't change this fetch policy
+      its required to make the new community creation flow work
+      after a user creates their community, they have a button to view
+      the community or go to settings - if this does not follow a cache hit
+      with a network request, the client will think the community does
+      not exist
+    */
+    fetchPolicy: 'cache-and-network',
   }),
 };
 
