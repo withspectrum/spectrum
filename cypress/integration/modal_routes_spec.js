c2f66a5c2e6d62d8c0afedb84d3ccc1d184da349
Brian Lovin <briandlovin@gmail.com>
2019-03-27 13:57:59 -0400

    Remove unneeded composer modal route tests

--- cypress/integration/modal_routes_spec.js
+++ cypress/integration/modal_routes_spec.js
@@ -9,50 +9,6 @@ const communityBeforeUrlIsValid = () =>
 const channelBeforeUrlIsValid = () =>
   cy.url().should('eq', 'http://localhost:3000/spectrum/general?tab=posts');
 
-describe('composer modal route', () => {
-  beforeEach(() => {
-    cy.auth(user.id);
-  });
-
-  const threadComposerWrapper = () =>
-    cy.get('[data-cy="thread-composer-wrapper"]');
-  const threadComposer = () => cy.get('[data-cy="modal-container"]');
-
-  it('handles community view', () => {
-    cy.visit('/spectrum');
-    communityBeforeUrlIsValid();
-    threadComposer().should('be.visible');
-    threadComposerWrapper().should('be.visible');
-    cy.url().should(
-      'eq',
-      'http://localhost:3000/new/thread?composerCommunityId=1'
-    );
-
-    pressEscape();
-
-    threadComposer().should('not.be.visible');
-    threadComposerWrapper().should('not.be.visible');
-    communityBeforeUrlIsValid();
-  });
-
-  it('handles channel view', () => {
-    cy.visit('/spectrum/general');
-    channelBeforeUrlIsValid();
-    threadComposer().should('be.visible');
-    threadComposerWrapper().should('be.visible');
-    cy.url().should(
-      'eq',
-      'http://localhost:3000/new/thread?composerChannelId=1&composerCommunityId=1'
-    );
-
-    pressEscape();
-
-    threadComposer().should('not.be.visible');
-    threadComposerWrapper().should('not.be.visible');
-    channelBeforeUrlIsValid();
-  });
-});
-
 describe('thread modal route', () => {
   const threadSlider = () => cy.get('[data-cy="modal-container"]');
   const threadSliderClose = () => cy.get('[data-cy="thread-slider-close"]');
