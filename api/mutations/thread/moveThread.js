4dc9caac6ed2783ddbac97ac717e536b638fab6c
Brian Lovin <briandlovin@gmail.com>
2019-05-02 09:00:20 -0400

    Remove pinned thread if thread is moved to a private channel

--- api/mutations/thread/moveThread.js
+++ api/mutations/thread/moveThread.js
@@ -3,7 +3,8 @@ import type { GraphQLContext } from '../../';
 import UserError from '../../utils/UserError';
 import { getThread, moveThread } from '../../models/thread';
 import { getUserPermissionsInCommunity } from '../../models/usersCommunities';
-import { getChannels } from '../../models/channel';
+import { getChannelById } from '../../models/channel';
+import { getCommunityById } from '../../models/community';
 import { isAuthedResolver as requireAuth } from '../../utils/permissions';
 import { events } from 'shared/analytics';
 import { trackQueue } from 'shared/bull/queues';
@@ -18,6 +19,8 @@ export default requireAuth(async (_: any, args: Input, ctx: GraphQLContext) => {
   const { threadId, channelId } = args;
 
   const thread = await getThread(threadId);
+  const { communityId } = thread;
+
   if (!thread) {
     trackQueue.add({
       userId: user.id,
@@ -65,7 +68,11 @@ export default requireAuth(async (_: any, args: Input, ctx: GraphQLContext) => {
     );
   }
 
-  const [newChannel] = await getChannels([channelId]);
+  const [newChannel, community] = await Promise.all([
+    getChannelById(channelId),
+    getCommunityById(thread.communityid),
+  ]);
+
   if (newChannel.communityId !== thread.communityId) {
     trackQueue.add({
       userId: user.id,
@@ -79,6 +86,16 @@ export default requireAuth(async (_: any, args: Input, ctx: GraphQLContext) => {
     return new UserError(
       'You can only move threads within the same community.'
     );
+  }
+
+  // if the thread is being moved into a private channel, make sure it is not pinned
+  // in the community
+  if (
+    newChannel.isPrivate &&
+    community.pinnedThreadId &&
+    thread.id === community.pinnedThreadId
+  ) {
+    await setPinnedThreadInCommunity(communityId, null, user.id);
   }
 
   return moveThread(threadId, channelId, user.id).then(res => {
