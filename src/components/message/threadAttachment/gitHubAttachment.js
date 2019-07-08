d89bfbb20210b990041daa33ed928aceb5e61612
Kwadwo Amankwa <citisolo@gmail.com>
2019-07-08 16:23:23 +0100

    Alternative format for github attachment

--- src/components/message/threadAttachment/gitHubAttachment.js
+++ src/components/message/threadAttachment/gitHubAttachment.js
@@ -1,9 +1,7 @@
 // @flow
 import React, { useState, useEffect } from 'react';
-import path from 'path';
 // $FlowFixMe
 import parseGithubUrl from 'parse-github-url';
-import { Loading } from 'src/components/loading';
 import { Container, Column, GitHubBadge } from './style';
 import { ThreadTitle } from 'src/components/inboxThread/style';
 import { InnerMessageContainer } from '../style';
@@ -11,71 +9,47 @@ import { InnerMessageContainer } from '../style';
 const GitHubAttachment = (props: { url: string }) => {
   const { url } = props;
 
-  const [apiData, setApiData] = useState(null);
-  const [urlData] = useState(parseGithubUrl(url));
-
-  const [loading, setLoading] = useState(true);
+  const [urlData, setUrlData] = useState('');
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    // if the request takes longer than timeout, fallback case is triggered.
-    // When request completes it either fails or succeeds and updates attachment
     const fetchData = async () => {
-      let fetchTimeOut = setTimeout(() => {
-        setError(true);
-        setLoading(false);
-      }, 4000);
-
       try {
-        const apiUrl = path.join('https://api.github.com/repos/', urlData.path);
-        const response = await fetch(apiUrl);
-
-        if (response.ok) {
-          const json = await response.json();
-          clearTimeout(fetchTimeOut);
-          setApiData(json);
-          setError(false);
-          setLoading(false);
-        } else {
-          throw new Error('Failed Response');
+        setUrlData(parseGithubUrl(url));
+        if (
+          !urlData.href ||
+          !urlData.branch ||
+          !urlData.filepath ||
+          !urlData.filepath
+        ) {
+          throw new Error('Url Error');
         }
       } catch (err) {
-        clearTimeout(fetchTimeOut);
         setError(true);
-        setLoading(false);
       }
     };
-
     fetchData();
-  }, []);
+  }, [url]);
 
   let attachment;
 
   if (error) {
-    attachment = <a href={url}>{url}</a>;
-  } else if (loading || !apiData) {
-    attachment = (
-      <Container style={{ padding: '16px 12px' }}>
-        <Loading />
-      </Container>
-    );
+    attachment = attachment = <a href={url}>{url}</a>;
   } else {
     attachment = (
       <div className="attachment-container">
         <Container data-cy="github-attachment">
           <Column>
-            <a href={apiData.html_url}>
+            <a href={urlData.href}>
               <ThreadTitle>
-                {`${apiData.title} · #${apiData.number} `}
-                <GitHubBadge
-                  color={apiData.state === 'open' ? '#2cbe4e' : '#cb2431'}
-                >
-                  {apiData.state}
-                </GitHubBadge>
+                {`${
+                  urlData.branch === 'issues' ? 'Issue' : 'Pull Request'
+                } ·  `}
+                <GitHubBadge color={'#5319e7'}>#{urlData.filepath}</GitHubBadge>
               </ThreadTitle>
             </a>
             <InnerMessageContainer style={{ fontSize: '12px' }}>
-              {`${urlData.repo} · ${apiData.user.login}`}
+              {`${urlData.repo}`}
             </InnerMessageContainer>
           </Column>
         </Container>
