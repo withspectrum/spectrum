4cb374eb89016a5c7abc44a76a43e4e37b35741a
Brian Lovin <briandlovin@gmail.com>
2020-04-27 16:35:25 -0700

    Don't automatically redirect to unbreak the dashboard navigation

--- src/views/viewHelpers/fullScreenRedirect.js
+++ src/views/viewHelpers/fullScreenRedirect.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { PrimaryButton } from 'src/components/button';
 import { Heading, Description, Card } from './style';
+import { CommunityAvatar } from 'src/components/avatar';
 import { ViewGrid, CenteredGrid } from 'src/components/layout';
 import type { CommunityInfoType } from 'shared/graphql/fragments/community/communityInfo';
 
@@ -20,14 +21,25 @@ export const FullScreenRedirectView = (props: Props) => {
             display: 'flex',
             alignItems: 'center',
             flexDirection: 'column',
+            textAlign: 'center',
+            padding: '48px 24px',
           }}
         >
+          <CommunityAvatar
+            community={community}
+            size={64}
+            showHoverProfile={false}
+            isClickable={false}
+            style={{ marginBottom: '32px' }}
+          />
           <Heading>{community.name} has a new home</Heading>
-          <Description>This community is no longer on Spectrum.</Description>
+          <Description style={{ padding: 0, marginTop: 0 }}>
+            This community is no longer on Spectrum.
+          </Description>
           <div style={{ padding: '16px' }} />
           {community.website && (
             <PrimaryButton href={community.website}>
-              Go to new community page
+              Visit new community
             </PrimaryButton>
           )}
         </Card>
