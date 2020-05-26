ef7af65321a37ab0340d6f30c7087531bd1bf53d
Max Schoening <max@max.dev>
2020-05-25 18:45:42 -0700

    Add script to automate Heroku deploys

---
+++ scripts/heroku-deploy.js
@@ -0,0 +1,75 @@
+const { execSync } = require('child_process');
+const error = require('./utils/error');
+const parse = require('./utils/parse-argv');
+const path = require('path');
+
+const exec = cmd =>
+  execSync(cmd, { cwd: path.join(__dirname, '../'), stdio: 'inherit' });
+
+const { args } = parse(process.argv);
+
+const VALID_SERVERS = [
+  'all',
+  'api',
+  'athena',
+  'chronos',
+  'hermes',
+  'hyperion',
+  'mercury',
+  'vulcan',
+];
+
+let servers = args;
+
+/*
+ * Must provide at least one server
+ */
+if (servers.length === 0)
+  error(
+    'Server name missing',
+    `Please provide one of the following server names: ${VALID_SERVERS.map(
+      s => `"${s}"`
+    ).join(', ')}`
+  );
+
+/*
+ * Must provide valid server names
+ */
+servers.forEach(s => {
+  if (VALID_SERVERS.indexOf(s) === -1)
+    error(
+      `Cannot deploy unknown server "${args[0]}"`,
+      `Please provide one of the following server names: ${VALID_SERVERS.map(
+        s => `"${s}"`
+      ).join(', ')}`
+    );
+});
+
+/*
+ * Allow targetting all deployments
+ */
+if (servers.indexOf('all') > -1) {
+  servers = VALID_SERVERS.filter(s => s !== 'all');
+}
+
+console.log(
+  `Deploying the following servers: ${servers.map(s => `"${s}"`).join(', ')}`
+);
+
+/*
+ * Build and push Docker images for each server
+ */
+servers.forEach(s => {
+  exec(
+    `docker build -t spectrum_${s} -t registry.heroku.com/spectrum-chat-${s}/web -f docker/Dockerfile.${s} .`
+  );
+
+  exec(`docker push registry.heroku.com/spectrum-chat-${s}/web`);
+});
+
+/*
+ * Release new images for each server
+ */
+servers.forEach(s => {
+  exec(`heroku container:release web -a spectrum-chat-${s}`);
+});
