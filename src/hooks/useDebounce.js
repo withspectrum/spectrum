5f58229fad5c1bc4310e55ba1907c76f0cf15702
edumoreira1506 <eduardo.moreira@codeminer42.com>
2020-07-24 10:43:09 -0300

    Add useDebounce to project

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,18 @@
+// @see https://dev.to/gabe_ragland/debouncing-with-react-hooks-jci
+function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
