90baed39192ab7af41891f63c666a95abe560c4c
Brian Lovin <briandlovin@gmail.com>
2021-08-23 08:04:53 -0400

    Remove DM button from user profiles

--- src/components/entities/profileCards/components/userActions.js
+++ src/components/entities/profileCards/components/userActions.js
@@ -6,12 +6,7 @@ import type { Dispatch } from 'redux';
 import type { UserInfoType } from 'shared/graphql/fragments/user/userInfo';
 import { openModal } from 'src/actions/modals';
 import { withCurrentUser } from 'src/components/withCurrentUser';
-import {
-  PrimaryOutlineButton,
-  HoverWarnOutlineButton,
-  OutlineButton,
-} from 'src/components/button';
-import InitDirectMessageWrapper from 'src/components/initDirectMessageWrapper';
+import { HoverWarnOutlineButton, OutlineButton } from 'src/components/button';
 import { ActionsRowContainer } from '../style';
 import { isAdmin } from 'src/helpers/is-admin';
 
@@ -26,10 +21,6 @@ export const UnconnectedUserActions = (props: Props) => {
 
   if (!user) return null;
 
-  const initReport = () => {
-    return dispatch(openModal('REPORT_USER_MODAL', { user }));
-  };
-
   const initBan = () => {
     return dispatch(openModal('BAN_USER_MODAL', { user }));
   };
@@ -40,21 +31,6 @@ export const UnconnectedUserActions = (props: Props) => {
         <OutlineButton to={`/users/${user.username}/settings`}>
           Settings
         </OutlineButton>
-      )}
-
-      <InitDirectMessageWrapper
-        user={user}
-        render={
-          <PrimaryOutlineButton data-cy="message-user-button">
-            Message
-          </PrimaryOutlineButton>
-        }
-      />
-
-      {currentUser && user.id !== currentUser.id && (
-        <HoverWarnOutlineButton onClick={initReport}>
-          Report
-        </HoverWarnOutlineButton>
       )}
 
       {currentUser && user.id !== currentUser.id && isAdmin(currentUser.id) && (
